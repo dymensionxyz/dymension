291eabcf37b2423cbbf2c8d5a562acc761ce0e3a
Lior Zilpa <liorzilp@gmail.com>
2022-07-31 14:58:25 +0300

    remove dymension module and re-add vue

--- vue/src/store/generated/index.ts
+++ vue/src/store/generated/index.ts
@@ -20,8 +20,11 @@ import CosmosIbcGoIbcCoreChannelV1 from './cosmos/ibc-go/ibc.core.channel.v1'
 import CosmosIbcGoIbcCoreClientV1 from './cosmos/ibc-go/ibc.core.client.v1'
 import CosmosIbcGoIbcCoreConnectionV1 from './cosmos/ibc-go/ibc.core.connection.v1'
 import CosmosIbcGoIbcCorePortV1 from './cosmos/ibc-go/ibc.core.port.v1'
+<<<<<<< HEAD
 import DymensionxyzDymensionDymensionxyzDymensionRollapp from './dymensionxyz/dymension/dymensionxyz.dymension.rollapp'
 import DymensionxyzDymensionDymensionxyzDymensionSequencer from './dymensionxyz/dymension/dymensionxyz.dymension.sequencer'
+=======
+>>>>>>> remove dymension module and re-add vue
 
 
 export default { 
@@ -45,8 +48,11 @@ export default {
   CosmosIbcGoIbcCoreClientV1: load(CosmosIbcGoIbcCoreClientV1, 'ibc.core.client.v1'),
   CosmosIbcGoIbcCoreConnectionV1: load(CosmosIbcGoIbcCoreConnectionV1, 'ibc.core.connection.v1'),
   CosmosIbcGoIbcCorePortV1: load(CosmosIbcGoIbcCorePortV1, 'ibc.core.port.v1'),
+<<<<<<< HEAD
   DymensionxyzDymensionDymensionxyzDymensionRollapp: load(DymensionxyzDymensionDymensionxyzDymensionRollapp, 'dymensionxyz.dymension.rollapp'),
   DymensionxyzDymensionDymensionxyzDymensionSequencer: load(DymensionxyzDymensionDymensionxyzDymensionSequencer, 'dymensionxyz.dymension.sequencer'),
+=======
+>>>>>>> remove dymension module and re-add vue
   
 }
 
