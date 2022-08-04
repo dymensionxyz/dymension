d36c9c70f748a5827ed8a8f5fd349863d1ec343e
Lior Zilpa <liorzilp@gmail.com>
2022-08-04 12:55:39 +0300

    add basic validation logic and errors

--- x/rollapp/types/message_create_rollapp.go
+++ x/rollapp/types/message_create_rollapp.go
@@ -49,5 +49,31 @@ func (msg *MsgCreateRollapp) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+	if msg.GetMaxSequencers() == 0 {
+		return sdkerrors.Wrap(ErrInvalidwMaxSequencers, "max-sequencers must be greater than 0")
+	}
+	if msg.GetMaxWithholdingBlocks() == 0 {
+		return sdkerrors.Wrap(ErrInvalidwMaxWithholding, "max-withholding-blocks must be greater than 0")
+	}
+	// verifies that there's no duplicate address in PermissionedAddresses
+	// and addresses are in Bech32 format
+	permissionedAddresses := msg.GetPermissionedAddresses()
+	if permissionedAddresses.Size() > 0 {
+		duplicateAddresses := make(map[string]bool)
+		for _, item := range permissionedAddresses.GetAddresses() {
+			// check if the item/element exist in the duplicateAddresses map
+			_, exist := duplicateAddresses[item]
+			if exist {
+				return sdkerrors.Wrapf(ErrPermissionedAddressesDuplicate, "address: %s", item)
+			}
+			// check Bech32 format
+			if _, err := sdk.AccAddressFromBech32(item); err != nil {
+				return sdkerrors.Wrapf(ErrInvalidPermissionedAddress, "invalid permissioned address: %s", err)
+			}
+			// mark as exist
+			duplicateAddresses[item] = true
+		}
+	}
+
 	return nil
 }
