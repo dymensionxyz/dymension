3b83821eff6b90fdc722b8a06865eb15bb6c87a1
Lior Zilpa <liorzilp@gmail.com>
2022-08-31 20:34:28 +0300

    simulate create rollapp

--- x/rollapp/simulation/create_rollapp.go
+++ x/rollapp/simulation/create_rollapp.go
@@ -1,11 +1,13 @@
 package simulation
 
 import (
+	"fmt"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	sharedtypes "github.com/dymensionxyz/dymension/shared/types"
 	"github.com/dymensionxyz/dymension/x/rollapp/keeper"
 	"github.com/dymensionxyz/dymension/x/rollapp/types"
 )
@@ -17,13 +19,65 @@ func SimulateMsgCreateRollapp(
 ) simtypes.Operation {
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
-		simAccount, _ := simtypes.RandomAcc(r, accs)
-		msg := &types.MsgCreateRollapp{
-			Creator: simAccount.Address.String(),
+		// choose creator and rollappId
+		simAccount, rollappNumId := simtypes.RandomAcc(r, accs)
+		rollappId := "rollapp" + fmt.Sprint(rollappNumId)
+
+		// check if we already created it
+		bAlreadyExists := false
+		for _, item := range globalRollappIdList {
+			if item == rollappId {
+				bAlreadyExists = true
+			}
 		}
 
-		// TODO: Handling the CreateRollapp simulation
+		permissionedAddresses := []string{}
+		bPermissioned := r.Int()%2 == 0
+		bFailDuplicateSequencer := false
+		if bPermissioned {
+			for i := 0; i < r.Intn(len(accs)); i++ {
+				seqAccount, _ := simtypes.RandomAcc(r, accs)
+				for _, item := range permissionedAddresses {
+					if item == seqAccount.Address.String() {
+						bFailDuplicateSequencer = true
+					}
+				}
+				permissionedAddresses = append(permissionedAddresses, seqAccount.Address.String())
+			}
+		}
 
-		return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "CreateRollapp simulation not implemented"), nil, nil
+		// calculate maxSequencers and whether or not to fail the transaction
+		bFailMaxSequencers := r.Int()%2 == 0
+		maxSequencers := uint64(r.Intn(100)) + 1
+		if bFailMaxSequencers {
+			maxSequencers = 0
+		}
+		// calculate maxWithholdingBlocks and whether or not to fail the transaction
+		bFailMaxWithholdingBlocks := r.Int()%2 == 0
+		maxWithholdingBlocks := uint64(r.Intn(len(accs))) + 1
+		if bFailMaxWithholdingBlocks {
+			maxWithholdingBlocks = 0
+		}
+		msg := &types.MsgCreateRollapp{
+			Creator:              simAccount.Address.String(),
+			RollappId:            rollappId,
+			CodeStamp:            "",
+			GenesisPath:          "",
+			MaxWithholdingBlocks: maxWithholdingBlocks,
+			MaxSequencers:        maxSequencers,
+			PermissionedAddresses: sharedtypes.Sequencers{
+				Addresses: permissionedAddresses,
+			},
+		}
+
+		// fmt.Printf("SimulateMsgCreateRollapp: RollappId(%s) bFailMaxSequencers(%t) bFailMaxWithholdingBlocks(%t) bFailDuplicateSequencer(%t)\n",
+		// 	msg.RollappId, bFailMaxSequencers, bFailMaxWithholdingBlocks, bFailDuplicateSequencer)
+		bExpectedError := bFailMaxSequencers || bFailMaxWithholdingBlocks || bFailDuplicateSequencer || bAlreadyExists
+
+		if !bExpectedError {
+			globalRollappIdList = append(globalRollappIdList, msg.RollappId)
+		}
+
+		return GenAndDeliverMsgWithRandFees(msg, msg.Type(), r, app, &ctx, &simAccount, &bk, &ak, nil, bExpectedError)
 	}
 }
--- x/rollapp/simulation/simap.go
+++ x/rollapp/simulation/simap.go
@@ -1,9 +1,18 @@
 package simulation
 
 import (
+	"math/rand"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"github.com/dymensionxyz/dymension/x/rollapp/types"
 )
+
+// list of created rollapps
+var globalRollappIdList []string = []string{}
 
 // FindAccount find a specific address from an account list
 func FindAccount(accs []simtypes.Account, address string) (simtypes.Account, bool) {
@@ -12,4 +21,50 @@ func FindAccount(accs []simtypes.Account, address string) (simtypes.Account, boo
 		panic(err)
 	}
 	return simtypes.FindAccount(accs, creator)
+}
+
+func GenAndDeliverMsgWithRandFees(
+	msg sdk.Msg,
+	msgType string,
+	r *rand.Rand,
+	app *baseapp.BaseApp,
+	ctx *sdk.Context,
+	simAccount *simtypes.Account,
+	bk *types.BankKeeper,
+	ak *types.AccountKeeper,
+	futureOperation []simtypes.FutureOperation,
+	bExpectedError bool) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+
+	spendableCoins := (*bk).SpendableCoins(*ctx, simAccount.Address)
+
+	if spendableCoins.Empty() {
+		return simtypes.NoOpMsg(types.ModuleName, msgType, "unable to grant empty coins as SpendLimit"), nil, nil
+	}
+
+	txCtx := simulation.OperationInput{
+		R:               r,
+		App:             app,
+		TxGen:           simappparams.MakeTestEncodingConfig().TxConfig,
+		Cdc:             nil,
+		Msg:             msg,
+		MsgType:         msgType,
+		CoinsSpentInMsg: spendableCoins,
+		Context:         *ctx,
+		SimAccount:      *simAccount,
+		AccountKeeper:   *ak,
+		Bankkeeper:      *bk,
+		ModuleName:      types.ModuleName,
+	}
+
+	operationMsg, additionalFutureOperation, err := simulation.GenAndDeliverTxWithRandFees(txCtx)
+
+	futureOperation = append(futureOperation, additionalFutureOperation...)
+	if bExpectedError {
+		err = nil
+	} else {
+		if err != nil {
+			panic(err)
+		}
+	}
+	return operationMsg, futureOperation, err
 }
