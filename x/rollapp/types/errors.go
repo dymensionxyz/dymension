9cfb350c86bd792d3d247251ddcd5d4eb9d52765
liorzilp <liorzilp@gmail.com>
2022-09-06 18:27:13 +0300

    Integration (#78)
    * change state fields of BlockDescriptor to be byte array
    
    * feat(rollapp): rollapp state finalization (#74)
    
    * ignite scaffold map block_height_to_finalization_queue finalization_queue:StateInfoIndex --index finalization_height:uint --no-message --module rollapp
    
    * fix ignite command
    
    * update finalizationQueue on UpdateState
    
    * finalize states on EndBlocker
    
    * add tests
    
    * fix lint
    
    * fix lint 2
    
    * feat: simulation tests (#76)
    
    * simulate create rollapp
    
    * add simulation flag in app & keeper - used in sequencer simulation
    
    * add simulation for update state
    
    * change state fields of BlockDescriptor to be byte array
    
    * fix gas consumption in simulation

--- x/rollapp/types/errors.go
+++ x/rollapp/types/errors.go
@@ -9,8 +9,8 @@ import (
 // x/rollapp module sentinel errors
 var (
 	ErrRollappExists                  = sdkerrors.Register(ModuleName, 1000, "rollapp already exist for this rollapp-id; must use new rollapp-id")
-	ErrInvalidwMaxSequencers          = sdkerrors.Register(ModuleName, 1001, "invalid max sequencers")
-	ErrInvalidwMaxWithholding         = sdkerrors.Register(ModuleName, 1002, "invalid max withholding")
+	ErrInvalidMaxSequencers           = sdkerrors.Register(ModuleName, 1001, "invalid max sequencers")
+	ErrInvalidMaxWithholding          = sdkerrors.Register(ModuleName, 1002, "invalid max withholding")
 	ErrInvalidPermissionedAddress     = sdkerrors.Register(ModuleName, 1003, "invalid permissioned address")
 	ErrPermissionedAddressesDuplicate = sdkerrors.Register(ModuleName, 1004, "permissioned-address has duplicates")
 	ErrInvalidNumBlocks               = sdkerrors.Register(ModuleName, 1005, "invalid number of blocks")
@@ -19,4 +19,6 @@ var (
 	ErrVersionMismatch                = sdkerrors.Register(ModuleName, 1008, "rollapp version mismatch")
 	ErrWrongBlockHeight               = sdkerrors.Register(ModuleName, 1009, "start-height does not match rollapps state")
 	ErrMultiUpdateStateInBlock        = sdkerrors.Register(ModuleName, 1010, "only one state update can take place per block")
+	ErrInvalidStateRoot               = sdkerrors.Register(ModuleName, 1011, "invalid blocks state root")
+	ErrInvalidIntermediateStatesRoot  = sdkerrors.Register(ModuleName, 1012, "invalid blocks intermediate states root")
 )
