c1af3952d8d13744ab12c3cadc5eb62d6e192ed6
Itzhak Bokris <jzak300@gmail.com>
2022-09-12 12:43:28 +0300

    Update run-local script so all the endpoints hosts and ports would be editable (#85)

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-# .................................................... #
-# ...........Set and validate parameters.............. #
-# .................................................... #
+# Set parameters
 DATA_DIRECTORY="$HOME/.dymension"
 CONFIG_DIRECTORY="$DATA_DIRECTORY/config"
+TENDERMINT_CONFIG_FILE="$CONFIG_DIRECTORY/config.toml"
 CLIENT_CONFIG_FILE="$CONFIG_DIRECTORY/client.toml"
+APP_CONFIG_FILE="$CONFIG_DIRECTORY/app.toml"
 GENESIS_FILE="$CONFIG_DIRECTORY/genesis.json"
 CHAIN_ID=${CHAIN_ID:-"local-testnet"}
 MONIKER_NAME=${MONIKER_NAME:-"local"}
 KEY_NAME=${KEY_NAME:-"local-user"}
+SETTLEMENT_RPC=${SETTLEMENT_RPC:-"0.0.0.0:26657"}
+P2P_ADDRESS=${P2P_ADDRESS:-"0.0.0.0:26656"}
 
+# Validate dymension binary exists
 export PATH=$PATH:$HOME/go/bin
 if ! command -v dymd; then
   echo "dYmension binary not found, call \"make install\"."
   exit 1
 fi
 
-# ...................................................... #
-# ............... Init dymension chain ................. #
-# ...................................................... #
+# Create and init dymension chain if need
 if [ ! -f "$GENESIS_FILE" ]; then
   dymd init "$MONIKER_NAME" --chain-id="$CHAIN_ID"
   dymd tendermint unsafe-reset-all
 
+  sed -i'' -e "/\[rpc\]/,+3 s/laddr *= .*/laddr = \"tcp:\/\/$SETTLEMENT_RPC\"/" "$TENDERMINT_CONFIG_FILE"
+  sed -i'' -e "/\[p2p\]/,+3 s/laddr *= .*/laddr = \"tcp:\/\/$P2P_ADDRESS\"/" "$TENDERMINT_CONFIG_FILE"
   sed -i'' -e "s/^chain-id *= .*/chain-id = \"$CHAIN_ID\"/" "$CLIENT_CONFIG_FILE"
+  sed -i'' -e "s/^node *= .*/node = \"$SETTLEMENT_RPC\"/" "$CLIENT_CONFIG_FILE"
+  sed -i'' -e 's/^enable *= true/enable = false/' "$APP_CONFIG_FILE"
   sed -i'' -e 's/bond_denom": ".*"/bond_denom": "dym"/' "$GENESIS_FILE"
   sed -i'' -e 's/mint_denom": ".*"/mint_denom": "dym"/' "$GENESIS_FILE"
 
@@ -34,7 +39,5 @@ if [ ! -f "$GENESIS_FILE" ]; then
   dymd collect-gentxs
 fi
 
-# ...................................................... #
-# ................... Run the chain .................... #
-# ...................................................... #
+# Run the dymension chain
 dymd start
