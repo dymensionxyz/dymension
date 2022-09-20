73cea4d3cb1b949c421087791f8030bb05f53cc1
Itzhak Bokris <jzak300@gmail.com>
2022-09-20 16:39:38 +0300

    Change rootCmd to return dymension short description

--- cmd/dymd/main.go
+++ cmd/dymd/main.go
@@ -19,6 +19,7 @@ func main() {
 		app.New,
 		// this line is used by starport scaffolding # root/arguments
 	)
+	rootCmd.Short = "Start dYmension app"
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
