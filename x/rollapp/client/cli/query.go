df304b3a6a0f34b9bd8ee50a2b439b6925ae1719
Lior Zilpa <liorzilp@gmail.com>
2022-12-18 14:27:11 +0200

    ignite scaffold query latest_finalized_state_info rollapp-id -d "query the latest StateInfo that was finalized" --response state_info:StateInfo --module rollapp

--- x/rollapp/client/cli/query.go
+++ x/rollapp/client/cli/query.go
@@ -33,6 +33,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowLatestStateInfoIndex())
 	cmd.AddCommand(CmdListBlockHeightToFinalizationQueue())
 	cmd.AddCommand(CmdShowBlockHeightToFinalizationQueue())
+	cmd.AddCommand(CmdLatestFinalizedStateInfo())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
