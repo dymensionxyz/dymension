19d54c1ed004382d6a5492b1bd26644c387aefec
Lior Zilpa <liorzilp@gmail.com>
2022-12-26 12:43:09 +0200

    check max_rollapps on CreateRollapp transaction

--- x/rollapp/keeper/grpc_query_params_test.go
+++ x/rollapp/keeper/grpc_query_params_test.go
@@ -13,13 +13,14 @@ func TestParamsQuery(t *testing.T) {
 	keeper, ctx := testkeeper.RollappKeeper(t)
 	wctx := sdk.WrapSDKContext(ctx)
 	params := types.DefaultParams()
+	params.DeployerWhitelist = []types.DeployerParams{{Address: "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3", MaxRollapps: 0}, {Address: "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3", MaxRollapps: 100}}
 	keeper.SetParams(ctx, params)
 
 	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
 	require.NoError(t, err)
 	require.EqualValues(t, params.DisputePeriodInBlocks, response.Params.DisputePeriodInBlocks)
 	require.EqualValues(t, len(params.DeployerWhitelist), len(response.Params.DeployerWhitelist))
-	for i, _ := range params.DeployerWhitelist {
+	for i := range params.DeployerWhitelist {
 		require.EqualValues(t, params.DeployerWhitelist[i], response.Params.DeployerWhitelist[i])
 	}
 }
