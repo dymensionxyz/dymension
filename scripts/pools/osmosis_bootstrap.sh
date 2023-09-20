bb8608d2a70dafa7e94a9649fa2b6339fdae4ac1
Michael Tsitrin <michael@dymension.xyz>
2023-09-20 22:51:45 +0300

    added bootstrap commands

--- scripts/pools/osmosis_bootstrap.sh
+++ scripts/pools/osmosis_bootstrap.sh
@@ -6,7 +6,19 @@ create_asset_pool() {
 }
 
 
+join_to_pool() {
+    dymd tx gamm join-pool --pool-id 1 --share-amount-out 40227549469722224220 --from local-user --keyring-backend=test
+}
+
+exit_pool() {
+    dymd tx gamm exit-pool --pool-id=$1 --shares=$2 --from pools  --keyring-backend=test -b block --gas auto --yes
+}
+
+swap_tokens() {
+    # dymd tx gamm swap --exact-amount-in=$1 --exact-amount-out=$2 --from pools  --keyring-backend=test -b block --gas auto --yes
+    dymd tx gamm swap-exact-amount-in 5000uatom 5000 --swap-route-pool-ids 1 --swap-route-denoms uusd --from local-user --keyring-backend test
+}
+
 
 create_asset_pool "$(dirname "$0")/nativeDenomPoolA.json"
 create_asset_pool "$(dirname "$0")/nativeDenomPoolB.json"
-create_asset_pool "$(dirname "$0")/nativeDenomThreeAssetPool.json"
