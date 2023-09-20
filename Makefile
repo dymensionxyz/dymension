2498bd7ba7f2defd6f402fad9b6d70ccbc43d271
Michael Tsitrin <michael@dymension.xyz>
2023-09-20 23:05:47 +0300

    added proto generation script

--- Makefile
+++ Makefile
@@ -94,3 +94,15 @@ install: go.sum
 .PHONY: build
 build: go.sum
 	go build $(BUILD_FLAGS) -o build/dymd ./cmd/dymd
+
+
+
+protoVer=v0.8
+protoImageName=osmolabs/osmo-proto-gen:$(protoVer)
+containerProtoGen=cosmos-sdk-proto-gen-$(protoVer)
+containerProtoFmt=cosmos-sdk-proto-fmt-$(protoVer)
+
+proto-gen:
+	@echo "Generating Protobuf files"
+	@if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerProtoGen}$$"; then docker start -a $(containerProtoGen); else docker run --name $(containerProtoGen) -v $(CURDIR):/workspace --workdir /workspace $(protoImageName) \
+		sh ./scripts/protocgen.sh; fi
---
+++ scripts/protocgen.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# get protoc executions
+go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+
+
+echo "Generating gogo proto code"
+cd proto
+proto_dirs=$(find ../proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package $file &>/dev/null; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
+
+cd ..
+
+# move proto files to the right places
+#
+# Note: Proto files are suffixed with the current binary version.
+cp -r github.com/dymensionxyz/dymension/* ./
+
+rm -rf github.com
+
+go mod tidy -compat=1.18
+
+# TODO: Uncomment once ORM/Pulsar support is needed.
+#
+# Ref: https://github.com/osmosis-labs/osmosis/pull/1589
