97f54fb1655735549e2d5063f7804f94c9e4fa25
Michael Tsitrin <michael@dymension.xyz>
2023-10-11 09:54:23 +0300

    linter issues

--- x/lockup/keeper/lock.go
+++ x/lockup/keeper/lock.go
@@ -254,7 +254,7 @@ func (k Keeper) beginUnlock(ctx sdk.Context, lock types.PeriodLock, coins sdk.Co
 func (k Keeper) clearKeysByPrefix(ctx sdk.Context, prefix []byte) {
 	store := ctx.KVStore(k.storeKey)
 	iterator := sdk.KVStorePrefixIterator(store, prefix)
-	defer iterator.Close()
+	defer iterator.Close() // nolint: errcheck
 
 	for ; iterator.Valid(); iterator.Next() {
 		store.Delete(iterator.Key())
