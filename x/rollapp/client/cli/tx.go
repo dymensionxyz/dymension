866c03618c2f7a9fdbc5b7f5bde0cf684ed5adfc
omritoptix <omritoptix@gmail.com>
2023-10-18 15:42:21 +0200

    Removed unused vars.

--- x/rollapp/client/cli/tx.go
+++ x/rollapp/client/cli/tx.go
@@ -15,12 +15,6 @@ var (
 	DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
 )
 
-// nolint:deadcode,unused,varcheck
-const (
-	flagPacketTimeoutTimestamp = "packet-timeout-timestamp"
-	listSeparator              = ","
-)
-
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd() *cobra.Command {
 	cmd := &cobra.Command{
