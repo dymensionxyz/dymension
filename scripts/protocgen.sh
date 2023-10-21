a5ed0d0348c0bdca6607553085a40feab0338a68
omritoptix <omritoptix@gmail.com>
2023-10-21 20:28:25 +0200

    Added IBC go as depedency for proto file generation.

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -10,11 +10,12 @@ echo "Generating gogo proto code"
 cd proto
 proto_dirs=$(find ../proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package $file &>/dev/null; then
-      buf generate --template buf.gen.gogo.yaml $file
-    fi
-  done
+    for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+      if grep go_package $file &>/dev/null; then
+        echo "Generating gogo proto code for $file"
+        buf generate --template buf.gen.gogo.yaml $file
+      fi
+    done
 done
 
 cd ..
@@ -26,7 +27,7 @@ cp -r github.com/dymensionxyz/dymension/* ./
 
 rm -rf github.com
 
-go mod tidy -compat=1.18
+
 
 # TODO: Uncomment once ORM/Pulsar support is needed.
 #
