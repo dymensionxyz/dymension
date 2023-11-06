52b99564a526e0b1702a1c98b7330d8ef1fe7d43
Michael Tsitrin <michael@dymension.xyz>
2023-11-06 14:55:10 +0200

    delayed ack WIP

---
+++ ibctesting/delayed_ack_test.go
@@ -0,0 +1,420 @@
+package ibctesting_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+)
+
+//transfer from rollapp to hub - delay
+//transfer from rollapp to hub - check delay is finialized eventually
+// timeout??
+
+func (suite *KeeperTestSuite) TestTransferCosmosToHub() {
+	// setup between cosmosChain and hubChain
+	path := suite.NewTransferPath(suite.cosmosChain, suite.hubChain)
+	suite.coordinator.Setup(path)
+
+	//	originalBalance := suite.cosmosChain.GetSimApp().BankKeeper.GetBalance(suite.cosmosChain.GetContext(), suite.cosmosChain.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
+	timeoutHeight := clienttypes.NewHeight(100, 110)
+
+	amount, ok := sdk.NewIntFromString("9223372036854775808") // 2^63 (one above int64)
+	suite.Require().True(ok)
+	coinToSendToB := sdk.NewCoin(sdk.DefaultBondDenom, amount)
+
+	// send from cosmosChain to hubChain
+	// msg := types.NewMsgTransfer(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, coinToSendToB, suite.hubChain.SenderAccount.GetAddress().String(), suite.cosmosChain.SenderAccount.GetAddress().String(), timeoutHeight, 0, "")
+	msg := types.NewMsgTransfer(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, coinToSendToB, suite.cosmosChain.SenderAccount.GetAddress().String(), suite.hubChain.SenderAccount.GetAddress().String(), timeoutHeight, 0, "")
+	res, err := suite.cosmosChain.SendMsgs(msg)
+	suite.Require().NoError(err) // message committed
+
+	packet, err := ibctesting.ParsePacketFromEvents(res.GetEvents())
+	suite.Require().NoError(err)
+
+	// relay send
+	err = path.RelayPacket(packet)
+	suite.Require().NoError(err) // relay committed
+}
+
+func (suite *KeeperTestSuite) TestTransferRollappToHub() {
+	// setup between cosmosChain and hubChain
+	path := suite.NewTransferPath(suite.rollappChain, suite.hubChain)
+	suite.coordinator.Setup(path)
+
+	//TODO: register rollapp
+
+	timeoutHeight := clienttypes.NewHeight(100, 110)
+	amount, ok := sdk.NewIntFromString("9223372036854775808") // 2^63 (one above int64)
+	suite.Require().True(ok)
+	coinToSendToB := sdk.NewCoin(sdk.DefaultBondDenom, amount)
+
+	msg := types.NewMsgTransfer(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, coinToSendToB, suite.rollappChain.SenderAccount.GetAddress().String(), suite.hubChain.SenderAccount.GetAddress().String(), timeoutHeight, 0, "")
+	res, err := suite.hubChain.SendMsgs(msg)
+	suite.Require().NoError(err) // message committed
+
+	packet, err := ibctesting.ParsePacketFromEvents(res.GetEvents())
+	suite.Require().NoError(err)
+
+	// relay send
+	err = path.RelayPacket(packet)
+	suite.Require().Error(err) // relay committed
+}
+
+func (suite *KeeperTestSuite) TestTransfer() {
+	path := suite.NewTransferPath(suite.hubChain, suite.cosmosChain) // clientID, connectionID, channelID empty
+	suite.coordinator.Setup(path)                                    // clientID, connectionID, channelID filled
+
+	suite.Require().Equal("07-tendermint-0", path.EndpointA.ClientID)
+	suite.Require().Equal("connection-0", path.EndpointA.ConnectionID)
+	suite.Require().Equal("channel-0", path.EndpointA.ChannelID)
+
+	// // send on endpointA
+	// sequence, err := path.EndpointA.SendPacket(timeoutHeight1, timeoutTimestamp1, packet1Data)
+
+	// // create packet 1
+	// packet1 := NewPacket() // NewPacket would construct your packet
+
+	// // receive on endpointB
+	// path.EndpointB.RecvPacket(packet1)
+
+	// // acknowledge the receipt of the packet
+	// path.EndpointA.AcknowledgePacket(packet1, ack)
+
+	// // we can also relay
+	// sequence, err := path.EndpointA.SendPacket(timeoutHeight2, timeoutTimestamp2, packet2Data)
+
+	// packet2 := NewPacket()
+
+	// path.Relay(packet2, expectedAck)
+
+	// // if needed we can update our clients
+	// path.EndpointB.UpdateClient()
+
+}
+
+/*
+
+
+
+
+func (suite *InterchainQueriesTestSuite) TestOnChanOpenInit() {
+	var (
+		channel      *channeltypes.Channel
+		path         *ibctesting.Path
+		chanCap      *capabilitytypes.Capability
+		counterparty channeltypes.Counterparty
+	)
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success", func() {}, true,
+		},
+		{
+			"empty version string", func() {
+				channel.Version = ""
+			}, true,
+		},
+		{
+			"invalid order - ORDERED", func() {
+				channel.Ordering = channeltypes.ORDERED
+			}, false,
+		},
+		{
+			"invalid port ID", func() {
+				path.EndpointA.ChannelConfig.PortID = ibctesting.MockPort
+			}, false,
+		},
+		{
+			"invalid version", func() {
+				channel.Version = version
+			}, false,
+		},
+		{
+			"capability already claimed", func() {
+				err := simapp.GetSimApp(suite.cosmosChain).ScopedICQKeeper.ClaimCapability(suite.cosmosChain.GetContext(), chanCap, host.ChannelCapabilityPath(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID))
+				suite.Require().NoError(err)
+			}, false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+			path = NewICQPath(suite.cosmosChain, suite.hubChain)
+			suite.coordinator.SetupConnections(path)
+			path.EndpointA.ChannelID = ibctesting.FirstChannelID
+
+			counterparty = channeltypes.NewCounterparty(path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID)
+			channel = &channeltypes.Channel{
+				State:          channeltypes.INIT,
+				Ordering:       channeltypes.UNORDERED,
+				Counterparty:   counterparty,
+				ConnectionHops: []string{path.EndpointA.ConnectionID},
+				Version:        types.Version,
+			}
+
+			var err error
+			chanCap, err = suite.cosmosChain.App.GetScopedIBCKeeper().NewCapability(suite.cosmosChain.GetContext(), host.ChannelCapabilityPath(types.PortID, path.EndpointA.ChannelID))
+			suite.Require().NoError(err)
+
+			tc.malleate() // explicitly change fields in channel and testChannel
+
+			icqModule := icq.NewIBCModule(simapp.GetSimApp(suite.cosmosChain).ICQKeeper)
+			version, err := icqModule.OnChanOpenInit(suite.cosmosChain.GetContext(), channel.Ordering, channel.GetConnectionHops(),
+				path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, chanCap, counterparty, channel.GetVersion(),
+			)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().Equal(types.Version, version)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Equal(version, "")
+			}
+		})
+	}
+}
+
+func (suite *InterchainQueriesTestSuite) TestOnChanOpenTry() {
+	var (
+		channel             *channeltypes.Channel
+		chanCap             *capabilitytypes.Capability
+		path                *ibctesting.Path
+		counterparty        channeltypes.Counterparty
+		counterpartyVersion string
+	)
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success", func() {}, true,
+		},
+		{
+			"invalid order - ORDERED", func() {
+				channel.Ordering = channeltypes.ORDERED
+			}, false,
+		},
+		{
+			"invalid port ID", func() {
+				path.EndpointA.ChannelConfig.PortID = ibctesting.MockPort
+			}, false,
+		},
+		{
+			"invalid counterparty version", func() {
+				counterpartyVersion = version
+			}, false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+
+			path = NewICQPath(suite.cosmosChain, suite.hubChain)
+			suite.coordinator.SetupConnections(path)
+			path.EndpointA.ChannelID = ibctesting.FirstChannelID
+
+			counterparty = channeltypes.NewCounterparty(path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID)
+			channel = &channeltypes.Channel{
+				State:          channeltypes.TRYOPEN,
+				Ordering:       channeltypes.UNORDERED,
+				Counterparty:   counterparty,
+				ConnectionHops: []string{path.EndpointA.ConnectionID},
+				Version:        types.Version,
+			}
+			counterpartyVersion = types.Version
+
+			module, _, err := suite.cosmosChain.App.GetIBCKeeper().PortKeeper.LookupModuleByPort(suite.cosmosChain.GetContext(), types.PortID)
+			suite.Require().NoError(err)
+
+			chanCap, err = suite.cosmosChain.App.GetScopedIBCKeeper().NewCapability(suite.cosmosChain.GetContext(), host.ChannelCapabilityPath(types.PortID, path.EndpointA.ChannelID))
+			suite.Require().NoError(err)
+
+			cbs, ok := suite.cosmosChain.App.GetIBCKeeper().Router.GetRoute(module)
+			suite.Require().True(ok)
+
+			tc.malleate() // explicitly change fields in channel and testChannel
+
+			version, err := cbs.OnChanOpenTry(suite.cosmosChain.GetContext(), channel.Ordering, channel.GetConnectionHops(),
+				path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, chanCap, channel.Counterparty, counterpartyVersion,
+			)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().Equal(types.Version, version)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Equal("", version)
+			}
+		})
+	}
+}
+
+func (suite *InterchainQueriesTestSuite) TestOnChanOpenAck() {
+	var counterpartyVersion string
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success", func() {}, true,
+		},
+		{
+			"invalid counterparty version", func() {
+				counterpartyVersion = version
+			}, false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+
+			path := NewICQPath(suite.cosmosChain, suite.hubChain)
+			suite.coordinator.SetupConnections(path)
+			path.EndpointA.ChannelID = ibctesting.FirstChannelID
+			counterpartyVersion = types.Version
+
+			module, _, err := suite.cosmosChain.App.GetIBCKeeper().PortKeeper.LookupModuleByPort(suite.cosmosChain.GetContext(), types.PortID)
+			suite.Require().NoError(err)
+
+			cbs, ok := suite.cosmosChain.App.GetIBCKeeper().Router.GetRoute(module)
+			suite.Require().True(ok)
+
+			tc.malleate() // explicitly change fields in channel and testChannel
+
+			err = cbs.OnChanOpenAck(suite.cosmosChain.GetContext(), path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointA.Counterparty.ChannelID, counterpartyVersion)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *InterchainQueriesTestSuite) TestOnAcknowledgementPacket() {
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"icq OnAcknowledgementPacket fails with ErrInvalidChannelFlow", func() {}, false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+
+			path := NewICQPath(suite.cosmosChain, suite.hubChain)
+			suite.coordinator.SetupConnections(path)
+
+			err := SetupICQPath(path)
+			suite.Require().NoError(err)
+
+			tc.malleate() // malleate mutates test data
+
+			module, _, err := suite.hubChain.App.GetIBCKeeper().PortKeeper.LookupModuleByPort(suite.hubChain.GetContext(), path.EndpointB.ChannelConfig.PortID)
+			suite.Require().NoError(err)
+
+			cbs, ok := suite.hubChain.App.GetIBCKeeper().Router.GetRoute(module)
+			suite.Require().True(ok)
+
+			packet := channeltypes.NewPacket(
+				[]byte("empty packet data"),
+				suite.cosmosChain.SenderAccount.GetSequence(),
+				path.EndpointB.ChannelConfig.PortID,
+				path.EndpointB.ChannelID,
+				path.EndpointA.ChannelConfig.PortID,
+				path.EndpointA.ChannelID,
+				clienttypes.NewHeight(0, 100),
+				0,
+			)
+
+			err = cbs.OnAcknowledgementPacket(suite.hubChain.GetContext(), packet, []byte("ackBytes"), nil)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *InterchainQueriesTestSuite) TestOnTimeoutPacket() {
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"icq OnTimeoutPacket fails with ErrInvalidChannelFlow", func() {}, false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest() // reset
+
+			path := NewICQPath(suite.cosmosChain, suite.hubChain)
+			suite.coordinator.SetupConnections(path)
+
+			err := SetupICQPath(path)
+			suite.Require().NoError(err)
+
+			tc.malleate() // malleate mutates test data
+
+			module, _, err := suite.cosmosChain.App.GetIBCKeeper().PortKeeper.LookupModuleByPort(suite.cosmosChain.GetContext(), path.EndpointB.ChannelConfig.PortID)
+			suite.Require().NoError(err)
+
+			cbs, ok := suite.cosmosChain.App.GetIBCKeeper().Router.GetRoute(module)
+			suite.Require().True(ok)
+
+			packet := channeltypes.NewPacket(
+				[]byte("empty packet data"),
+				suite.cosmosChain.SenderAccount.GetSequence(),
+				path.EndpointB.ChannelConfig.PortID,
+				path.EndpointB.ChannelID,
+				path.EndpointA.ChannelConfig.PortID,
+				path.EndpointA.ChannelID,
+				clienttypes.NewHeight(0, 100),
+				0,
+			)
+
+			err = cbs.OnTimeoutPacket(suite.cosmosChain.GetContext(), packet, nil)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+*/
