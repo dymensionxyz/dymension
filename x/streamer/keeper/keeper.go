336b38a6af17d90f738f8af7b0033fb08a313485
Michael Tsitrin <michael@dymension.xyz>
2023-11-22 13:57:33 +0200

    implemented keeper handlers

--- x/streamer/keeper/keeper.go
+++ x/streamer/keeper/keeper.go
@@ -48,9 +48,13 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 }
 
 // CreateStream creates a stream and sends coins to the stream.
-func (k Keeper) CreateStream(ctx sdk.Context, coins sdk.Coins, distrTo *types.DistrInfo, startTime time.Time, epochIdentifier string, numEpochsPaidOver uint64) (uint64, error) {
+func (k Keeper) CreateStream(ctx sdk.Context, coins sdk.Coins, distrInfo *types.DistrInfo, startTime time.Time, epochIdentifier string, numEpochsPaidOver uint64) (uint64, error) {
 	if !coins.IsAllPositive() {
 		return 0, fmt.Errorf("all coins %s must be positive", coins)
+	}
+
+	if err := distrInfo.Validate(); err != nil {
+		return 0, err
 	}
 
 	//TODO: it's better to check only the denoms of the requested coins. No need to itereate entire balance.
@@ -69,13 +73,9 @@ func (k Keeper) CreateStream(ctx sdk.Context, coins sdk.Coins, distrTo *types.Di
 		return 0, fmt.Errorf("numEpochsPaidOver must be greater than 0")
 	}
 
-	if err := distrTo.Validate(); err != nil {
-		return 0, err
-	}
-
 	stream := types.NewStream(
 		k.GetLastStreamID(ctx)+1,
-		distrTo,
+		distrInfo,
 		coins.Sort(),
 		startTime,
 		epochIdentifier,
---
+++ x/streamer/keeper/keeper_replace_update_distribution_test.go
@@ -0,0 +1,265 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dymensionxyz/dymension/x/streamer/types"
+)
+
+func (suite *KeeperTestSuite) TestReplaceDistrRecords() {
+	initialWeight := defaultDistrInfo.TotalWeight
+
+	tests := []struct {
+		name               string
+		testingDistrRecord []types.DistrRecord
+		expectErr          bool
+		expectTotalWeight  sdk.Int
+		streamId           uint64
+	}{
+		{
+			name: "happy flow - same gauges with different weights",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(10),
+				},
+				{
+					GaugeId: 2,
+					Weight:  sdk.NewInt(20),
+				},
+			},
+			expectErr:         false,
+			expectTotalWeight: sdk.NewInt(30),
+		},
+		{
+			name:               "happy flow - same gauges with same weights",
+			testingDistrRecord: defaultDistrInfo.Records,
+			expectErr:          false,
+			expectTotalWeight:  initialWeight,
+		},
+		{
+			name: "happy flow - changing gauges",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 2,
+					Weight:  sdk.NewInt(10),
+				},
+				{
+					GaugeId: 3,
+					Weight:  sdk.NewInt(20),
+				},
+			},
+			expectErr:         false,
+			expectTotalWeight: sdk.NewInt(30),
+		},
+		{
+			name:               "Not existent stream.",
+			testingDistrRecord: defaultDistrInfo.Records,
+			expectErr:          true,
+			streamId:           12,
+		},
+		{
+			name: "Not existent gauge.",
+			testingDistrRecord: []types.DistrRecord{{
+				GaugeId: 12,
+				Weight:  sdk.NewInt(100),
+			}},
+			expectErr: true,
+		},
+		{
+			name: "Adding two of the same gauge id at once should error",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(100),
+				},
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(200),
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "Adding unsort gauges at once should error",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 2,
+					Weight:  sdk.NewInt(200),
+				},
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(250),
+				},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		suite.Run(test.name, func() {
+			suite.SetupTest()
+
+			err := suite.CreateGauge()
+			suite.Require().NoError(err)
+			err = suite.CreateGauge()
+			suite.Require().NoError(err)
+			err = suite.CreateGauge()
+			suite.Require().NoError(err)
+
+			id, _ := suite.CreateDefaultStream(sdk.NewCoins(sdk.NewInt64Coin("udym", 100000)))
+			if test.streamId != 0 {
+				id = test.streamId
+			}
+
+			err = suite.App.StreamerKeeper.ReplaceDistrRecords(suite.Ctx, id, test.testingDistrRecord)
+			if test.expectErr {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+				stream, err := suite.App.StreamerKeeper.GetStreamByID(suite.Ctx, id)
+				suite.Require().NoError(err)
+
+				suite.Require().Equal(len(test.testingDistrRecord), len(stream.DistributeTo.Records))
+				suite.Require().Equal(test.expectTotalWeight, stream.DistributeTo.TotalWeight)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestUpdateDistrRecords() {
+	initialWeight := defaultDistrInfo.TotalWeight
+
+	tests := []struct {
+		name               string
+		testingDistrRecord []types.DistrRecord
+		expectErr          bool
+		expectTotalWeight  sdk.Int
+		streamId           uint64
+	}{
+		{
+			name: "happy flow - same gauges with different weights",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(10),
+				},
+				{
+					GaugeId: 2,
+					Weight:  sdk.NewInt(20),
+				},
+			},
+			expectErr:         false,
+			expectTotalWeight: sdk.NewInt(30),
+		},
+		{
+			name: "happy flow - remove gauge and add new one",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(0),
+				},
+				{
+					GaugeId: 3,
+					Weight:  sdk.NewInt(30),
+				},
+			},
+			expectErr:         false,
+			expectTotalWeight: sdk.NewInt(80),
+		},
+		{
+			name:               "happy flow - same gauges with same weights",
+			testingDistrRecord: defaultDistrInfo.Records,
+			expectErr:          false,
+			expectTotalWeight:  initialWeight,
+		},
+		{
+			name: "happy flow - changing gauges",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(0),
+				},
+				{
+					GaugeId: 2,
+					Weight:  sdk.NewInt(0),
+				},
+				{
+					GaugeId: 3,
+					Weight:  sdk.NewInt(20),
+				},
+			},
+			expectErr:         false,
+			expectTotalWeight: sdk.NewInt(20),
+		},
+		{
+			name:               "Not existent stream.",
+			testingDistrRecord: defaultDistrInfo.Records,
+			expectErr:          true,
+			streamId:           12,
+		},
+		{
+			name: "Not existent gauge.",
+			testingDistrRecord: []types.DistrRecord{{
+				GaugeId: 12,
+				Weight:  sdk.NewInt(100),
+			}},
+			expectErr: true,
+		},
+		{
+			name: "Adding two of the same gauge id at once should error",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(100),
+				},
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(200),
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "Adding unsort gauges at once should error",
+			testingDistrRecord: []types.DistrRecord{
+				{
+					GaugeId: 2,
+					Weight:  sdk.NewInt(200),
+				},
+				{
+					GaugeId: 1,
+					Weight:  sdk.NewInt(250),
+				},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		suite.Run(test.name, func() {
+			suite.SetupTest()
+
+			err := suite.CreateGauge()
+			suite.Require().NoError(err)
+			err = suite.CreateGauge()
+			suite.Require().NoError(err)
+			err = suite.CreateGauge()
+			suite.Require().NoError(err)
+
+			id, _ := suite.CreateDefaultStream(sdk.NewCoins(sdk.NewInt64Coin("udym", 100000)))
+			if test.streamId != 0 {
+				id = test.streamId
+			}
+
+			err = suite.App.StreamerKeeper.UpdateDistrRecords(suite.Ctx, id, test.testingDistrRecord)
+			if test.expectErr {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+				stream, err := suite.App.StreamerKeeper.GetStreamByID(suite.Ctx, id)
+				suite.Require().NoError(err)
+				suite.Require().Equal(test.expectTotalWeight, stream.DistributeTo.TotalWeight)
+			}
+		})
+	}
+}
