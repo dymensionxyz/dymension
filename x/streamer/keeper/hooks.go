9317727b036737bbdb431c7ac9c63653033483fc
Michael Tsitrin <michael@dymension.xyz>
2023-11-22 12:07:34 +0200

    added UT for stopStream

--- x/streamer/keeper/hooks.go
+++ x/streamer/keeper/hooks.go
@@ -16,7 +16,7 @@ func (k Keeper) BeforeEpochStart(ctx sdk.Context, epochIdentifier string, epochN
 // AfterEpochEnd is the epoch end hook.
 func (k Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, epochNumber int64) error {
 	streams := k.GetUpcomingStreams(ctx)
-	// move to active if it's correct epoch and start time reached
+	// move to active if start time reached
 	for _, stream := range streams {
 		if !ctx.BlockTime().Before(stream.StartTime) {
 			if err := k.moveUpcomingStreamToActiveStream(ctx, stream); err != nil {
