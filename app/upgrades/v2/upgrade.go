326c23622b8297b905e67f96a9323fa3d839846d
omritoptix <omritoptix@gmail.com>
2023-12-03 18:13:54 +0100

    Seperated the upgrade logic to different functions.

--- app/upgrades/v2/upgrade.go
+++ app/upgrades/v2/upgrade.go
@@ -3,6 +3,8 @@ package rc
 import (
 	"fmt"
 
+	"github.com/tendermint/tendermint/libs/log"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -29,85 +31,18 @@ func CreateUpgradeHandler(
 
 ) upgradetypes.UpgradeHandler {
 
-	// Create a map from client id to status where the statuses will be saved
-	clientStatuses := make(map[string]exported.Status)
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 
 		logger := ctx.Logger().With("upgrade", UpgradeName)
 
-		// Iterate over all clients and change their client state to Tendermint client state
-		clientKeeper.IterateClients(ctx, func(clientID string, clientState exported.ClientState) bool {
-			logger.Info("Trying to upgrade client", "clientID", clientID, "clientState", clientState)
-			dmClientState, ok := clientState.(*dmtypes.ClientState)
-			if !ok {
-				logger.Info("Not a Dymint client state. Skipping", "clientID", clientID, "clientState", clientState)
-				return false
-			}
-			logger.Info("Found dymint state. Upgrading client", "clientID", clientID, "clientState", clientState)
-			prevStatus := dmClientState.Status(ctx, clientKeeper.ClientStore(ctx, clientID), cdc)
-			clientStatuses[clientID] = prevStatus
-			newClientState := tmtypes.NewClientState(
-				dmClientState.ChainId,
-				tmtypes.DefaultTrustLevel,
-				dmClientState.TrustingPeriod,
-				dmClientState.UnbondingPeriod,
-				dmClientState.MaxClockDrift,
-				dmClientState.LatestHeight,
-				dmClientState.GetProofSpecs(),
-				dmClientState.UpgradePath,
-				true,
-				true,
-			)
-			clientKeeper.SetClientState(ctx, clientID, newClientState)
+		// Create a map from client id to status where the statuses will be saved
+		clientStatuses := updateClientStates(ctx, clientKeeper, logger, cdc)
 
-			return false
-		})
+		// Update the consensus states
+		updateConsensusStates(ctx, clientKeeper, logger)
 
-		logger.Info("Upgraded all clients Successfully. Length", "length", len(clientStatuses))
-
-		// Iterate over all consensus states and change them to Tendermint consensus state
-		clientKeeper.IterateConsensusStates(ctx, func(clientID string, consensusStateWithHeight clienttypes.ConsensusStateWithHeight) bool {
-			// Get the consensus state
-			logger.Info("Trying to upgrade consensus state", "clientID", clientID)
-			exportedConsensusState, found := clientKeeper.GetClientConsensusState(ctx, clientID, consensusStateWithHeight.Height)
-			dmConsensusState, ok := exportedConsensusState.(*dmtypes.ConsensusState)
-			if !ok {
-				logger.Info("Not a Dymint consensus state. Skipping", "clientID", clientID)
-				return false
-			}
-			if !found {
-				logger.Info("Consensus state not found. Skipping", "clientID", clientID)
-				return false
-			}
-			// Convert to Tendermint consensus state
-			tmConsensusState := &tmtypes.ConsensusState{
-				Timestamp:          dmConsensusState.Timestamp,
-				Root:               dmConsensusState.Root,
-				NextValidatorsHash: dmConsensusState.NextValidatorsHash,
-			}
-
-			clientKeeper.SetClientConsensusState(ctx, clientID, consensusStateWithHeight.Height, tmConsensusState)
-			logger.Info("Upgraded consensus state", "clientID", clientID, "consensusStateHeight", consensusStateWithHeight.Height)
-			return false
-		})
-
-		// Iterate over all clients and change their client state to Tendermint client state
-		clientKeeper.IterateClients(ctx, func(clientID string, clientState exported.ClientState) bool {
-			// Check the status of the client is active after the upgrade
-			logger.Info("Checking client status after upgrade", "clientID", clientID)
-			status := clientState.Status(ctx, clientKeeper.ClientStore(ctx, clientID), cdc)
-			if _, ok := clientStatuses[clientID]; ok {
-				if status != clientStatuses[clientID] {
-					msg := fmt.Sprintf("client status has changed after upgrade. Expected: %s, got: %s", clientStatuses[clientID], status)
-					panic(msg)
-				}
-				return false
-			} else {
-				panic(fmt.Sprintf("client status not found for clientID: %s", clientID))
-			}
-		})
-
-		logger.Info("Upgraded all consensus states successfully")
+		// Verify the client statuses are the same after the upgrade
+		verifyClientStatus(ctx, clientKeeper, clientStatuses, logger, cdc)
 
 		// Update the denom metadata for DYM token
 		bankKeeper.SetDenomMetaData(ctx, DYMTokenMetata)
@@ -127,6 +62,94 @@ func CreateUpgradeHandler(
 		logger.Debug("running module migrations ...")
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
+}
+
+// updateClientState updates the client state from Dymint client state to Tendermint client state
+func updateClientStates(ctx sdk.Context, clientKeeper clientkeeper.Keeper, logger log.Logger, cdc codec.BinaryCodec) map[string]exported.Status {
+	// Create a map from client id to status where the statuses will be saved
+	clientStatuses := make(map[string]exported.Status)
+
+	// Iterate over all clients and change their client state to Tendermint client state
+	clientKeeper.IterateClients(ctx, func(clientID string, clientState exported.ClientState) bool {
+		logger.Info("Trying to upgrade client", "clientID", clientID, "clientState", clientState)
+		dmClientState, ok := clientState.(*dmtypes.ClientState)
+		if !ok {
+			logger.Info("Not a Dymint client state. Skipping", "clientID", clientID, "clientState", clientState)
+			return false
+		}
+		logger.Info("Found dymint state. Upgrading client", "clientID", clientID, "clientState", clientState)
+		prevStatus := dmClientState.Status(ctx, clientKeeper.ClientStore(ctx, clientID), cdc)
+		clientStatuses[clientID] = prevStatus
+		newClientState := tmtypes.NewClientState(
+			dmClientState.ChainId,
+			tmtypes.DefaultTrustLevel,
+			dmClientState.TrustingPeriod,
+			dmClientState.UnbondingPeriod,
+			dmClientState.MaxClockDrift,
+			dmClientState.LatestHeight,
+			dmClientState.GetProofSpecs(),
+			dmClientState.UpgradePath,
+			true,
+			true,
+		)
+		clientKeeper.SetClientState(ctx, clientID, newClientState)
+
+		return false
+	})
+
+	logger.Info("Upgraded all clients Successfully")
+	return clientStatuses
+}
+
+func updateConsensusStates(ctx sdk.Context, clientKeeper clientkeeper.Keeper, logger log.Logger) {
+	// Iterate over all consensus states and change them to Tendermint consensus state
+	clientKeeper.IterateConsensusStates(ctx, func(clientID string, consensusStateWithHeight clienttypes.ConsensusStateWithHeight) bool {
+		// Get the consensus state
+		logger.Info("Trying to upgrade consensus state", "clientID", clientID)
+		exportedConsensusState, found := clientKeeper.GetClientConsensusState(ctx, clientID, consensusStateWithHeight.Height)
+		dmConsensusState, ok := exportedConsensusState.(*dmtypes.ConsensusState)
+		if !ok {
+			logger.Info("Not a Dymint consensus state. Skipping", "clientID", clientID)
+			return false
+		}
+		if !found {
+			logger.Info("Consensus state not found. Skipping", "clientID", clientID)
+			return false
+		}
+		// Convert to Tendermint consensus state
+		tmConsensusState := &tmtypes.ConsensusState{
+			Timestamp:          dmConsensusState.Timestamp,
+			Root:               dmConsensusState.Root,
+			NextValidatorsHash: dmConsensusState.NextValidatorsHash,
+		}
+
+		clientKeeper.SetClientConsensusState(ctx, clientID, consensusStateWithHeight.Height, tmConsensusState)
+		logger.Info("Upgraded consensus state", "clientID", clientID, "consensusStateHeight", consensusStateWithHeight.Height)
+		return false
+	})
+
+	logger.Info("Upgraded all consensus states successfully")
+
+}
+
+func verifyClientStatus(ctx sdk.Context, clientKeeper clientkeeper.Keeper, clientStatuses map[string]exported.Status, logger log.Logger, cdc codec.BinaryCodec) {
+	// Iterate over all clients and change their client state to Tendermint client state
+	clientKeeper.IterateClients(ctx, func(clientID string, clientState exported.ClientState) bool {
+		// Check the status of the client is active after the upgrade
+		logger.Info("Checking client status after upgrade", "clientID", clientID)
+		status := clientState.Status(ctx, clientKeeper.ClientStore(ctx, clientID), cdc)
+		if _, ok := clientStatuses[clientID]; ok {
+			if status != clientStatuses[clientID] {
+				msg := fmt.Sprintf("client status has changed after upgrade. Expected: %s, got: %s", clientStatuses[clientID], status)
+				panic(msg)
+			}
+			return false
+		} else {
+			panic(fmt.Sprintf("client status not found for clientID: %s", clientID))
+		}
+	})
+
+	logger.Info("Client status verification passed successfully")
 }
 
 func GetStoreUpgrades() *storetypes.StoreUpgrades {
