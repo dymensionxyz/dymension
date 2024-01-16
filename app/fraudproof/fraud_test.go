84b2f344e8b5750cbe0a7594d3f796b8a16ac55f
Michael Tsitrin <michael@dymension.xyz>
2024-01-16 11:40:09 +0200

    local manual UT

---
+++ app/fraudproof/fraud_test.go
@@ -0,0 +1,10 @@
+package fraudproof_test
+
+// func TestFraud(t *testing.T) {
+// 	app := app.Setup(t, false)
+// 	ctx := app.GetBaseApp().NewContext(false, tmproto.Header{})
+
+// 	reqFP := abci.RequestGenerateFraudProof{}
+// 	app.GenerateFraudProof()
+
+// }
