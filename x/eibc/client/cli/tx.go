a8b3cbd9d89f84bfa36aacdefc408be933b480b2
omritoptix <omritoptix@gmail.com>
2024-01-21 16:18:24 +0100

    Added cli command for fulfilling demand orders.

--- x/eibc/client/cli/tx.go
+++ x/eibc/client/cli/tx.go
@@ -7,7 +7,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	// "github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/dymensionxyz/dymension/x/eibc/types"
 )
 
@@ -25,7 +26,37 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(NewFullfilOrderTxCmd())
+
+	return cmd
+}
+
+func NewFullfilOrderTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "fulfill-order [order-id]",
+		Short:   "Fullfil a new eibc order",
+		Example: "dymd tx eibc fulfill-order <order-id>",
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			orderId := args[0]
+
+			msg := types.NewMsgFullfillOrder(
+				clientCtx.GetFromAddress().String(),
+				orderId,
+			)
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
 }
