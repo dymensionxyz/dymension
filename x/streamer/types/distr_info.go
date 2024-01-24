78c55b8ef5d67eabc1f68506f2bf225342013053
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2024-01-24 11:10:46 +0200

    fix: minor audit fixes (#501)
    Co-authored-by: omritoptix <omritoptix@gmail.com>

--- x/streamer/types/distr_info.go
+++ x/streamer/types/distr_info.go
@@ -18,19 +18,11 @@ func NewDistrInfo(records []DistrRecord) (*DistrInfo, error) {
 	distrInfo.Records = records
 	distrInfo.TotalWeight = totalWeight
 
+	if !totalWeight.IsPositive() {
+		return nil, ErrDistrInfoNotPositiveWeight
+	}
+
 	return &distrInfo, nil
-}
-
-func (d DistrInfo) Validate() error {
-	totalWeight := sdk.NewInt(0)
-	for _, record := range d.Records {
-		totalWeight = totalWeight.Add(record.Weight)
-	}
-
-	if !totalWeight.Equal(d.TotalWeight) {
-		return ErrDistrInfoTotalWeightNotEqual
-	}
-	return nil
 }
 
 // ValidateBasic is a basic validation test on recordd distribution gauges' weights.
