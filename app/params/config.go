12c0ba356f6c705aefd917572af7ef3c62d61b58
Michael Tsitrin <michael@dymension.xyz>
2024-01-24 12:07:22 +0200

    eip712 ante handler uses txfees for fee deduction

--- app/params/config.go
+++ app/params/config.go
@@ -10,7 +10,7 @@ const (
 	// DisplayDenom is the denomination used to display the amount of tokens held
 	DisplayDenom = "dym"
 	// BaseDenomUnit is the base denom unit for the Hub
-	BaseDenom = "udym"
+	BaseDenom = "adym"
 
 	// BaseDenomUnit defines the base denomination unit for Photons.
 	// 1 DYM = 1x10^{BaseDenomUnit} adym
