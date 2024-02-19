a1eaefe2651aef8721affda6f6117b9dc7fb2d1e
Sergi Rene <sergi@dymension.xyz>
2024-02-19 09:42:02 +0000

    removed invalid data msg since not supported yet

--- app/dainclusionproofs/noninclusion.go
+++ app/dainclusionproofs/noninclusion.go
@@ -1,1 +1,12 @@
 package inclusion
+
+type NonInclusionProof struct {
+	RowProofs [][]byte
+	DataRoot  []byte
+}
+
+func (nip *NonInclusionProof) VerifyNonInclusion(namespace []byte, span int, length int, dataRoot []byte) error {
+
+	//TODO (srene): validate nmt root to data root using included merkle proofs
+	return nil
+}
--- x/rollapp/handler.go
+++ x/rollapp/handler.go
@@ -30,9 +30,7 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgWrongCommitmentBatch:
 			res, err := msgServer.SubmitWrongCommitmentBatch(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
-		case *types.MsgInvalidDataBatch:
-			res, err := msgServer.SubmitInvalidDataBatch(sdk.WrapSDKContext(ctx), msg)
-			return sdk.WrapServiceResult(ctx, res, err)
+
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
