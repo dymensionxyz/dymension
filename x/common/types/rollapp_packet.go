c5ebca810d8a9a5dd80b156a85c2b6dee88dd68f
omritoptix <omritoptix@gmail.com>
2024-03-03 11:54:50 +0100

    Moved rollapp packet events to events.go.

--- x/common/types/rollapp_packet.go
+++ x/common/types/rollapp_packet.go
@@ -6,17 +6,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-const (
-	AttributeKeyRollappId                = "rollapp_id"
-	AttributeKeyPacketStatus             = "status"
-	AttributeKeyPacketSourcePort         = "source_port"
-	AttributeKeyPacketSourceChannel      = "source_channel"
-	AttributeKeyPacketDestinationPort    = "destination_port"
-	AttributeKeyPacketDestinationChannel = "destination_channel"
-	AttributeKeyPacketSequence           = "packet_sequence"
-	AttributeKeyPacketError              = "error"
-)
-
 func (r RollappPacket) GetEvents() []sdk.Attribute {
 	eventAttributes := []sdk.Attribute{
 		sdk.NewAttribute(AttributeKeyRollappId, r.RollappId),
