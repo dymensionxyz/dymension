06c5202850eefa19a0afa1789efaa6bd49654ffd
Michael Tsitrin <michael@dymension.xyz>
2024-03-06 22:36:37 +0200

    fixed the context to use packetId for unique key

--- x/delayedack/types/proof_height_context.go
+++ x/delayedack/types/proof_height_context.go
@@ -6,20 +6,21 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
 )
 
 const (
-	// userKey to store the user information inside of a context
+	// proofHeightCtxKey is a context key to pass the proof height from the msg to the IBC middleware
 	proofHeightCtxKey = "ibc_proof_height"
 )
 
-func NewIBCProofContext(ctx sdk.Context, sequence uint64, height clienttypes.Height) sdk.Context {
-	key := fmt.Sprintf("%s_%d", proofHeightCtxKey, sequence)
+func NewIBCProofContext(ctx sdk.Context, packetId channeltypes.PacketId, height clienttypes.Height) sdk.Context {
+	key := fmt.Sprintf("%s_%s", proofHeightCtxKey, packetId.String())
 	return ctx.WithValue(key, height)
 }
 
-func FromIBCProofContext(ctx sdk.Context, sequence uint64) (clienttypes.Height, bool) {
-	key := fmt.Sprintf("%s_%d", proofHeightCtxKey, sequence)
+func FromIBCProofContext(ctx sdk.Context, packetId channeltypes.PacketId) (clienttypes.Height, bool) {
+	key := fmt.Sprintf("%s_%s", proofHeightCtxKey, packetId.String())
 	u, ok := ctx.Value(key).(clienttypes.Height)
 	return u, ok
 }
