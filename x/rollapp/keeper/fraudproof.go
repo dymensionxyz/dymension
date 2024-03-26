f7eb9e449484351d3e9a59ab5c72617aaf6d012f
danwt <30197399+danwt@users.noreply.github.com>
2024-03-26 16:16:14 +0100

    few cleanups of the validation

--- x/rollapp/keeper/fraudproof.go
+++ x/rollapp/keeper/fraudproof.go
@@ -53,31 +53,31 @@ func (k *Keeper) ValidateFraudProof(ctx sdk.Context, rollappID string, fp fraudt
 	idx := blockHeight - int64(stateInfo.StartHeight)
 	blockDescriptor := stateInfo.BDs.BD[idx]
 
-	if blockDescriptor.IntermediateStatesRoots == nil {
+	err = validateBlockDescriptor(blockDescriptor, fp.PreStateAppHash, fp.ExpectedValidAppHash)
+	if err != nil {
+		return fmt.Errorf("validate block descriptor: %w", err)
+	}
+	// TODO: Validate the fraudulent state transition is contained in the block header (TODO: ask Michael)
+	// TODO: anything else to do?
+	return nil
+}
+
+// expectedValidAppHash is the app hash that the fraud proof creator thinks the app hash should be after
+// the invalid state transition. Therefore, we shall only process the proof if it's different from what we had before
+func validateBlockDescriptor(bd types.BlockDescriptor, preAppStateHash, expectedValidAppHash []byte) error {
+	if len(bd.IntermediateStatesRoots) == 0 {
 		return types.ErrMissingIntermediateStatesRoots
 	}
 
-	foundIdx := -1
-	for idx, isr := range blockDescriptor.IntermediateStatesRoots {
-		// skip the last ISR
-		if idx == len(blockDescriptor.IntermediateStatesRoots)-1 {
-			break
-		}
-		if bytes.Equal(isr, fp.PreStateAppHash) {
-			foundIdx = idx
-			break
+	for i, isr := range bd.IntermediateStatesRoots[:len(bd.IntermediateStatesRoots)-1] { // go over all but the last
+		if bytes.Equal(isr, preAppStateHash) { // TODO(danwt): what if you have two hashes the same (would require empty block/ no tx?)
+			if bytes.Equal(bd.IntermediateStatesRoots[i+1], expectedValidAppHash) {
+				// in a valid fraud proof, these would be different, but they're not
+				return types.ErrInvalidExpectedAppHash
+			}
+			return nil
 		}
 	}
 
-	if foundIdx == -1 {
-		return types.ErrInvalidPreStateAppHash
-	}
-
-	if bytes.Equal(blockDescriptor.IntermediateStatesRoots[foundIdx+1], fp.ExpectedValidAppHash) {
-		return types.ErrInvalidExpectedAppHash
-	}
-
-	// TODO: Validate the fraudulent state transition is contained in the block header
-
-	return nil
+	return types.ErrInvalidPreStateAppHash
 }
