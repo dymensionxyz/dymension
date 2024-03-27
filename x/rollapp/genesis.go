4255a5d05b598223606091d399ff5d379fb75dcc
Sergi Rene <sergi@dymension.xyz>
2024-03-27 18:57:56 +0100

    adding status checks in finalization queue

--- x/rollapp/genesis.go
+++ x/rollapp/genesis.go
@@ -44,7 +44,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		// load rollapp object for stateful validations
 		rollapp, isFound := k.GetRollapp(ctx, elem.StateInfoIndex.RollappId)
 		if !isFound {
-			k.Logger(ctx).Error("error init genesis rollapp not found for state info: rollapp:%s: state info index: %s", elem.StateInfoIndex.RollappId, elem.StateInfoIndex.Index)
+			k.Logger(ctx).Error("error init genesis rollapp not found for state info: rollapp:%s: state info index: %d", elem.StateInfoIndex.RollappId, elem.StateInfoIndex.Index)
 			continue
 		}
 
@@ -62,7 +62,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		// check the latest state info is found
 		_, found := k.GetStateInfo(ctx, elem.RollappId, elem.Index)
 		if !found {
-			k.Logger(ctx).Error("error init genesis state info not found for latest state info index: rollapp:%s: latest state info index: %s:", elem.RollappId, elem.Index)
+			k.Logger(ctx).Error("error init genesis state info not found for latest state info index: rollapp:%s: latest state info index: %d:", elem.RollappId, elem.Index)
 			//invariant breaking. removing all state info for the rollapp
 			removeAllStateInfo(ctx, k, elem.RollappId)
 			continue
@@ -70,7 +70,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		// check if there are state infos with higher index
 		_, found = k.GetStateInfo(ctx, elem.RollappId, elem.Index+1)
 		if found {
-			k.Logger(ctx).Error("error init genesis state latest state info index is not the latest: rollapp:%s: latest state info index: %s:", elem.RollappId, elem.Index)
+			k.Logger(ctx).Error("error init genesis state latest state info index is not the latest: rollapp:%s: latest state info index: %d:", elem.RollappId, elem.Index)
 			//invariant breaking. removing all state info for the rollapp
 			removeAllStateInfo(ctx, k, elem.RollappId)
 			continue
@@ -84,7 +84,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		// check the rollapp exists
 		_, found := k.GetStateInfo(ctx, elem.RollappId, elem.Index)
 		if !found {
-			k.Logger(ctx).Error("error init genesis state info not found for latest finalized state info index: rollapp:%s: latest state info index: %s:", elem.RollappId, elem.Index)
+			k.Logger(ctx).Error("error init genesis state info not found for latest finalized state info index: rollapp:%s: latest state info index: %d:", elem.RollappId, elem.Index)
 			continue
 		}
 
@@ -96,13 +96,13 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		}
 		latestStateInfo, found := k.GetStateInfo(ctx, elem.RollappId, latestStateInfoIndex.Index)
 		if !found {
-			k.Logger(ctx).Error("error init genesis latest state info not found: rollapp:%s: latest state info index: %s:", elem.RollappId, latestStateInfoIndex.Index)
+			k.Logger(ctx).Error("error init genesis latest state info not found: rollapp:%s: latest state info index: %d:", elem.RollappId, latestStateInfoIndex.Index)
 			continue
 		}
 
 		// check the latest state info is not previous to the latest finalized state info
 		if latestStateInfo.StateInfoIndex.Index < elem.Index {
-			k.Logger(ctx).Error("error init genesis latest state info index lower than latest finalized state info: rollapp:%s: latest state info index: %s: latest finalized state info index:%s", elem.RollappId, latestStateInfoIndex.Index, elem.Index)
+			k.Logger(ctx).Error("error init genesis latest state info index lower than latest finalized state info: rollapp:%s: latest state info index: %d: latest finalized state info index:%d", elem.RollappId, latestStateInfoIndex.Index, elem.Index)
 			//invariant breaking. removing all state info for the rollapp
 			removeAllStateInfo(ctx, k, latestStateInfo.StateInfoIndex.RollappId)
 			removeLatestStateInfo(ctx, k, latestStateInfo.StateInfoIndex.RollappId)
@@ -113,6 +113,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		for i := uint64(1); i <= elem.Index; i++ {
 			stateInfo, found := k.GetStateInfo(ctx, elem.RollappId, i)
 			if !found || stateInfo.Status != common.Status_FINALIZED {
+				k.Logger(ctx).Error("error init genesis there are non-finalized state infos previous to the finalized state info: rollapp:%s: state info index: %d: latest finalized state info index:%d", elem.RollappId, stateInfo.StateInfoIndex.Index, elem.Index)
 				//invariant breaking. removing all state info for the rollapp
 				removeAllStateInfo(ctx, k, latestStateInfo.StateInfoIndex.RollappId)
 				removeLatestStateInfo(ctx, k, latestStateInfo.StateInfoIndex.RollappId)
@@ -133,14 +134,38 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 		}
 		for _, stateInfoIndex := range elem.FinalizationQueue {
 			stateInfo, found := k.GetStateInfo(ctx, stateInfoIndex.RollappId, stateInfoIndex.Index)
-			if !found || stateInfo.Status != common.Status_PENDING {
+			if !found {
 				// Invariant breaking
-				k.Logger(ctx).Error("error init genesis failed to find state for finalization: rollappId %s, index %d, found %t, status %s",
-					stateInfoIndex.RollappId, stateInfoIndex.Index, found, stateInfo.Status)
-			} else {
-				// adding state info to queue after validating state
-				queue.FinalizationQueue = append(queue.FinalizationQueue, stateInfoIndex)
+				k.Logger(ctx).Error("error init genesis failed to find state in finalization queue: rollappId %s, index %d",
+					stateInfoIndex.RollappId, stateInfoIndex.Index)
+				continue
 			}
+			if stateInfo.Status != common.Status_PENDING {
+				k.Logger(ctx).Error("error init genesis state info in finalization queue is not in pending state: rollappId %s, index %d",
+					stateInfoIndex.RollappId, stateInfoIndex.Index)
+				continue
+			}
+			latestFinalizedIndex, found := k.GetLatestFinalizedStateIndex(ctx, stateInfoIndex.RollappId)
+			if !found {
+				k.Logger(ctx).Error("error init genesis latest finalized index not found: rollappId %s, index %d",
+					stateInfoIndex.RollappId, stateInfoIndex.Index)
+				continue
+			}
+			latestFinalizedStateInfo, found := k.GetStateInfo(ctx, stateInfoIndex.RollappId, latestFinalizedIndex.Index)
+			if !found {
+				k.Logger(ctx).Error("error init genesis latest finalized state info not found: rollappId %s, index %d",
+					stateInfoIndex.RollappId, stateInfoIndex.Index)
+				continue
+			}
+
+			if latestFinalizedStateInfo.StateInfoIndex.Index >= stateInfo.StateInfoIndex.Index {
+				// Invariant breaking
+				k.Logger(ctx).Error("error init genesis state info in finalization queue should be finalized: rollappId %s, index: %d, finalized index:%d",
+					stateInfoIndex.RollappId, stateInfoIndex.Index, latestFinalizedStateInfo.StateInfoIndex.Index)
+			}
+			// adding state info to queue after validating state
+			queue.FinalizationQueue = append(queue.FinalizationQueue, stateInfoIndex)
+
 		}
 		k.SetBlockHeightToFinalizationQueue(ctx, queue)
 
