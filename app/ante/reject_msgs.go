ad78dc954504fc7083143e27e465c6d8d1c177af
omritoptix <omritoptix@gmail.com>
2024-03-30 22:43:20 +0100

    Removed vesting msgs rejections

--- app/ante/reject_msgs.go
+++ app/ante/reject_msgs.go
@@ -5,8 +5,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
-
-	vesting "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 )
 
 // RejectMessagesDecorator prevents invalid msg types from being executed
@@ -19,11 +17,7 @@ var _ sdk.AnteDecorator = RejectMessagesDecorator{}
 // NewRejectMessagesDecorator creates a decorator to block vesting messages from reaching the mempool
 func NewRejectMessagesDecorator() RejectMessagesDecorator {
 	return RejectMessagesDecorator{
-		disabledMsgTypeURLs: []string{
-			sdk.MsgTypeURL(&vesting.MsgCreateVestingAccount{}),
-			sdk.MsgTypeURL(&vesting.MsgCreatePermanentLockedAccount{}),
-			sdk.MsgTypeURL(&vesting.MsgCreatePeriodicVestingAccount{}),
-		},
+		disabledMsgTypeURLs: []string{},
 	}
 }
 
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/dymensionxyz/dymension/v3
 
-go 1.22.0
+go 1.22
 
 require (
 	cosmossdk.io/errors v1.0.1
