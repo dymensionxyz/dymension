317000ff184fea9b41e1bf4d6aaf314118ba1032
Michael Tsitrin <michael@dymension.xyz>
2024-03-31 15:51:14 +0300

    calling init genesis of modules with changed params

--- app/upgrades/v3/upgrade.go
+++ app/upgrades/v3/upgrade.go
@@ -5,6 +5,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	delayedacktypes "github.com/dymensionxyz/dymension/v3/x/delayedack/types"
+	rollapptypes "github.com/dymensionxyz/dymension/v3/x/rollapp/types"
+	seqtypes "github.com/dymensionxyz/dymension/v3/x/sequencer/types"
 )
 
 func GetStoreUpgrades() *storetypes.StoreUpgrades {
@@ -20,11 +24,16 @@ func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 ) upgradetypes.UpgradeHandler {
-	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
+
+		// this will trigger the init Genesis of those modules, as their params struct changed
+		delete(fromVM, delayedacktypes.ModuleName)
+		delete(fromVM, rollapptypes.ModuleName)
+		delete(fromVM, seqtypes.ModuleName)
 
 		// Start running the module migrations
 		logger.Debug("running module migrations ...")
-		return mm.RunMigrations(ctx, configurator, vm)
+		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
