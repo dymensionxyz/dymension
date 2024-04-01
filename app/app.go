153e9239330a1d987546ea5b030cb1dfd47de788
Omri <omritoptix@gmail.com>
2024-04-01 16:01:11 +0200

    fix(account): fixed wrong bech32 prefix for accountKeeper (#776)

--- app/app.go
+++ app/app.go
@@ -111,6 +111,7 @@ import (
 
 	ante "github.com/dymensionxyz/dymension/v3/app/ante"
 	appparams "github.com/dymensionxyz/dymension/v3/app/params"
+
 	rollappmodule "github.com/dymensionxyz/dymension/v3/x/rollapp"
 	rollappmoduleclient "github.com/dymensionxyz/dymension/v3/x/rollapp/client"
 	rollappmodulekeeper "github.com/dymensionxyz/dymension/v3/x/rollapp/keeper"
@@ -460,7 +461,7 @@ func New(
 
 	// add keepers
 	app.AccountKeeper = authkeeper.NewAccountKeeper(
-		appCodec, keys[authtypes.StoreKey], app.GetSubspace(authtypes.ModuleName), authtypes.ProtoBaseAccount, maccPerms, sdk.Bech32MainPrefix,
+		appCodec, keys[authtypes.StoreKey], app.GetSubspace(authtypes.ModuleName), authtypes.ProtoBaseAccount, maccPerms, sdk.GetConfig().GetBech32AccountAddrPrefix(),
 	)
 
 	app.AuthzKeeper = authzkeeper.NewKeeper(
