8b69563582dc2fdc81bd49478c8d33ece9a6e914
zale144 <aleksandar.sukovic@gmail.com>
2024-04-03 13:57:42 +0200

    Compare channel id to rollap's channel id, on packet ValidateRollappID

--- x/rollapp/types/errors.go
+++ x/rollapp/types/errors.go
@@ -42,6 +42,7 @@ var (
 	ErrInvalidGenesisAccount               = errorsmod.Register(ModuleName, 1031, "invalid genesis account")
 	ErrMintTokensFailed                    = errorsmod.Register(ModuleName, 1032, "failed to mint tokens")
 	ErrRegisterDenomMetadataFailed         = errorsmod.Register(ModuleName, 1033, "failed to register denom metadata")
+	ErrMismatchedChannelID                 = errorsmod.Register(ModuleName, 1034, "mismatched channel id")
 	/* ------------------------------ fraud related ----------------------------- */
 	ErrDisputeAlreadyFinalized = errorsmod.Register(ModuleName, 2000, "disputed height already finalized")
 	ErrDisputeAlreadyReverted  = errorsmod.Register(ModuleName, 2001, "disputed height already reverted")
