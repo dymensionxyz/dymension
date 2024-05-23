b6de6268f4f66466686893ca0e3ec637fa8a14e3
danwt <30197399+danwt@users.noreply.github.com>
2024-05-23 14:51:36 +0100

    impl stub trigger genesis evt

--- x/rollapp/keeper/msg_server.go
+++ x/rollapp/keeper/msg_server.go
@@ -1,11 +1,18 @@
 package keeper
 
 import (
+	"context"
+
 	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 )
 
 type msgServer struct {
 	Keeper
+}
+
+func (k msgServer) TriggerGenesisEvent(context.Context, *types.MsgRollappGenesisEvent) (*types.MsgRollappGenesisEventResponse, error) {
+	// TODO: return not implemented or deprecated error
+	return &types.MsgRollappGenesisEventResponse{}, nil
 }
 
 // NewMsgServerImpl returns an implementation of the MsgServer interface
