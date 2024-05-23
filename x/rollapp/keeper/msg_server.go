7068c0acf76ba59e9c6c365e5d6b301d52298998
danwt <30197399+danwt@users.noreply.github.com>
2024-05-23 14:50:43 +0100

    removes old genesis event from tx

--- x/rollapp/keeper/msg_server.go
+++ x/rollapp/keeper/msg_server.go
@@ -1,12 +1,6 @@
 package keeper
 
 import (
-	"context"
-	"fmt"
-
-	errorsmod "cosmossdk.io/errors"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	tenderminttypes "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint/types"
 	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 )
 
@@ -21,41 +15,3 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
-
-func (k Keeper) TriggerGen(goCtx context.Context, msg *types.MsgRollappGenesisEvent) (*types.MsgRollappGenesisEventResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// NOTE: whitelist check removed here
-
-	// Get the rollapp
-	rollapp, found := k.GetRollapp(ctx, msg.RollappId)
-	if !found {
-		return nil, types.ErrUnknownRollappID
-	}
-
-	// Get the channel and validate it's connected client chain is the same as the rollapp's
-	_, clientState, err := k.channelKeeper.GetChannelClientState(ctx, "transfer", msg.ChannelId)
-	if err != nil {
-		return nil, fmt.Errorf("get channel client state: %w", err)
-	}
-	tmClientState, ok := clientState.(*tenderminttypes.ClientState)
-	if !ok {
-		return nil, errorsmod.Wrapf(types.ErrInvalidGenesisChannelId, "expected tendermint client state, got %T", clientState)
-	}
-	if tmClientState.GetChainID() != msg.RollappId {
-		return nil, errorsmod.Wrapf(types.ErrInvalidGenesisChannelId, "channel connected to wrong chain: channel: %s: got: %s: expect: %s",
-			msg.ChannelId, tmClientState.GetChainID(), msg.RollappId)
-	}
-
-	// Update the rollapp with the channelID and trigger the genesis event
-	rollapp.ChannelId = msg.ChannelId
-	if err = k.TriggerRollappGenesisEvent(ctx, rollapp); err != nil {
-		return nil, fmt.Errorf("trigger rollapp genesis event: %w", err)
-	}
-
-	return &types.MsgRollappGenesisEventResponse{}, nil
-}
-
-func (k msgServer) TriggerGenesisEvent(goCtx context.Context, msg *types.MsgRollappGenesisEvent) (*types.MsgRollappGenesisEventResponse, error) {
-	return k.TriggerGen(goCtx, msg)
-}
---
+++ x/rollapp/keeper/wiz.go
@@ -0,0 +1,46 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	"cosmossdk.io/errors"
+
+	types2 "github.com/cosmos/cosmos-sdk/types"
+	types3 "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint/types"
+	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
+)
+
+func (k Keeper) TriggerGen(goCtx context.Context, msg *types.MsgRollappGenesisEvent) (*types.MsgRollappGenesisEventResponse, error) {
+	ctx := types2.UnwrapSDKContext(goCtx)
+
+	// NOTE: whitelist check removed here
+
+	// Get the rollapp
+	rollapp, found := k.GetRollapp(ctx, msg.RollappId)
+	if !found {
+		return nil, types.ErrUnknownRollappID
+	}
+
+	// Get the channel and validate it's connected client chain is the same as the rollapp's
+	_, clientState, err := k.channelKeeper.GetChannelClientState(ctx, "transfer", msg.ChannelId)
+	if err != nil {
+		return nil, fmt.Errorf("get channel client state: %w", err)
+	}
+	tmClientState, ok := clientState.(*types3.ClientState)
+	if !ok {
+		return nil, errors.Wrapf(types.ErrInvalidGenesisChannelId, "expected tendermint client state, got %T", clientState)
+	}
+	if tmClientState.GetChainID() != msg.RollappId {
+		return nil, errors.Wrapf(types.ErrInvalidGenesisChannelId, "channel connected to wrong chain: channel: %s: got: %s: expect: %s",
+			msg.ChannelId, tmClientState.GetChainID(), msg.RollappId)
+	}
+
+	// Update the rollapp with the channelID and trigger the genesis event
+	rollapp.ChannelId = msg.ChannelId
+	if err = k.TriggerRollappGenesisEvent(ctx, rollapp); err != nil {
+		return nil, fmt.Errorf("trigger rollapp genesis event: %w", err)
+	}
+
+	return &types.MsgRollappGenesisEventResponse{}, nil
+}
