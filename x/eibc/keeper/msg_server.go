dc2837f21209780ec73b6397cb434b26f80caeae
Michael Tsitrin <michael@dymension.xyz>
2024-06-09 15:42:25 +0300

    removed profitable check:

--- x/eibc/keeper/msg_server.go
+++ x/eibc/keeper/msg_server.go
@@ -38,15 +38,6 @@ func (m msgServer) FulfillOrder(goCtx context.Context, msg *types.MsgFulfillOrde
 		return nil, types.ErrDemandAlreadyFulfilled
 	}
 
-	// Check the order is profitable in regards to the bridging fee
-	denom := demandOrder.Price[0].Denom
-	originalAmt := demandOrder.Price.AmountOf(denom).Add(demandOrder.Fee.AmountOf(denom))
-	bridgingFee := m.DelayedAckKeeper.BridgingFeeFromAmt(ctx, originalAmt)
-
-	if demandOrder.Fee.AmountOf(denom).LT(bridgingFee) {
-		return nil, types.ErrDemandOrderNotProfitable
-	}
-
 	// Check the underlying packet is still relevant (i.e not expired, rejected, reverted)
 	if demandOrder.TrackingPacketStatus != commontypes.Status_PENDING {
 		return nil, types.ErrDemandOrderInactive
