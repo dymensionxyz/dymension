debff31fc1524ed0cb3c0f45b5a2f37f2033c030
danwt <30197399+danwt@users.noreply.github.com>
2024-06-11 17:27:48 +0100

    confirms installs

--- app/app.go
+++ app/app.go
@@ -763,7 +763,7 @@ func New(
 	var delayedAckMiddleware ibcporttypes.Middleware
 	delayedAckMiddleware = delayedackmodule.NewIBCMiddleware(transferStack, app.DelayedAckKeeper, app.RollappKeeper)
 	transferStack = delayedAckMiddleware
-	transferStack = transfergenesis.NewIBCModule(transferStack, app.DelayedAckKeeper, app.RollappKeeper, app.TransferKeeper, app.DenomMetadataKeeper, app.IBCKeeper.ChannelKeeper)
+	transferStack = transfergenesis.NewIBCModule(transferStack, app.DelayedAckKeeper, app.RollappKeeper, app.TransferKeeper, app.DenomMetadataKeeper)
 	transferStack = transferinject.NewIBCModule(transferStack, app.RollappKeeper)
 
 	// Create static IBC router, add transfer route, then set and seal it
