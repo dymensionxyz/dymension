390b0d35f2a84eb38489d216a04f10f36eda4c00
danwt <30197399+danwt@users.noreply.github.com>
2024-06-12 11:10:10 +0100

    adds transfer enabled ibc module

--- x/rollapp/transfersenabled/ibc_module.go
+++ x/rollapp/transfersenabled/ibc_module.go
@@ -1,10 +1,13 @@
 package transfersenabled
 
 import (
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
 	porttypes "github.com/cosmos/ibc-go/v6/modules/core/05-port/types"
 	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+	"github.com/dymensionxyz/dymension/v3/utils/gerr"
+	delayedackkeeper "github.com/dymensionxyz/dymension/v3/x/delayedack/keeper"
 	rollappkeeper "github.com/dymensionxyz/dymension/v3/x/rollapp/keeper"
 	"github.com/tendermint/tendermint/libs/log"
 )
@@ -12,15 +15,18 @@ import (
 type IBCModule struct {
 	porttypes.IBCModule // next one
 	rollappKeeper       rollappkeeper.Keeper
+	delayedackKeeper    delayedackkeeper.Keeper
 }
 
 func NewIBCModule(
 	next porttypes.IBCModule,
 	rollappKeeper rollappkeeper.Keeper,
+	delayedAckKeeper delayedackkeeper.Keeper,
 ) IBCModule {
 	return IBCModule{
-		IBCModule:     next,
-		rollappKeeper: rollappKeeper,
+		IBCModule:        next,
+		rollappKeeper:    rollappKeeper,
+		delayedackKeeper: delayedAckKeeper,
 	}
 }
 
@@ -37,9 +43,52 @@ func (w IBCModule) logger(
 	)
 }
 
+type ctxKeySkip struct{}
+
+func SkipContext(ctx sdk.Context) sdk.Context {
+	return ctx.WithValue(ctxKeySkip{}, true)
+}
+
+// skip returns if the context contains the skip directive
+// Not intended to be used outside of module
+func skip(ctx sdk.Context) bool {
+	val, ok := ctx.Value(ctxKeySkip{}).(bool)
+	return ok && val
+}
+
 func (w IBCModule) OnRecvPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	relayer sdk.AccAddress,
 ) exported.Acknowledgement {
+	l := w.logger(ctx, packet)
+
+	_ = l
+
+	if skip(ctx) {
+		return w.IBCModule.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	if !w.delayedackKeeper.IsRollappsEnabled(ctx) {
+		return w.IBCModule.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	transfer, err := w.rollappKeeper.GetValidTransferFromReceivedPacket(ctx, packet)
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(errorsmod.Wrap(err, "get valid transfer"))
+	}
+
+	if !transfer.IsRollapp() {
+		return w.IBCModule.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	ra := w.rollappKeeper.MustGetRollapp(ctx, transfer.RollappID)
+
+	if !ra.GenesisState.TransfersEnabled {
+		err = errorsmod.Wrapf(gerr.ErrFailedPrecondition, "transfers are disabled: rollapp id: %s", ra.RollappId)
+		// Someone on the RA tried to send a transfer before the bridge is open! Return an err ack and they will get refunded
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
+	return w.IBCModule.OnRecvPacket(ctx, packet, relayer)
 }
