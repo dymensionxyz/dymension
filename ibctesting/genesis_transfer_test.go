f5a05a7a6c6265f33487f1c5472ec7cca0c80c19
danwt <30197399+danwt@users.noreply.github.com>
2024-06-13 16:16:43 +0100

    refac, test is still failijng

--- ibctesting/genesis_transfer_test.go
+++ ibctesting/genesis_transfer_test.go
@@ -3,6 +3,8 @@ package ibctesting_test
 import (
 	"testing"
 
+	"cosmossdk.io/math"
+
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	rollapptypes "github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 
@@ -17,6 +19,8 @@ import (
 
 type transferGenesisSuite struct {
 	utilSuite
+
+	path *ibctesting.Path
 }
 
 func TestTransferGenesisTestSuite(t *testing.T) {
@@ -25,17 +29,17 @@ func TestTransferGenesisTestSuite(t *testing.T) {
 
 func (s *transferGenesisSuite) SetupTest() {
 	s.utilSuite.SetupTest()
-}
-
-// In the happy path, the new rollapp will send ibc transfers with a special
-// memo immediately when the channel opens. This will cause  all the denoms to get registered, and tokens
-// to go to the right addresses.
-func (s *transferGenesisSuite) TestHappyPath() {
 	path := s.newTransferPath(s.hubChain(), s.rollappChain())
 	s.coordinator.Setup(path)
 	s.createRollapp(false, nil) // genesis protocol is not finished yet
 	s.registerSequencer()
+	s.path = path
+}
 
+// In the happy path, the new rollapp will send ibc transfers with a special
+// memo immediately when the channel opens. This will cause  all the denoms to get registered, and tokens
+// to go to the right addresses. After all transfers are sent, the bridge opens.
+func (s *transferGenesisSuite) TestHappyPath() {
 	/*
 		Send a bunch of transfer packets to the hub
 		Check the balances are created
@@ -43,7 +47,6 @@ func (s *transferGenesisSuite) TestHappyPath() {
 		Check the bridge is enabled (or not)
 	*/
 
-	timeoutHeight := clienttypes.NewHeight(100, 110)
 	amt, ok := sdk.NewIntFromString("10000000000000000000") // 10
 	s.Require().True(ok)
 
@@ -51,58 +54,25 @@ func (s *transferGenesisSuite) TestHappyPath() {
 
 	for i, denom := range denoms {
 		/* ------------------- move non-registered token from rollapp ------------------- */
-		meta := banktypes.Metadata{
-			Description: "",
-			DenomUnits: []*banktypes.DenomUnit{
-				{
-					Denom: denom,
-				},
-				{
-					Denom:    "a" + denom,
-					Exponent: 18,
-				},
-			},
-			Base:    denom,
-			Display: denom,
-			Name:    denom,
-			Symbol:  denom,
-			URI:     "",
-			URIHash: "",
-		}
-		s.Require().NoError(meta.Validate()) // sanity check the test is written correctly
-		memo := rollapptypes.GenesisTransferMemo{
-			Denom:             meta,
-			TotalNumTransfers: uint64(len(denoms)),
-			ThisTransferIx:    uint64(i),
-		}.Namespaced().MustString()
-		tokens := sdk.NewCoin(meta.Base, amt)
 
-		apptesting.FundAccount(s.rollappApp(), s.rollappCtx(), s.rollappChain().SenderAccount.GetAddress(), sdk.Coins{tokens})
-		msg := types.NewMsgTransfer(
-			path.EndpointB.ChannelConfig.PortID,
-			path.EndpointB.ChannelID,
-			tokens,
-			s.rollappChain().SenderAccount.GetAddress().String(),
-			s.hubChain().SenderAccount.GetAddress().String(),
-			timeoutHeight,
-			0,
-			memo,
-		)
+		msg := s.transferMsg(amt, denom, i, len(denoms))
+		apptesting.FundAccount(s.rollappApp(), s.rollappCtx(), s.rollappChain().SenderAccount.GetAddress(), sdk.Coins{msg.Token})
 		res, err := s.rollappChain().SendMsgs(msg)
 		s.Require().NoError(err)
 		packet, err := ibctesting.ParsePacketFromEvents(res.GetEvents())
 		s.Require().NoError(err)
 
-		err = path.RelayPacket(packet)
+		err = s.path.RelayPacket(packet)
 		s.Require().NoError(err)
 
+		// after the last one, it should be OK
 		transfersEnabled := s.hubApp().RollappKeeper.MustGetRollapp(s.hubCtx(), rollappChainID()).GenesisState.TransfersEnabled
 		s.Require().Equal(i == len(denoms)-1, transfersEnabled, "transfers enabled check", "i", i)
 	}
 
 	for _, denom := range denoms {
 		// has the denom?
-		ibcDenom := types.ParseDenomTrace(types.GetPrefixedDenom(path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, denom)).IBCDenom()
+		ibcDenom := types.ParseDenomTrace(types.GetPrefixedDenom(s.path.EndpointB.ChannelConfig.PortID, s.path.EndpointB.ChannelID, denom)).IBCDenom()
 		metadata, found := s.hubApp().BankKeeper.GetDenomMetaData(s.hubCtx(), ibcDenom)
 		s.Require().True(found, "missing denom metadata for rollapps taking token")
 		s.Require().Equal(denom, metadata.Base)
@@ -110,4 +80,47 @@ func (s *transferGenesisSuite) TestHappyPath() {
 		c := s.hubApp().BankKeeper.GetBalance(s.hubCtx(), s.hubChain().SenderAccount.GetAddress(), ibcDenom)
 		s.Require().Equal(amt, c.Amount)
 	}
+}
+
+func (s *transferGenesisSuite) transferMsg(amt math.Int, denom string, i, nDenomsTotal int) *types.MsgTransfer {
+	meta := banktypes.Metadata{
+		Description: "",
+		DenomUnits: []*banktypes.DenomUnit{
+			{
+				Denom: denom,
+			},
+			{
+				Denom:    "a" + denom,
+				Exponent: 18,
+			},
+		},
+		Base:    denom,
+		Display: "a" + denom, // TODO: set right
+		Name:    denom,
+		Symbol:  denom,
+		URI:     "",
+		URIHash: "",
+	}
+	s.Require().NoError(meta.Validate()) // sanity check the test is written correctly
+	memo := rollapptypes.GenesisTransferMemo{
+		Denom:             meta,
+		TotalNumTransfers: uint64(nDenomsTotal),
+		ThisTransferIx:    uint64(i),
+	}.Namespaced().MustString()
+	tokens := sdk.NewCoin(meta.Base, amt)
+
+	timeoutHeight := clienttypes.NewHeight(100, 110)
+
+	msg := types.NewMsgTransfer(
+		s.path.EndpointB.ChannelConfig.PortID,
+		s.path.EndpointB.ChannelID,
+		tokens,
+		s.rollappChain().SenderAccount.GetAddress().String(),
+		s.hubChain().SenderAccount.GetAddress().String(),
+		timeoutHeight,
+		0,
+		memo,
+	)
+
+	return msg
 }
