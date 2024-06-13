709b8457bcbeca7659aaa2cc5fb51c0ebe3f6671
danwt <30197399+danwt@users.noreply.github.com>
2024-06-13 13:39:50 +0100

    bridging fee

--- ibctesting/bridging_fee_test.go
+++ ibctesting/bridging_fee_test.go
@@ -11,57 +11,56 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type BridgingFeeTestSuite struct {
+type bridgingFeeSuite struct {
 	utilSuite
 }
 
 func TestBridgingFeeTestSuite(t *testing.T) {
-	suite.Run(t, new(BridgingFeeTestSuite))
+	suite.Run(t, new(bridgingFeeSuite))
 }
 
-func (suite *BridgingFeeTestSuite) TestNotRollappNoBridgingFee() {
+func (s *bridgingFeeSuite) TestNotRollappNoBridgingFee() {
 	// setup between cosmosChain and hubChain
-	path := suite.newTransferPath(suite.hubChain(), suite.cosmosChain())
-	suite.coordinator.Setup(path)
+	path := s.newTransferPath(s.hubChain(), s.cosmosChain())
+	s.coordinator.Setup(path)
 	hubEndpoint := path.EndpointA
 	cosmosEndpoint := path.EndpointB
 
 	timeoutHeight := clienttypes.NewHeight(100, 110)
 	amount, ok := sdk.NewIntFromString("10000000000000000000") // 10DYM
-	suite.Require().True(ok)
+	s.Require().True(ok)
 	coinToSendToB := sdk.NewCoin(sdk.DefaultBondDenom, amount)
 
 	// send from cosmosChain to hubChain
 	msg := types.NewMsgTransfer(cosmosEndpoint.ChannelConfig.PortID, cosmosEndpoint.ChannelID, coinToSendToB, cosmosEndpoint.Chain.SenderAccount.GetAddress().String(), hubEndpoint.Chain.SenderAccount.GetAddress().String(), timeoutHeight, 0, "")
 	res, err := cosmosEndpoint.Chain.SendMsgs(msg)
-	suite.Require().NoError(err) // message committed
+	s.Require().NoError(err) // message committed
 	packet, err := ibctesting.ParsePacketFromEvents(res.GetEvents())
-	suite.Require().NoError(err)
+	s.Require().NoError(err)
 	err = path.RelayPacket(packet)
-	suite.NoError(err) // relay committed
+	s.NoError(err) // relay committed
 
-	denom := suite.getRollappToHubIBCDenomFromPacket(packet)
-	finalBalance := suite.hubApp().BankKeeper.GetBalance(suite.hubChain().GetContext(), suite.hubChain().SenderAccount.GetAddress(), denom)
-	suite.Equal(sdk.NewCoin(denom, coinToSendToB.Amount), finalBalance)
+	denom := s.getRollappToHubIBCDenomFromPacket(packet)
+	finalBalance := s.hubApp().BankKeeper.GetBalance(s.hubChain().GetContext(), s.hubChain().SenderAccount.GetAddress(), denom)
+	s.Equal(sdk.NewCoin(denom, coinToSendToB.Amount), finalBalance)
 }
 
-func (suite *BridgingFeeTestSuite) TestBridgingFee() {
-	path := suite.newTransferPath(suite.hubChain(), suite.rollappChain())
-	suite.coordinator.Setup(path)
+func (s *bridgingFeeSuite) TestBridgingFee() {
+	path := s.newTransferPath(s.hubChain(), s.rollappChain())
+	s.coordinator.Setup(path)
+	s.createRollappWithFinishedGenesis(path.EndpointA.ChannelID)
+	s.registerSequencer()
 
 	rollappEndpoint := path.EndpointB
-	rollappIBCKeeper := suite.rollappChain().App.GetIBCKeeper()
-
-	suite.createRollappWithFinishedGenesis(path.EndpointA.ChannelID)
-	suite.registerSequencer()
+	rollappIBCKeeper := s.rollappChain().App.GetIBCKeeper()
 
 	// Update rollapp state
-	currentRollappBlockHeight := uint64(suite.rollappChain().GetContext().BlockHeight())
-	suite.updateRollappState(currentRollappBlockHeight)
+	currentRollappBlockHeight := uint64(s.rollappChain().GetContext().BlockHeight())
+	s.updateRollappState(currentRollappBlockHeight)
 
 	timeoutHeight := clienttypes.NewHeight(100, 110)
 	amount, ok := sdk.NewIntFromString("10000000000000000000") // 10DYM
-	suite.Require().True(ok)
+	s.Require().True(ok)
 	coinToSendToB := sdk.NewCoin(sdk.DefaultBondDenom, amount)
 
 	/* --------------------- initiating transfer on rollapp --------------------- */
@@ -69,41 +68,41 @@ func (suite *BridgingFeeTestSuite) TestBridgingFee() {
 		rollappEndpoint.ChannelConfig.PortID,
 		rollappEndpoint.ChannelID,
 		coinToSendToB,
-		suite.rollappChain().SenderAccount.GetAddress().String(),
-		suite.hubChain().SenderAccount.GetAddress().String(),
+		s.rollappChain().SenderAccount.GetAddress().String(),
+		s.hubChain().SenderAccount.GetAddress().String(),
 		timeoutHeight,
 		0,
 		"",
 	)
-	res, err := suite.rollappChain().SendMsgs(msg)
-	suite.Require().NoError(err) // message committed
+	res, err := s.rollappChain().SendMsgs(msg)
+	s.Require().NoError(err) // message committed
 	packet, err := ibctesting.ParsePacketFromEvents(res.GetEvents())
-	suite.Require().NoError(err)
+	s.Require().NoError(err)
 	found := rollappIBCKeeper.ChannelKeeper.HasPacketCommitment(rollappEndpoint.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
-	suite.Require().True(found)
+	s.Require().True(found)
 	err = path.RelayPacket(packet)
-	suite.Require().Error(err) // expecting error as no AcknowledgePacket expected to return
+	s.Require().Error(err) // expecting error as no AcknowledgePacket expected to return
 
 	// check balance before finalization
-	denom := suite.getRollappToHubIBCDenomFromPacket(packet)
+	denom := s.getRollappToHubIBCDenomFromPacket(packet)
 	transferredCoins := sdk.NewCoin(denom, coinToSendToB.Amount)
-	recipient := suite.hubChain().SenderAccount.GetAddress()
-	initialBalance := suite.hubApp().BankKeeper.SpendableCoins(suite.hubChain().GetContext(), recipient)
-	suite.Require().Equal(initialBalance.AmountOf(denom), sdk.ZeroInt())
+	recipient := s.hubChain().SenderAccount.GetAddress()
+	initialBalance := s.hubApp().BankKeeper.SpendableCoins(s.hubChain().GetContext(), recipient)
+	s.Require().Equal(initialBalance.AmountOf(denom), sdk.ZeroInt())
 
 	// Finalize the rollapp state
-	currentRollappBlockHeight = uint64(suite.rollappChain().GetContext().BlockHeight())
-	_, err = suite.finalizeRollappState(1, currentRollappBlockHeight)
-	suite.Require().NoError(err)
+	currentRollappBlockHeight = uint64(s.rollappChain().GetContext().BlockHeight())
+	_, err = s.finalizeRollappState(1, currentRollappBlockHeight)
+	s.Require().NoError(err)
 
 	// check balance after finalization
-	expectedFee := suite.hubApp().DelayedAckKeeper.BridgingFeeFromAmt(suite.hubChain().GetContext(), transferredCoins.Amount)
+	expectedFee := s.hubApp().DelayedAckKeeper.BridgingFeeFromAmt(s.hubChain().GetContext(), transferredCoins.Amount)
 	expectedBalance := initialBalance.Add(transferredCoins).Sub(sdk.NewCoin(denom, expectedFee))
-	finalBalance := suite.hubApp().BankKeeper.SpendableCoins(suite.hubChain().GetContext(), recipient)
-	suite.Equal(expectedBalance, finalBalance)
+	finalBalance := s.hubApp().BankKeeper.SpendableCoins(s.hubChain().GetContext(), recipient)
+	s.Equal(expectedBalance, finalBalance)
 
 	// check fees
-	addr := suite.hubApp().AccountKeeper.GetModuleAccount(suite.hubChain().GetContext(), txfees.ModuleName)
-	txFeesBalance := suite.hubApp().BankKeeper.GetBalance(suite.hubChain().GetContext(), addr.GetAddress(), denom)
-	suite.Equal(expectedFee, txFeesBalance.Amount)
+	addr := s.hubApp().AccountKeeper.GetModuleAccount(s.hubChain().GetContext(), txfees.ModuleName)
+	txFeesBalance := s.hubApp().BankKeeper.GetBalance(s.hubChain().GetContext(), addr.GetAddress(), denom)
+	s.Equal(expectedFee, txFeesBalance.Amount)
 }
