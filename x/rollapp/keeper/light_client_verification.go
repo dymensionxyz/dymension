780bad5908b91bb63cee566cfc1f5c62efc75e33
danwt <30197399+danwt@users.noreply.github.com>
2024-06-18 13:19:28 +0100

    save some notes

--- x/rollapp/keeper/light_client_verification.go
+++ x/rollapp/keeper/light_client_verification.go
@@ -20,6 +20,41 @@ import (
 	sequencerkeeper "github.com/dymensionxyz/dymension/v3/x/sequencer/keeper"
 )
 
+/*
+
+Whats the attack we are trying to stop?
+A sequencer from saying he did X via the light client, when in reality he did Y
+
+So what will be the basic idea here?
+
+When a light client update comes in, it has a client id, and a header
+
+The height on the header is the height of the rollapp
+
+
+Links
+ibc go header https://github.com/cosmos/ibc-go/blob/v6.2.2/modules/light-clients/07-tendermint/types/update.go
+tendermind header https://github.com/cosmos/ibc-go/blob/a89bb2f556f519a3bbc3c97e107e29f722d59155/modules/light-clients/07-tendermint/types/tendermint.pb.go#L185-L202
+what the validators actually sign https://github.com/cometbft/cometbft/blob/v0.34.33/types/canonical.go#L54-L65
+
+
+How does the light client update work?
+The update contains a trusted height and a trusted validators
+If that height is available and trusted on the LC, check that the nextValidatorsHash on the trusted data matches (the hash) in the header
+then do light.Verify
+light.Verify does 2 cases. For h+1, or more than h+1:
+	basically it makes sure that
+		- the trusting period not passed
+		- header timestamp is inside trusting period
+		- header timestamp is progressing forward from the last trusted one
+		- make sure trust level proportion of trusted validators signed new commit (?)
+	if h+1, requires the untrusted validators hash to be exactly equal to the trusted nextValidatorsHash
+	otherwise;
+		Trust level is default 1/3
+		If (+)1/3 of trusted guys signed the new header AND 2/3 of new validators signed the new header
+
+*/
+
 type ClientKeeper interface {
 	GetClientConsensusState(ctx sdk.Context, clientID string, height exported.Height) (exported.ConsensusState, bool)
 }
