280eed775b9b4ef70487a1ef0d3fb694e09f85ec
danwt <30197399+danwt@users.noreply.github.com>
2024-06-18 17:35:29 +0100

    before start looking at conn init notes

--- x/rollapp/keeper/light_client_verification.go
+++ x/rollapp/keeper/light_client_verification.go
@@ -30,7 +30,14 @@ update entry point https://github.com/cosmos/ibc-go/blob/edb6efaddf45053efb9cbd2
 ibc go header https://github.com/cosmos/ibc-go/blob/v6.2.2/modules/light-clients/07-tendermint/types/update.go
 tendermind header https://github.com/cosmos/ibc-go/blob/a89bb2f556f519a3bbc3c97e107e29f722d59155/modules/light-clients/07-tendermint/types/tendermint.pb.go#L185-L202
 what the validators actually sign https://github.com/cometbft/cometbft/blob/v0.34.33/types/canonical.go#L54-L65
+verify commit https://github.com/cometbft/cometbft/blob/v0.34.33/types/validator_set.go#L716-L743
 
+
+Note, the root in the tendermint consensus state, is the app hash from the state after TXs from the PREVIOUS block
+	https://github.com/cosmos/ibc-go/blob/edb6efaddf45053efb9cbd2ad1ceab64b22fb085/modules/light-clients/07-tendermint/types/header.go#L21
+	https://github.com/dymensionxyz/cometbft/blob/a059b062dcfc719406354e0a80f5f6d3cf7401e1/proto/tendermint/types/types.proto#L76
+which should be equal to the corresponding state info block descriptor 'StateRoot'
+	https://github.com/dymensionxyz/dymint/blob/d9dec3e96bd058732186d80bcc3d01f489f71634/settlement/dymension/dymension.go#L429
 
 How does the light client update work?
 The update contains a trusted height and a trusted validators
@@ -74,6 +81,11 @@ type clientUpdateEvent struct {
 	header          exported.Header
 }
 
+// if the ibc module accepts a new consensus state, then it must have been signed by 2/3 of the voting power
+// of the NEW validators, as well as 1/3 of the previously trusted validator set
+// therefore, as long as know that the first every consensus state was signed by the rollapp sequencer
+// then we know all future consensus states were signed by him too
+// so we just need to check the sequencer signed the first ever one
 func (v lcv) verifyNewLightClientHeader(ctx sdk.Context, evt clientUpdateEvent) error {
 	consState, ok := v.ibckeeper.ClientKeeper.GetClientConsensusState(ctx, evt.clientID, evt.consensusHeight)
 	if !ok {
@@ -111,7 +123,7 @@ func (v lcv) verify(
 	height uint64,
 	lightClientState exported.ConsensusState,
 	lightClientStateTendermint *tendermint.ConsensusState,
-	rollappState *types.StateInfo,
+	rollappState *types.StateInfo, // needs to be the one for (h-1) where h is the light client one
 ) error {
 	// We check if it matches
 	raBD, err := rollappState.BlockDescriptor(height)
