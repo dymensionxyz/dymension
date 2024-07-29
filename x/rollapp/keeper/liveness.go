c306757bbdea430fab3cab7bd9aa3bf79def7e92
danwt <30197399+danwt@users.noreply.github.com>
2024-07-29 16:17:52 +0200

    implemented the logic

--- x/rollapp/keeper/liveness.go
+++ x/rollapp/keeper/liveness.go
@@ -13,5 +13,16 @@ func NextSlashOrJailHeight(
 	heightEvent int64, // hub height to schedule event
 	isJail bool, // is it a jail event? (false -> slash)
 ) {
+	// how long has the rollapp been down already?
 	downTime := time.Duration(heightHub-heightLastRollappUpdate) * hubBlockInterval
+	// when should we schedule the next slash/jail, in terms of down time?
+	var eventDuration time.Duration
+	if downTime <= slashTimeNoUpdate {
+		eventDuration = slashTimeNoUpdate
+	} else {
+		eventDuration = slashTimeNoUpdate + ((downTime-slashTimeNoUpdate)/slashInterval+1)*slashTimeNoUpdate
+	}
+	heightEvent = heightLastRollappUpdate + int64(eventDuration/hubBlockInterval) + 1
+	isJail = jailTime <= eventDuration
+	return
 }
