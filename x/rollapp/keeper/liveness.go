cff313d6a3d6a5eb42104357402a335b95392079
danwt <30197399+danwt@users.noreply.github.com>
2024-07-29 19:15:12 +0200

    impl the keys

--- x/rollapp/keeper/liveness.go
+++ x/rollapp/keeper/liveness.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 )
 
 func NextSlashOrJailHeight(
@@ -29,7 +30,52 @@ func NextSlashOrJailHeight(
 // CheckLiveness will slash or jail any sequencers for whom their rollapp has been down
 // and a slash or jail event is due
 func (k Keeper) CheckLiveness(ctx sdk.Context) {
+	evts := k.GetLivenessEvents(ctx)
+	for _, evt := range evts {
+		if !evt.IsJail {
+			// TODO: slash
+		} else {
+			// TODO: jail
+		}
+		// TODO: check if jailed, if he is not then schedule another event
+	}
 }
 
-func (k Keeper) GetScheduledLivenessEvents(ctx sdk.Context) {
+// GetLivenessEvents returns all scheduled events for the current block height
+func (k Keeper) GetLivenessEvents(ctx sdk.Context) []types.LivenessEvent {
+	h := ctx.BlockHeight()
+	return k.getLivenessEvents(ctx, &h)
+}
+
+// GetAllLivenessEvents returns all scheduled events (for genesis export)
+func (k Keeper) GetAllLivenessEvents(ctx sdk.Context) []types.LivenessEvent {
+	return k.getLivenessEvents(ctx, nil)
+}
+
+// getLivenessEvents returns events. If a height is specified, only for that height.
+func (k Keeper) getLivenessEvents(ctx sdk.Context, height *int64) []types.LivenessEvent {
+	store := ctx.KVStore(k.storeKey)
+	iterator := sdk.KVStorePrefixIterator(store, types.LivenessEventQueueIterKey(height))
+	defer iterator.Close() // nolint: errcheck
+
+	ret := []types.LivenessEvent{}
+	for ; iterator.Valid(); iterator.Next() {
+		e := types.LivenessEventQueueItemToEvent(iterator.Key(), iterator.Value())
+		if height != nil && *height < e.HubHeight {
+			break
+		}
+		ret = append(ret, e)
+	}
+	return ret
+}
+
+// SetLivenessEvent returns all scheduled events (for genesis export)
+func (k Keeper) SetLivenessEvent(ctx sdk.Context, e types.LivenessEvent) {
+	store := ctx.KVStore(k.storeKey)
+	key := types.LivenessEventQueueKey(e.HubHeight, e.RollappId)
+	val := types.LivenessEventQueueSlash
+	if e.IsJail {
+		val = types.LivenessEventQueueJail
+	}
+	store.Set(key, val)
 }
--- x/rollapp/types/liveness.go
+++ x/rollapp/types/liveness.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"bytes"
+	"encoding/binary"
 	"time"
 )
 
@@ -17,4 +19,50 @@ type LivenessCheckParams struct {
 	SlashInterval time.Duration
 	// JailTime	is the time a sequencer can be down after which he will be jailed rather than slashed
 	JailTime time.Duration
+}
+
+var (
+	LivenessEventQueueKeyPrefix = []byte("LivenessEventQueue/")
+	LivenessEventQueueSlash     = []byte("s")
+	LivenessEventQueueJail      = []byte("j")
+)
+
+func LivenessEventQueueKey(height int64, rollappID string) []byte {
+	return createLivenessEventQueueKey(&height, &rollappID)
+}
+
+func LivenessEventQueueIterKey(height *int64) []byte {
+	return createLivenessEventQueueKey(height, nil)
+}
+
+func createLivenessEventQueueKey(height *int64, rollappID *string) []byte {
+	if rollappID != nil && height == nil {
+		panic("must provide height with rollapp id")
+	}
+	var key []byte
+	key = append(key, LivenessEventQueueKeyPrefix...)
+	if height != nil {
+		hBz := make([]byte, 8)
+		binary.BigEndian.PutUint64(hBz, uint64(*height))
+		key = append(key, hBz...)
+	}
+	if rollappID != nil {
+		key = append(key, []byte("/")...)
+		key = append(key, []byte(*rollappID)...)
+	}
+	return key
+}
+
+// LivenessEventQueueItemToEvent converts store key and value to an event
+// Assumes the key is well-formed (contains both height and rollapp id)
+func LivenessEventQueueItemToEvent(k, v []byte) LivenessEvent {
+	ret := LivenessEvent{}
+	ret.IsJail = bytes.Equal(v, LivenessEventQueueJail)
+	// key is like 'foo/height/rollapp'
+	//                  i      j
+	i := len(LivenessEventQueueKeyPrefix) + 1
+	j := i + 8 + 1
+	ret.HubHeight = int64(binary.BigEndian.Uint64(k[i : j-1]))
+	ret.RollappId = string(k[j:])
+	return ret
 }
