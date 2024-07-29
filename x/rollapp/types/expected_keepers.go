d9aae1cf5f6a451a0fdd01e4262fc7881aee6268
danwt <30197399+danwt@users.noreply.github.com>
2024-07-29 15:52:29 +0200

    bring back main file

---
+++ x/rollapp/types/expected_keepers.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+)
+
+type IBCClientKeeper interface {
+	GetClientState(ctx sdk.Context, clientID string) (exported.ClientState, bool)
+	SetClientState(ctx sdk.Context, clientID string, clientState exported.ClientState)
+}
+
+type ChannelKeeper interface {
+	GetChannelClientState(ctx sdk.Context, portID, channelID string) (string, exported.ClientState, error)
+}
