e5618a2721abc934dcb8de69a3a12099ce4763b6
danwt <30197399+danwt@users.noreply.github.com>
2024-07-29 15:54:28 +0200

    move a type across

--- x/sequencer/keeper/check_liveness.go
+++ x/sequencer/keeper/check_liveness.go
@@ -9,6 +9,18 @@ import (
 	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
+type CalculateNextSlashOrJailHeight func(
+	HubBlockInterval time.Duration, // average time between hub blocks
+	SlashTimeNoUpdate time.Duration, // time until first slash if not updating
+	SlashInterval time.Duration, // gap between slash if still not updating
+	JailTime time.Duration, // time until jail if not updating
+	HubHeight int64, // current hub height
+	LastRollappUpdateHeight int64, // when was the rollapp last updated
+) (
+	hubHeight int64, // height to schedule event
+	isJail bool, // is it a jail event? (false -> slash)
+)
+
 func MulCoinsDec(coins sdk.Coins, dec sdk.Dec) sdk.Coins {
 	return sdk.Coins{}
 }
