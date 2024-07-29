a925e938c7c358e2ba05fe16c2df9c97ef6faa28
danwt <30197399+danwt@users.noreply.github.com>
2024-07-29 15:58:48 +0200

    move some stuff across

--- x/sequencer/keeper/slashing.go
+++ x/sequencer/keeper/slashing.go
@@ -42,3 +42,50 @@ func (k Keeper) Slashing(ctx sdk.Context, seqAddr string) error {
 
 	return nil
 }
+
+func MulCoinsDec(coins sdk.Coins, dec sdk.Dec) sdk.Coins {
+	// TODO: use it
+	return sdk.Coins{}
+}
+
+func (k Keeper) Slash(ctx sdk.Context, seq types.Sequencer, amt sdk.Coins, recipientAddr *sdk.AccAddress) error {
+	seq.Tokens.Sub(amt...)
+	if recipientAddr != nil {
+		err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, *recipientAddr, amt)
+		if err != nil {
+			return errorsmod.Wrap(err, "send coins from module to account")
+		}
+	} else {
+		err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, amt)
+		if err != nil {
+			return errorsmod.Wrap(err, "burn coins")
+		}
+	}
+	// TODO: write back sequencer?
+	return nil
+}
+
+func (k Keeper) Jail(ctx sdk.Context, seq types.Sequencer) error {
+	// TODO: check contents of this, since it was copied
+
+	oldStatus := seq.Status
+	wasProposer := seq.Proposer
+	// in case we are slashing an unbonding sequencer, we need to remove it from the unbonding queue
+	if oldStatus == types.Unbonding {
+		k.removeUnbondingSequencer(ctx, seq)
+	}
+
+	// set the status to unbonded
+	seq.Status = types.Unbonded
+	seq.Jailed = true
+	seq.Proposer = false
+	seq.UnbondingHeight = ctx.BlockHeight()
+	seq.UnbondTime = ctx.BlockHeader().Time
+	k.UpdateSequencer(ctx, seq, oldStatus)
+	// rotate proposer if the slashed sequencer was the proposer
+	if wasProposer {
+		k.RotateProposer(ctx, seq.RollappId)
+	}
+
+	return nil
+}
