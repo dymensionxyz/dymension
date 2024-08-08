57eef7dd7cc384d71f8bb7668b034e26d5dcd67f
VictorTrustyDev <me@victortrusty.dev>
2024-08-08 17:27:19 +0700

    implement place sell order type Alias

--- x/dymns/keeper/msg_server_place_sell_order.go
+++ x/dymns/keeper/msg_server_place_sell_order.go
@@ -11,8 +11,6 @@ import (
 	dymnstypes "github.com/dymensionxyz/dymension/v3/x/dymns/types"
 )
 
-// TODO DymNS: bidder should be Roll-App owner
-
 // PlaceSellOrder is message handler,
 // handles creating a Sell-Order that advertise a Dym-Name/Alias is for sale, performed by the owner.
 func (k msgServer) PlaceSellOrder(goCtx context.Context, msg *dymnstypes.MsgPlaceSellOrder) (*dymnstypes.MsgPlaceSellOrderResponse, error) {
@@ -29,6 +27,8 @@ func (k msgServer) PlaceSellOrder(goCtx context.Context, msg *dymnstypes.MsgPlac
 
 	if msg.OrderType == dymnstypes.NameOrder {
 		resp, err = k.processPlaceSellOrderTypeDymName(ctx, msg, params)
+	} else if msg.OrderType == dymnstypes.AliasOrder {
+		resp, err = k.processPlaceSellOrderTypeAlias(ctx, msg, params)
 	} else {
 		err = errorsmod.Wrapf(gerrc.ErrInvalidArgument, "invalid order type: %s", msg.OrderType)
 	}
@@ -119,4 +119,78 @@ func (k msgServer) validatePlaceSellOrderTypeDymName(
 	}
 
 	return dymName, nil
+}
+
+// processPlaceSellOrderTypeAlias handles the message handled by PlaceSellOrder, type Alias.
+func (k msgServer) processPlaceSellOrderTypeAlias(
+	ctx sdk.Context,
+	msg *dymnstypes.MsgPlaceSellOrder, params dymnstypes.Params,
+) (*dymnstypes.MsgPlaceSellOrderResponse, error) {
+	if !params.Misc.EnableTradingAlias {
+		return nil, errorsmod.Wrapf(gerrc.ErrFailedPrecondition, "trading of Alias is disabled")
+	}
+
+	err := k.validatePlaceSellOrderTypeAlias(ctx, msg, params)
+	if err != nil {
+		return nil, err
+	}
+
+	so := msg.ToSellOrder()
+	so.ExpireAt = ctx.BlockTime().Add(params.Misc.SellOrderDuration).Unix()
+
+	if err := so.Validate(); err != nil {
+		panic(errorsmod.Wrap(err, "un-expected invalid state of created SO"))
+	}
+
+	if err := k.SetSellOrder(ctx, so); err != nil {
+		return nil, err
+	}
+
+	aSoe := k.GetActiveSellOrdersExpiration(ctx, so.Type)
+	aSoe.Add(so.GoodsId, so.ExpireAt)
+	if err := k.SetActiveSellOrdersExpiration(ctx, aSoe, so.Type); err != nil {
+		return nil, err
+	}
+
+	return &dymnstypes.MsgPlaceSellOrderResponse{}, nil
+}
+
+// validatePlaceSellOrderTypeAlias handles validation for message handled by PlaceSellOrder, type Alias.
+func (k msgServer) validatePlaceSellOrderTypeAlias(
+	ctx sdk.Context,
+	msg *dymnstypes.MsgPlaceSellOrder, params dymnstypes.Params,
+) error {
+	alias := msg.GoodsId
+
+	sourceRollAppId, found := k.GetRollAppIdByAlias(ctx, alias)
+	if !found {
+		return errorsmod.Wrapf(gerrc.ErrNotFound, "alias: %s", alias)
+	}
+
+	// TODO DymNS: rename to IsRollAppOwner and the other places using term `creator`
+	if !k.IsRollAppCreator(ctx, sourceRollAppId, msg.Owner) {
+		return errorsmod.Wrapf(gerrc.ErrPermissionDenied,
+			"not the owner of the RollApp using the alias: %s", sourceRollAppId,
+		)
+	}
+
+	existingActiveSo := k.GetSellOrder(ctx, alias, msg.OrderType)
+	if existingActiveSo != nil {
+		if existingActiveSo.HasFinishedAtCtx(ctx) {
+			return errorsmod.Wrap(
+				gerrc.ErrAlreadyExists,
+				"an active expired/completed Sell-Order already exists for the Alias, must wait until processed",
+			)
+		}
+		return errorsmod.Wrap(gerrc.ErrAlreadyExists, "an active Sell-Order already exists for the Alias")
+	}
+
+	if msg.MinPrice.Denom != params.Price.PriceDenom {
+		return errorsmod.Wrapf(
+			gerrc.ErrInvalidArgument,
+			"the only denom allowed as price: %s", params.Price.PriceDenom,
+		)
+	}
+
+	return nil
 }
--- x/dymns/keeper/msg_server_place_sell_order_test.go
+++ x/dymns/keeper/msg_server_place_sell_order_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 	"time"
 
+	rollappkeeper "github.com/dymensionxyz/dymension/v3/x/rollapp/keeper"
+	rollapptypes "github.com/dymensionxyz/dymension/v3/x/rollapp/types"
+
 	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -15,7 +18,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_msgServer_PlaceSellOrder(t *testing.T) {
+func Test_msgServer_PlaceSellOrder_DymName(t *testing.T) {
 	now := time.Now().UTC()
 
 	const daysProhibitSell = 30
@@ -321,6 +324,282 @@ func Test_msgServer_PlaceSellOrder(t *testing.T) {
 			var found bool
 			for _, record := range aSoe.Records {
 				if record.GoodsId == name {
+					found = true
+					require.Equal(t, expectedSo.ExpireAt, record.ExpireAt)
+					break
+				}
+			}
+
+			require.True(t, found)
+		})
+	}
+}
+
+func Test_msgServer_PlaceSellOrder_Alias(t *testing.T) {
+	now := time.Now().UTC()
+
+	const daysSellOrderDuration = 7
+	denom := dymnsutils.TestCoin(0).Denom
+
+	setupTest := func() (sdk.Context, dymnskeeper.Keeper, rollappkeeper.Keeper, dymnskeeper.BankKeeper) {
+		dk, bk, rk, ctx := testkeeper.DymNSKeeper(t)
+		ctx = ctx.WithBlockTime(now)
+
+		moduleParams := dk.GetParams(ctx)
+		moduleParams.Price.PriceDenom = denom
+		moduleParams.Misc.SellOrderDuration = daysSellOrderDuration * 24 * time.Hour
+		// force enable trading
+		moduleParams.Misc.EnableTradingName = true
+		moduleParams.Misc.EnableTradingAlias = true
+		err := dk.SetParams(ctx, moduleParams)
+		require.NoError(t, err)
+
+		return ctx, dk, rk, bk
+	}
+
+	const srcRollAppId = "rollapp_1-1"
+	const alias = "alias"
+	const dstRollAppId = "rollapp_2-2"
+
+	ownerA := testAddr(1).bech32()
+	notOwnerA := testAddr(2).bech32()
+	bidderA := testAddr(3).bech32()
+
+	coin100 := dymnsutils.TestCoin(100)
+	coin200 := dymnsutils.TestCoin(200)
+	coin300 := dymnsutils.TestCoin(300)
+
+	tests := []struct {
+		name               string
+		withoutAlias       bool
+		existingSo         *dymnstypes.SellOrder
+		customOwner        string
+		customRollAppOwner string
+		minPrice           sdk.Coin
+		sellPrice          *sdk.Coin
+		preRunSetup        func(sdk.Context, dymnskeeper.Keeper)
+		wantErr            bool
+		wantErrContains    string
+	}{
+		{
+			name:            "fail - alias does not exists",
+			withoutAlias:    true,
+			minPrice:        coin100,
+			wantErr:         true,
+			wantErrContains: "alias: alias: not found",
+		},
+		{
+			name:               "fail - wrong owner",
+			customOwner:        ownerA,
+			customRollAppOwner: notOwnerA,
+			minPrice:           coin100,
+			wantErr:            true,
+			wantErrContains:    "not the owner of the RollApp using the alias",
+		},
+		{
+			name: "fail - existing active SO, not finished",
+			existingSo: &dymnstypes.SellOrder{
+				Type:      dymnstypes.AliasOrder,
+				ExpireAt:  now.Add(time.Hour).Unix(),
+				MinPrice:  coin100,
+				SellPrice: &coin200,
+			},
+			minPrice:        coin100,
+			wantErr:         true,
+			wantErrContains: "an active Sell-Order already exists",
+		},
+		{
+			name: "fail - existing active SO, expired",
+			existingSo: &dymnstypes.SellOrder{
+				Type:      dymnstypes.AliasOrder,
+				ExpireAt:  now.Add(-1 * time.Hour).Unix(),
+				MinPrice:  coin100,
+				SellPrice: &coin200,
+			},
+			minPrice:        coin100,
+			wantErr:         true,
+			wantErrContains: "an active expired/completed Sell-Order already exists ",
+		},
+		{
+			name: "fail - existing active SO, not expired, completed",
+			existingSo: &dymnstypes.SellOrder{
+				Type:      dymnstypes.AliasOrder,
+				ExpireAt:  now.Add(time.Hour).Unix(),
+				MinPrice:  coin100,
+				SellPrice: &coin200,
+				HighestBid: &dymnstypes.SellOrderBid{
+					Bidder: bidderA,
+					Price:  coin200,
+					Params: []string{dstRollAppId},
+				},
+			},
+			minPrice:        coin100,
+			wantErr:         true,
+			wantErrContains: "an active expired/completed Sell-Order already exists ",
+		},
+		{
+			name: "fail - existing active SO, expired, completed",
+			existingSo: &dymnstypes.SellOrder{
+				Type:      dymnstypes.AliasOrder,
+				ExpireAt:  now.Add(-1 * time.Hour).Unix(),
+				MinPrice:  coin100,
+				SellPrice: &coin200,
+				HighestBid: &dymnstypes.SellOrderBid{
+					Bidder: bidderA,
+					Price:  coin200,
+					Params: []string{dstRollAppId},
+				},
+			},
+			minPrice:        coin100,
+			wantErr:         true,
+			wantErrContains: "an active expired/completed Sell-Order already exists",
+		},
+		{
+			name:            "fail - not allowed denom",
+			minPrice:        sdk.NewInt64Coin("u"+denom, 100),
+			wantErr:         true,
+			wantErrContains: fmt.Sprintf("the only denom allowed as price: %s", denom),
+		},
+		{
+			name:      "pass - successfully place Alias Sell-Order, without sell price",
+			minPrice:  coin100,
+			sellPrice: nil,
+		},
+		{
+			name:      "pass - successfully place Alias Sell-Order, without sell price",
+			minPrice:  coin100,
+			sellPrice: dymnsutils.TestCoinP(0),
+		},
+		{
+			name:      "pass - successfully place Alias Sell-Order, with sell price",
+			minPrice:  coin100,
+			sellPrice: &coin300,
+		},
+		{
+			name:      "pass - successfully place Alias Sell-Order, with sell price equals to min-price",
+			minPrice:  coin100,
+			sellPrice: &coin100,
+		},
+		{
+			name:      "fail - can NOT place Alias Sell-Order, when Alias trading is disabled",
+			minPrice:  coin100,
+			sellPrice: nil,
+			preRunSetup: func(ctx sdk.Context, dk dymnskeeper.Keeper) {
+				moduleParams := dk.GetParams(ctx)
+				moduleParams.Misc.EnableTradingAlias = false
+				err := dk.SetParams(ctx, moduleParams)
+				require.NoError(t, err)
+			},
+			wantErr:         true,
+			wantErrContains: "trading of Alias is disabled",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, dk, rk, _ := setupTest()
+
+			useRollAppOwner := ownerA
+			if tt.customRollAppOwner != "" {
+				useRollAppOwner = tt.customRollAppOwner
+			}
+
+			rk.SetRollapp(ctx, rollapptypes.Rollapp{
+				RollappId: srcRollAppId,
+				Owner:     useRollAppOwner,
+			})
+			if !tt.withoutAlias {
+				err := dk.SetAliasForRollAppId(ctx, srcRollAppId, alias)
+				require.NoError(t, err)
+			}
+
+			if tt.existingSo != nil {
+				tt.existingSo.GoodsId = alias
+				err := dk.SetSellOrder(ctx, *tt.existingSo)
+				require.NoError(t, err)
+			}
+
+			useOwner := ownerA
+			if tt.customOwner != "" {
+				useOwner = tt.customOwner
+			}
+			msg := &dymnstypes.MsgPlaceSellOrder{
+				GoodsId:   alias,
+				OrderType: dymnstypes.AliasOrder,
+				MinPrice:  tt.minPrice,
+				SellPrice: tt.sellPrice,
+				Owner:     useOwner,
+			}
+
+			if tt.preRunSetup != nil {
+				tt.preRunSetup(ctx, dk)
+			}
+
+			resp, err := dymnskeeper.NewMsgServerImpl(dk).PlaceSellOrder(ctx, msg)
+			moduleParams := dk.GetParams(ctx)
+
+			defer func() {
+				if tt.withoutAlias {
+					requireAliasNotInUse(alias, t, ctx, dk)
+					requireRollAppHasNoAlias(srcRollAppId, t, ctx, dk)
+				} else {
+					requireAliasLinkedToRollApp(alias, srcRollAppId, t, ctx, dk)
+				}
+			}()
+
+			if tt.wantErr {
+				require.NotEmpty(t, tt.wantErrContains, "mis-configured test case")
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.wantErrContains)
+
+				require.Nil(t, resp)
+
+				so := dk.GetSellOrder(ctx, alias, dymnstypes.AliasOrder)
+				if tt.existingSo != nil {
+					require.NotNil(t, so)
+					require.Equal(t, *tt.existingSo, *so)
+				} else {
+					require.Nil(t, so)
+				}
+
+				require.Less(t,
+					ctx.GasMeter().GasConsumed(), dymnstypes.OpGasPlaceSellOrder,
+					"should not consume params gas on failed operation",
+				)
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+
+			so := dk.GetSellOrder(ctx, alias, dymnstypes.AliasOrder)
+			require.NotNil(t, so)
+
+			expectedSo := dymnstypes.SellOrder{
+				GoodsId:    alias,
+				Type:       dymnstypes.AliasOrder,
+				ExpireAt:   ctx.BlockTime().Add(moduleParams.Misc.SellOrderDuration).Unix(),
+				MinPrice:   msg.MinPrice,
+				SellPrice:  msg.SellPrice,
+				HighestBid: nil,
+			}
+			if !expectedSo.HasSetSellPrice() {
+				expectedSo.SellPrice = nil
+			}
+
+			require.Nil(t, so.HighestBid, "highest bid should not be set")
+
+			require.Equal(t, expectedSo, *so)
+
+			require.GreaterOrEqual(t,
+				ctx.GasMeter().GasConsumed(), dymnstypes.OpGasPlaceSellOrder,
+				"should consume params gas",
+			)
+
+			aSoe := dk.GetActiveSellOrdersExpiration(ctx, dymnstypes.AliasOrder)
+
+			var found bool
+			for _, record := range aSoe.Records {
+				if record.GoodsId == alias {
 					found = true
 					require.Equal(t, expectedSo.ExpireAt, record.ExpireAt)
 					break
