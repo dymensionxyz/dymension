8acc57a1d8844f0f30c6bc19972a6f41d09c99e8
danwt <30197399+danwt@users.noreply.github.com>
2024-08-08 18:09:25 +0100

    sequencer

--- x/sequencer/handler.go
+++ x/sequencer/handler.go
@@ -1,10 +1,8 @@
 package sequencer
 
 import (
-	"fmt"
-
-	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 
 	"github.com/dymensionxyz/dymension/v3/x/sequencer/keeper"
 	"github.com/dymensionxyz/dymension/v3/x/sequencer/types"
@@ -31,8 +29,7 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 			res, err := msgServer.IncreaseBond(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		default:
-			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
-			return nil, errorsmod.Wrap(types.ErrUnknownRequest, errMsg)
+			return nil, gerrc.ErrInvalidArgument.Wrapf("unrecognized %s message type: %T", types.ModuleName, msg)
 		}
 	}
 }
--- x/sequencer/types/errors.go
+++ x/sequencer/types/errors.go
@@ -3,29 +3,27 @@ package types
 // DONTCOVER
 
 import (
-	errorsmod "cosmossdk.io/errors"
 	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
 // x/sequencer module sentinel errors
 var (
-	ErrSequencerExists          = errorsmod.Register(ModuleName, 1000, "sequencer already exist for this address; must use new sequencer address")
-	ErrUnknownRollappID         = errorsmod.Register(ModuleName, 1002, "rollapp does not exist")
-	ErrUnknownSequencer         = errorsmod.Register(ModuleName, 1005, "sequencer was not registered")
-	ErrSequencerRollappMismatch = errorsmod.Register(ModuleName, 1006, "sequencer was not registered for this rollapp")
-	ErrNotActiveSequencer       = errorsmod.Register(ModuleName, 1007, "sequencer is not active")
-	ErrInvalidSequencerStatus   = errorsmod.Register(ModuleName, 1008, "invalid sequencer status")
-	ErrInvalidCoinDenom         = errorsmod.Register(ModuleName, 1010, "invalid coin denomination")
-	ErrInsufficientBond         = errorsmod.Register(ModuleName, 1011, "insufficient bond")
-	ErrRollappJailed            = errorsmod.Register(ModuleName, 1012, "rollapp is jailed")
-	ErrInvalidAddress           = errorsmod.Register(ModuleName, 1013, "invalid address")
-	ErrInvalidPubKey            = errorsmod.Register(ModuleName, 1014, "invalid pubkey")
-	ErrInvalidCoins             = errorsmod.Register(ModuleName, 1015, "invalid coins")
-	ErrInvalidType              = errorsmod.Register(ModuleName, 1016, "invalid type")
-	ErrUnknownRequest           = errorsmod.Register(ModuleName, 1017, "unknown request")
-	ErrInvalidRequest           = errorsmod.Register(ModuleName, 1018, "invalid request")
-	ErrSequencerJailed          = errorsmod.Register(ModuleName, 1019, "sequencer is jailed")
-	ErrNotInitialSequencer      = errorsmod.Wrap(gerrc.ErrFailedPrecondition, "not the initial sequencer")
-	ErrInvalidURL               = errorsmod.Wrap(gerrc.ErrInvalidArgument, "invalid url")
-	ErrInvalidMetadata          = errorsmod.Wrap(gerrc.ErrInvalidArgument, "invalid metadata")
+	ErrSequencerExists          = gerrc.ErrAlreadyExists.Wrap("sequencer")
+	ErrUnknownRollappID         = gerrc.ErrNotFound.Wrap("rollapp does not exist")
+	ErrUnknownSequencer         = gerrc.ErrNotFound.Wrap("sequencer was not registered")
+	ErrSequencerRollappMismatch = gerrc.ErrFailedPrecondition.Wrap("sequencer was not registered for this rollapp")
+	ErrNotActiveSequencer       = gerrc.ErrFailedPrecondition.Wrap("sequencer is not active")
+	ErrInvalidSequencerStatus   = gerrc.ErrInvalidArgument.Wrap("sequencer status")
+	ErrInvalidCoinDenom         = gerrc.ErrInvalidArgument.Wrap("coin denomination")
+	ErrInsufficientBond         = gerrc.ErrOutOfRange.Wrap("insufficient bond")
+	ErrRollappJailed            = gerrc.ErrFailedPrecondition.Wrap("rollapp is jailed")
+	ErrInvalidAddress           = gerrc.ErrInvalidArgument.Wrap("address")
+	ErrInvalidPubKey            = gerrc.ErrInvalidArgument.Wrap("pubkey")
+	ErrInvalidCoins             = gerrc.ErrInvalidArgument.Wrap("coins")
+	ErrInvalidType              = gerrc.ErrInvalidArgument.Wrap("type")
+	ErrInvalidRequest           = gerrc.ErrInvalidArgument.Wrap("request")
+	ErrSequencerJailed          = gerrc.ErrFailedPrecondition.Wrap("sequencer is jailed")
+	ErrNotInitialSequencer      = gerrc.ErrFailedPrecondition.Wrap("not the initial sequencer")
+	ErrInvalidURL               = gerrc.ErrInvalidArgument.Wrap("url")
+	ErrInvalidMetadata          = gerrc.ErrInvalidArgument.Wrap("metadata")
 )
