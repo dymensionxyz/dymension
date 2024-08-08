a42e61698df9458dd786488b07996eb4577277c3
danwt <30197399+danwt@users.noreply.github.com>
2024-08-08 18:23:23 +0100

    working to fix tests

--- x/sponsorship/types/genesis.go
+++ x/sponsorship/types/genesis.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"errors"
+
+	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -15,13 +18,13 @@ func DefaultGenesis() *GenesisState {
 func (g GenesisState) Validate() error {
 	err := g.Params.Validate()
 	if err != nil {
-		return ErrInvalidGenesis.Wrapf(err.Error())
+		return errors.Join(ErrInvalidGenesis, err)
 	}
 
 	for _, i := range g.VoterInfos {
 		err = i.Validate()
 		if err != nil {
-			return ErrInvalidGenesis.Wrapf(err.Error())
+			return errors.Join(ErrInvalidGenesis, err)
 		}
 	}
 
@@ -31,15 +34,12 @@ func (g GenesisState) Validate() error {
 func (v VoterInfo) Validate() error {
 	_, err := sdk.AccAddressFromBech32(v.Voter)
 	if err != nil {
-		return ErrInvalidVoterInfo.Wrapf(
-			"voter '%s' must be a valid bech32 address: %s",
-			v.Voter, err.Error(),
-		)
+		return errorsmod.Wrapf(errors.Join(ErrInvalidVoterInfo, err), "voter '%s' must be a valid bech32 address:", v.Voter)
 	}
 
 	err = v.Vote.Validate()
 	if err != nil {
-		return ErrInvalidVoterInfo.Wrapf(err.Error())
+		return errors.Join(ErrInvalidVoterInfo, err)
 	}
 
 	// Validate validators
@@ -47,13 +47,13 @@ func (v VoterInfo) Validate() error {
 	validators := make(map[string]struct{}, len(v.Validators)) // this map helps check for duplicates
 	for _, val := range v.Validators {
 		if _, ok := validators[val.Validator]; ok {
-			return ErrInvalidVoterInfo.Wrapf("duplicated validators: %s", val.Validator)
+			return errorsmod.Wrapf(ErrInvalidVoterInfo, "duplicated validators: %s", val.Validator)
 		}
 		validators[val.Validator] = struct{}{}
 		total = total.Add(val.Power)
 	}
 	if total.GT(v.Vote.VotingPower) {
-		return ErrInvalidVoterInfo.Wrapf("voting power mismatch: vote voting power %s is less than total validator power %s", v.Vote.VotingPower, total)
+		return errorsmod.Wrapf(ErrInvalidVoterInfo, "voting power mismatch: vote voting power %s is less than total validator power %s", v.Vote.VotingPower, total)
 	}
 
 	return nil
