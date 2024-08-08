37cba3409dc330faaba1aa1e0ad01272cf7a44fd
danwt <30197399+danwt@users.noreply.github.com>
2024-08-08 18:11:45 +0100

    streamer

--- x/streamer/types/errors.go
+++ x/streamer/types/errors.go
@@ -1,22 +1,16 @@
 package types
 
 import (
-	errorsmod "cosmossdk.io/errors"
+	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
-// The following regiisters various lockdrop errors.
+// The following registers various lockdrop errors.
 var (
-	ErrNoGaugeIdExist                = errorsmod.Register(ModuleName, 1, "no gauge id exist")
-	ErrDistrRecordNotPositiveWeight  = errorsmod.Register(ModuleName, 2, "weight in record should be non negative")
-	ErrDistrRecordNotRegisteredGauge = errorsmod.Register(ModuleName, 3, "gauge was not registered")
-	ErrDistrRecordRegisteredGauge    = errorsmod.Register(ModuleName, 4, "gauge was already registered")
-	ErrDistrRecordNotSorted          = errorsmod.Register(ModuleName, 5, "gauges are not sorted")
-	ErrDistrInfoNotPositiveWeight    = errorsmod.Register(ModuleName, 6, "total distribution weight should be positive")
+	ErrDistrRecordNotPositiveWeight = gerrc.ErrOutOfRange.Wrap("weight in record should be non negative")
+	ErrDistrRecordRegisteredGauge   = gerrc.ErrAlreadyExists.Wrap("gauge")
+	ErrDistrRecordNotSorted         = gerrc.ErrInvalidArgument.Wrap("not sorted")
+	ErrDistrInfoNotPositiveWeight   = gerrc.ErrOutOfRange.Wrap("total distribution weight should be positive")
 
-	ErrEmptyProposalRecords         = errorsmod.Register(ModuleName, 10, "records are empty")
-	ErrEmptyProposalGaugeIds        = errorsmod.Register(ModuleName, 11, "gauge ids are empty")
-	ErrDistrInfoTotalWeightNotEqual = errorsmod.Register(ModuleName, 12, "total weight is not equal to sum of weights in records")
-
-	ErrInvalidStreamStatus = errorsmod.Register(ModuleName, 20, "invalid stream status")
-	ErrUnknownRequest      = errorsmod.Register(ModuleName, 21, "unknown request")
+	ErrInvalidStreamStatus = gerrc.ErrFailedPrecondition.Wrap("stream status")
+	ErrUnknownRequest      = gerrc.ErrInvalidArgument.Wrap("request")
 )
