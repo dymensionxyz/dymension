383f493c6e2394a90ceade10fdf3e3f7b6b79fde
VictorTrustyDev <me@victortrusty.dev>
2024-08-10 05:53:01 +0700

    use cometbft max length, add a TODO for future refactor

--- x/dymns/utils/chain.go
+++ x/dymns/utils/chain.go
@@ -1,15 +1,20 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+
+	cometbfttypes "github.com/cometbft/cometbft/types"
+)
 
 // patternValidChainId is a regex pattern for valid chain id format.
 var patternValidChainId = regexp.MustCompile(`^[a-z]+(-[a-z]+)?(_\d+)?(-\d+)?$`)
 
 // IsValidChainIdFormat returns true if the given string is a valid chain id format.
 // It checks the length and the pattern of the chain id.
-// The chain id length can be between 3 and 47 characters.
+// The chain id length can be between 3 and 50 characters.
 func IsValidChainIdFormat(chainId string) bool {
-	if len(chainId) < 3 || len(chainId) > 47 {
+	// TODO: move validation functions like this to sdk-utils repo
+	if len(chainId) < 3 || len(chainId) > cometbfttypes.MaxChainIDLen {
 		return false
 	}
 
