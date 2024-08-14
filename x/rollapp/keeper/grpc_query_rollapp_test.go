c9c033e1e0f07867bcc41bd7f6b4647bc6cb96c8
Daniel T <30197399+danwt@users.noreply.github.com>
2024-08-14 08:49:14 +0100

    fix(rollapp): queries now return full rollapp info (#1096)

--- x/rollapp/keeper/grpc_query_rollapp_test.go
+++ x/rollapp/keeper/grpc_query_rollapp_test.go
@@ -1,126 +1,107 @@
 package keeper_test
 
 import (
-	"strconv"
+	"flag"
+	"math"
 	"testing"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/dymensionxyz/gerr-cosmos/gerrc"
+	"github.com/dymensionxyz/sdk-utils/utils/urand"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
+	"pgregory.net/rapid"
 
 	keepertest "github.com/dymensionxyz/dymension/v3/testutil/keeper"
-	"github.com/dymensionxyz/dymension/v3/testutil/nullify"
 	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 )
 
-// Prevent strconv unused error
-var _ = strconv.IntSize
-
-func TestRollappQuerySingle(t *testing.T) {
-	keeper, ctx := keepertest.RollappKeeper(t)
-	wctx := sdk.WrapSDKContext(ctx)
-	msgs, _ := createNRollapp(keeper, ctx, 2)
-	for _, tc := range []struct {
-		desc     string
-		request  *types.QueryGetRollappRequest
-		response *types.QueryGetRollappResponse
-		err      error
-	}{
-		{
-			desc: "First",
-			request: &types.QueryGetRollappRequest{
-				RollappId: msgs[0].RollappId,
-			},
-			response: &types.QueryGetRollappResponse{Rollapp: msgs[0]},
-		},
-		{
-			desc: "Second",
-			request: &types.QueryGetRollappRequest{
-				RollappId: msgs[1].RollappId,
-			},
-			response: &types.QueryGetRollappResponse{Rollapp: msgs[1]},
-		},
-		{
-			desc: "KeyNotFound",
-			request: &types.QueryGetRollappRequest{
-				RollappId: strconv.Itoa(100000),
-			},
-			err: status.Error(codes.NotFound, "not found"),
-		},
-		{
-			desc: "InvalidRequest",
-			err:  status.Error(codes.InvalidArgument, "invalid request"),
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			response, err := keeper.Rollapp(wctx, tc.request)
-			if tc.err != nil {
-				require.ErrorIs(t, err, tc.err)
-			} else {
-				require.NoError(t, err)
-				require.Equal(t,
-					nullify.Fill(tc.response),
-					nullify.Fill(response),
-				)
-			}
+func TestQuery(t *testing.T) {
+	_ = flag.Set("rapid.checks", "50")
+	_ = flag.Set("rapid.steps", "50")
+	rapid.Check(t, func(r *rapid.T) {
+		k, ctx := keepertest.RollappKeeper(t)
+		ids := rapid.SampledFrom([]string{
+			urand.RollappID(),
+			urand.RollappID(),
+			urand.RollappID(),
+			urand.RollappID(),
+			urand.RollappID(),
 		})
-	}
-}
-
-func TestRollappQueryPaginated(t *testing.T) {
-	keeper, ctx := keepertest.RollappKeeper(t)
-	wctx := sdk.WrapSDKContext(ctx)
-	_, msgs := createNRollapp(keeper, ctx, 5)
-
-	request := func(next []byte, offset, limit uint64, total bool) *types.QueryAllRollappRequest {
-		return &types.QueryAllRollappRequest{
-			Pagination: &query.PageRequest{
-				Key:        next,
-				Offset:     offset,
-				Limit:      limit,
-				CountTotal: total,
+		m := map[string]types.Rollapp{}
+		r.Repeat(map[string]func(*rapid.T){
+			"": func(t *rapid.T) {
 			},
-		}
-	}
-	t.Run("ByOffset", func(t *testing.T) {
-		step := 2
-		for i := 0; i < len(msgs); i += step {
-			resp, err := keeper.RollappAll(wctx, request(nil, uint64(i), uint64(step), false))
-			require.NoError(t, err)
-			require.LessOrEqual(t, len(resp.Rollapp), step)
-			require.Subset(t,
-				nullify.Fill(msgs),
-				nullify.Fill(resp.Rollapp),
-			)
-		}
-	})
-	t.Run("ByKey", func(t *testing.T) {
-		step := 2
-		var next []byte
-		for i := 0; i < len(msgs); i += step {
-			resp, err := keeper.RollappAll(wctx, request(next, 0, uint64(step), false))
-			require.NoError(t, err)
-			require.LessOrEqual(t, len(resp.Rollapp), step)
-			require.Subset(t,
-				nullify.Fill(msgs),
-				nullify.Fill(resp.Rollapp),
-			)
-			next = resp.Pagination.NextKey
-		}
-	})
-	t.Run("Total", func(t *testing.T) {
-		resp, err := keeper.RollappAll(wctx, request(nil, 0, 0, true))
-		require.NoError(t, err)
-		require.Equal(t, len(msgs), int(resp.Pagination.Total))
-		require.ElementsMatch(t,
-			nullify.Fill(msgs),
-			nullify.Fill(resp.Rollapp),
-		)
-	})
-	t.Run("InvalidRequest", func(t *testing.T) {
-		_, err := keeper.RollappAll(wctx, nil)
-		require.ErrorIs(t, err, status.Error(codes.InvalidArgument, "invalid request"))
+			"put": func(t *rapid.T) {
+				ra := types.Rollapp{RollappId: ids.Draw(t, "id")}
+				m[ra.RollappId] = ra
+				k.SetRollapp(ctx, ra)
+			},
+			"del": func(t *rapid.T) {
+				id := ids.Draw(t, "id")
+				delete(m, id)
+				k.RemoveRollapp(ctx, id)
+			},
+			"get one": func(t *rapid.T) {
+				id := ids.Draw(t, "id")
+				ra, ok := k.GetRollapp(ctx, id)
+				_, okM := m[ra.RollappId]
+				require.Equal(t, okM, ok)
+				if okM {
+					require.Equal(t, ra.RollappId, id)
+				}
+			},
+			"get all": func(t *rapid.T) {
+				got := k.GetAllRollapps(ctx)
+				for _, ra := range got {
+					_, ok := m[ra.RollappId]
+					require.True(t, ok)
+				}
+				for _, ra := range m {
+					require.True(t, slices.ContainsFunc(got, func(raGot types.Rollapp) bool {
+						return raGot.RollappId == ra.RollappId
+					}))
+				}
+			},
+			"get one by name": func(t *rapid.T) {
+				id := ids.Draw(t, "id")
+				cid, _ := types.NewChainID(id)
+				ra, ok := k.GetRollappByName(ctx, cid.GetName())
+				_, okM := m[id]
+				require.Equal(t, okM, ok)
+				if okM {
+					require.Equal(t, id, ra.RollappId)
+				}
+			},
+			"query one": func(t *rapid.T) {
+				id := ids.Draw(t, "id")
+				res, err := k.Rollapp(sdk.WrapSDKContext(ctx), &types.QueryGetRollappRequest{RollappId: id})
+				_, okM := m[id]
+				if !okM {
+					require.True(t, errorsmod.IsOf(err, gerrc.ErrNotFound))
+				} else {
+					require.Equal(t, id, res.Rollapp.RollappId)
+				}
+			},
+			"query many": func(t *rapid.T) {
+				var key []byte
+				unseen := maps.Clone(m)
+				q := &types.QueryAllRollappRequest{Pagination: &query.PageRequest{
+					Key:   key,
+					Limit: math.MaxUint64, // no need to rest pagination mechanism
+				}}
+				res, err := k.RollappAll(sdk.WrapSDKContext(ctx), q)
+				require.NoError(t, err)
+				for _, ra := range res.Rollapp {
+					_, ok := unseen[ra.Rollapp.RollappId]
+					require.True(t, ok)
+					delete(unseen, ra.Rollapp.RollappId)
+				}
+				require.Empty(t, unseen)
+			},
+		})
 	})
 }
