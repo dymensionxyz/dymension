03072c56d75d8930db4f2e717a24e3578ef1a77b
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-08-20 09:32:21 +0530

    implementing ibc_msg_channel_open_ack test

---
+++ x/lightclient/ante/ibc_msg_channel_open_ack_test.go
@@ -0,0 +1,78 @@
+package ante_test
+
+import (
+	"testing"
+
+	ibcchanneltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+	keepertest "github.com/dymensionxyz/dymension/v3/testutil/keeper"
+	"github.com/dymensionxyz/dymension/v3/x/lightclient/ante"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandleMsgChannelOpenAck(t *testing.T) {
+	keeper, ctx := keepertest.LightClientKeeper(t)
+	rollappKeeper := NewMockRollappKeeper()
+	ibcclientKeeper := NewMockIBCClientKeeper()
+	ibcchannelKeeper := NewMockIBCChannelKeeper()
+	keeper.SetCanonicalClient(ctx, "rollapp-has-canon-client", "canon-client-id")
+	keeper.SetCanonicalClient(ctx, "rollapp-no-canon-channel", "canon-client-id-2")
+	ibcMsgDecorator := ante.NewIBCMessagesDecorator(*keeper, ibcclientKeeper, ibcchannelKeeper, rollappKeeper)
+	testCases := []struct {
+		name           string
+		inputMsg       ibcchanneltypes.MsgChannelOpenAck
+		err            error
+		canonClientSet bool
+	}{
+		{
+			name: "port id is not transfer port",
+			inputMsg: ibcchanneltypes.MsgChannelOpenAck{
+				PortId:    "not-transfer-port",
+				ChannelId: "channel-id",
+			},
+			err:            nil,
+			canonClientSet: false,
+		},
+		{
+			name: "channel not on a canonical client",
+			inputMsg: ibcchanneltypes.MsgChannelOpenAck{
+				PortId:    "transfer",
+				ChannelId: "non-canon-channel-id",
+			},
+			err:            nil,
+			canonClientSet: false,
+		},
+		{
+			name: "canonical channel already exists for rollapp",
+			inputMsg: ibcchanneltypes.MsgChannelOpenAck{
+				PortId:    "transfer",
+				ChannelId: "new-channel-on-canon-client",
+			},
+			err:            ibcchanneltypes.ErrChannelExists,
+			canonClientSet: false,
+		},
+		{
+			name: "canonical channel does not exist - set new channel as canonical",
+			inputMsg: ibcchanneltypes.MsgChannelOpenAck{
+				PortId:    "transfer",
+				ChannelId: "first-channel-on-canon-client",
+			},
+			err:            nil,
+			canonClientSet: true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := ibcMsgDecorator.HandleMsgChannelOpenAck(ctx, &tc.inputMsg)
+			if tc.err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+			}
+			if tc.canonClientSet {
+				rollapp, found := rollappKeeper.GetRollapp(ctx, "rollapp-no-canon-channel")
+				require.True(t, found)
+				require.Equal(t, tc.inputMsg.ChannelId, rollapp.ChannelId)
+			}
+		})
+	}
+}
---
+++ x/lightclient/ante/ibc_msgs_test.go
@@ -0,0 +1,107 @@
+package ante_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+
+	ibcconnectiontypes "github.com/cosmos/ibc-go/v7/modules/core/03-connection/types"
+	ibcchanneltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+	ibcsolomachine "github.com/cosmos/ibc-go/v7/modules/light-clients/06-solomachine"
+	ibctm "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
+	rollapptypes "github.com/dymensionxyz/dymension/v3/x/rollapp/types"
+)
+
+type MockRollappKeeper struct {
+	rollapps map[string]rollapptypes.Rollapp
+}
+
+func NewMockRollappKeeper() *MockRollappKeeper {
+	testRollappWithCanonChannel := rollapptypes.Rollapp{
+		RollappId: "rollapp-has-canon-client",
+		ChannelId: "channel-on-canon-client",
+	}
+	testRollappWithNoCanonChannel := rollapptypes.Rollapp{
+		RollappId: "rollapp-no-canon-channel",
+		ChannelId: "",
+	}
+	return &MockRollappKeeper{
+		rollapps: map[string]rollapptypes.Rollapp{
+			"rollapp-has-canon-client": testRollappWithCanonChannel,
+			"rollapp-no-canon-channel": testRollappWithNoCanonChannel,
+		},
+	}
+}
+
+func (m *MockRollappKeeper) GetRollapp(ctx sdk.Context, rollappId string) (val rollapptypes.Rollapp, found bool) {
+	val, found = m.rollapps[rollappId]
+	return val, found
+}
+
+func (m *MockRollappKeeper) SetRollapp(ctx sdk.Context, rollapp rollapptypes.Rollapp) {
+	m.rollapps[rollapp.RollappId] = rollapp
+}
+
+func (m *MockRollappKeeper) FindStateInfoByHeight(ctx sdk.Context, rollappId string, height uint64) (*rollapptypes.StateInfo, error) {
+	return nil, nil
+}
+
+func (m *MockRollappKeeper) GetStateInfo(ctx sdk.Context, rollappId string, index uint64) (val rollapptypes.StateInfo, found bool) {
+	return rollapptypes.StateInfo{}, false
+}
+
+type MockIBCCLientKeeper struct{}
+
+func NewMockIBCClientKeeper() *MockIBCCLientKeeper {
+	return &MockIBCCLientKeeper{}
+}
+
+func (m *MockIBCCLientKeeper) GetClientConsensusState(ctx sdk.Context, clientID string, height exported.Height) (exported.ConsensusState, bool) {
+	return nil, false
+}
+
+func (m *MockIBCCLientKeeper) GetClientState(ctx sdk.Context, clientID string) (exported.ClientState, bool) {
+	switch clientID {
+	case "non-tm-client-id":
+		clientState := ibcsolomachine.ClientState{}
+		return &clientState, true
+	case "canon-client-id":
+		clientState := ibctm.ClientState{
+			ChainId: "rollapp-has-canon-client",
+		}
+		return &clientState, true
+	}
+	return nil, false
+}
+
+func (m *MockIBCCLientKeeper) GenerateClientIdentifier(ctx sdk.Context, clientType string) string {
+	return ""
+}
+
+type MockIBCChannelKeeper struct{}
+
+func NewMockIBCChannelKeeper() *MockIBCChannelKeeper {
+	return &MockIBCChannelKeeper{}
+}
+
+func (m *MockIBCChannelKeeper) GetChannel(ctx sdk.Context, portID, channelID string) (channel ibcchanneltypes.Channel, found bool) {
+	return ibcchanneltypes.Channel{}, false
+}
+
+func (m *MockIBCChannelKeeper) GetChannelConnection(ctx sdk.Context, portID, channelID string) (string, exported.ConnectionI, error) {
+	if portID == "transfer" {
+		if channelID == "new-channel-on-canon-client" {
+			return "", ibcconnectiontypes.ConnectionEnd{
+				ClientId: "canon-client-id",
+			}, nil
+		}
+		if channelID == "first-channel-on-canon-client" {
+			return "", ibcconnectiontypes.ConnectionEnd{
+				ClientId: "canon-client-id-2",
+			}, nil
+		}
+		return "", ibcconnectiontypes.ConnectionEnd{
+			ClientId: "non-canon-client-id",
+		}, nil
+	}
+	return "", nil, nil
+}
