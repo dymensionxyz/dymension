0a13b9aab3e1124a458b69558e8d98298451fa33
keruch <verendeev.kirill.2000@gmail.com>
2024-08-21 18:14:48 +0200

    feat(streamer): added docstring

--- x/streamer/types/distr_info.go
+++ x/streamer/types/distr_info.go
@@ -36,8 +36,24 @@ func (r DistrRecord) ValidateBasic() error {
 	return nil
 }
 
-// DistrInfoFromDistribution converts sponsorship distribution to the DistrInfo type.
-// Returning an empty DistrInfo (with zero DistrInfo.TotalWeight) is a valid scenario.
+// DistrInfoFromDistribution converts sponsorship distribution to the DistrInfo type. Returning an empty
+// DistrInfo (with zero DistrInfo.TotalWeight) is a valid scenario. Note that some part of the distribution
+// might be abstained. In that case, the sum of gauge powers would be less than the total distribution weight.
+//
+// Example! Let's say we have the following distribution in the sponsorship:
+//
+//	Total: 100
+//	Gauge1: 30%
+//	Gauge2: 50%
+//	Abstained: 20%
+//
+// We want to distribute 100 DYM according to this distribution. Since 20% is abstained, we will normalize the figures
+// for Gauge1 and Gauge2. The total "active" voting power is 80%, which comes from 30% (Gauge1) + 50% (Gauge2).
+//
+//	Gauge1: 30% / 80% = 37.5% (in the new distribution)
+//	Gauge2: 50% / 80% = 62.5% (in the new distribution)
+//
+// So, Gauge1 gets 37.5 DYM, and Gauge2 gets 62.5 DYM.
 func DistrInfoFromDistribution(d sponsorshiptypes.Distribution) *DistrInfo {
 	totalWeight := math.ZeroInt()
 
