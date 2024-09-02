d84513819da17f0b6eabef60adab794eb4cd8690
danwt <30197399+danwt@users.noreply.github.com>
2024-09-02 12:40:46 +0100

    linter

--- x/lightclient/module.go
+++ x/lightclient/module.go
@@ -74,7 +74,9 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	if err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)); err != nil {
+		panic(err)
+	}
 }
 
 // GetTxCmd returns the module's root tx command.
