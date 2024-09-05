a30747c910064e52696bc298dbe91f13e0df1217
omritoptix <omritoptix@gmail.com>
2024-09-05 12:50:24 +0200

    Fixed sponsership gauge weight comment.

--- x/sponsorship/types/sponsorship.pb.go
+++ x/sponsorship/types/sponsorship.pb.go
@@ -271,7 +271,7 @@ type GaugeWeight struct {
 	// GaugeID is the ID of the gauge.
 	GaugeId uint64 `protobuf:"varint,1,opt,name=gauge_id,json=gaugeId,proto3" json:"gauge_id,omitempty"`
 	// Weight is a portion of the voting power that is allocated for the given
-	// gauge. The value must fall between Params.MinAllocationWeight and 100,
+	// gauge. The value must fall between Params.MinAllocationWeight and 100*10^18,
 	// inclusive.
 	Weight github_com_cosmos_cosmos_sdk_types.Int `protobuf:"bytes,2,opt,name=weight,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Int" json:"weight"`
 }
