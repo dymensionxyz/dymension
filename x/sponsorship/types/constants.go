8dfe1dcbab0fcaafeec7b1ef86a32ff478076ac4
keruch <verendeev.kirill.2000@gmail.com>
2024-09-05 10:45:28 +0200

    adym

--- x/sponsorship/types/constants.go
+++ x/sponsorship/types/constants.go
@@ -5,13 +5,15 @@ import (
 )
 
 var (
-	// DYM represents 1 DYM. Equals to 10^18 aDYM.
+	// ADYM represents 1 ADYM.
+	ADYM = math.NewInt(1)
+	// DYM represents 1 DYM. Equals to 10^18 ADYM.
 	DYM = math.NewIntWithDecimal(1, 18)
 
 	// MinAllocationWeight is a min weight one can allocate for the gauge.
 	// Equals 1 and is measured in percentages.
 	// 1 unit is 10^-18%.
-	MinAllocationWeight = math.NewInt(1)
+	MinAllocationWeight = ADYM
 	// MaxAllocationWeight is a max weight one can allocate for the gauge.
 	// Equals 100 * 10^18 and is measured in percentages.
 	// 1 unit is 10^-18%. 100 * 10^18 is 100%.
