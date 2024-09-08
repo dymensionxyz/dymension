70a02cb6850cec27c60434fd14e3cfb660817560
Michael Tsitrin <michael@dymension.xyz>
2024-09-08 16:26:27 +0300

    cli - create plan

---
+++ x/iro/cli/flags.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"time"
+
+	"github.com/dymensionxyz/dymension/v3/x/iro/types"
+	flag "github.com/spf13/pflag"
+)
+
+/*
+	BondingCurve    BondingCurve                           `protobuf:"bytes,4,opt,name=bonding_curve,json=bondingCurve,proto3" json:"bonding_curve"`
+	// The start time of the plan.
+	StartTime time.Time `protobuf:"bytes,5,opt,name=start_time,json=startTime,proto3,stdtime" json:"start_time"`
+	// The time before which the rollapp cannot be started.
+	PreLaunchTime time.Time `protobuf:"bytes,6,opt,name=pre_launch_time,json=preLaunchTime,proto3,stdtime" json:"pre_launch_time"`
+	// The incentive plan parameters for the tokens left after the plan is settled.
+	IncentivePlanParams IncentivePlanParams `protobuf:"bytes,7,opt,name=incentive_plan_params,json=incentivePlanParams,proto3" json:"incentive_plan_params"`
+}
+*/
+
+// Flags for streamer module tx commands.
+const (
+	FlagStartTime                              = "start-time"
+	FlagBondingCurve                           = "curve"
+	FlagIncentivesStartDurationAfterSettlement = "incentives-start"
+	FlagIncentivesEpochs                       = "incentives-epochs"
+)
+
+var (
+	defaultIncentivePlanParams_epochs = types.DefaultIncentivePlanMinimumParams.NumEpochsPaidOver
+	defaultIncentivePlanParams_start  = 7 * 24 * time.Hour
+)
+
+// FlagSetCreatePlan returns flags for creating gauges.
+func FlagSetCreatePlan() *flag.FlagSet {
+	fs := flag.NewFlagSet("", flag.ContinueOnError)
+
+	fs.String(FlagStartTime, "", "The start time of the plan. Default is the current time.")
+	fs.String(FlagBondingCurve, "", "The bonding curve parameters.")
+	fs.Duration(FlagIncentivesStartDurationAfterSettlement, defaultIncentivePlanParams_start, "The duration after the plan is settled to start the incentives.")
+	fs.Uint64(FlagIncentivesEpochs, defaultIncentivePlanParams_epochs, "The number of epochs for the incentives.")
+
+	return fs
+}
--- x/iro/cli/tx.go
+++ x/iro/cli/tx.go
@@ -1,23 +1,22 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
+	"cosmossdk.io/math"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+
 	// "github.com/cosmos/cosmos-sdk/client/flags"
+
 	"github.com/dymensionxyz/dymension/v3/x/iro/types"
-)
-
-// FIXME: implement!!
-
-var DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
-
-const (
-	flagPacketTimeoutTimestamp = "packet-timeout-timestamp"
-	listSeparator              = ","
 )
 
 // GetTxCmd returns the transaction commands for this module
@@ -30,7 +29,139 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(CmdCreateIRO())
 
 	return cmd
 }
+
+func CmdCreateIRO() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-iro [rollappId] [allocation] [pre-launch-time] --curve [curve params]",
+		Short: "Create a new IRO plan",
+		Example: `dymd create-iro [rollappId] [allocation] [pre-launch-time] --curve "1.2,0.4,0"
+        Optional:
+        --start-time [start-time]
+        --incentives-start [incentives-start]
+        --incentives-epochs [incentives-epochs]
+        `,
+		Args: cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			argRollappId := args[0]
+			argAllocation := args[1]
+			argPreLaunchTimeStr := args[2]
+
+			allocationAmt, ok := math.NewIntFromString(argAllocation)
+			if !ok {
+				return fmt.Errorf("invalid allocation amount: %s", argAllocation)
+			}
+
+			var preLaunchTime time.Time
+			if argPreLaunchTimeStr == "" {
+				return errors.New("pre-launch time cannot be empty")
+			} else if timeUnix, err := strconv.ParseInt(argPreLaunchTimeStr, 10, 64); err == nil { // unix time
+				preLaunchTime = time.Unix(timeUnix, 0)
+			} else if timeRFC, err := time.Parse(time.RFC3339, argPreLaunchTimeStr); err == nil { // RFC time
+				preLaunchTime = timeRFC
+			} else { // invalid input
+				return errors.New("invalid start time format")
+			}
+
+			// Parse curve flag
+			curveStr, err := cmd.Flags().GetString(FlagBondingCurve)
+			if err != nil {
+				return err
+			}
+			curve, err := ParseBondingCurve(curveStr)
+			if err != nil {
+				return errors.Join(types.ErrInvalidBondingCurve, err)
+			}
+
+			/* ----------------------------- optional flags ----------------------------- */
+			var startTime time.Time
+			timeStr, err := cmd.Flags().GetString(FlagStartTime)
+			if err != nil {
+				return err
+			}
+			if timeStr == "" { // empty start time
+				startTime = time.Unix(0, 0)
+			} else if timeUnix, err := strconv.ParseInt(timeStr, 10, 64); err == nil { // unix time
+				startTime = time.Unix(timeUnix, 0)
+			} else if timeRFC, err := time.Parse(time.RFC3339, timeStr); err == nil { // RFC time
+				startTime = timeRFC
+			} else { // invalid input
+				return errors.New("invalid start time format")
+			}
+
+			incentivesStart, err := cmd.Flags().GetDuration(FlagIncentivesStartDurationAfterSettlement)
+			if err != nil {
+				return err
+			}
+
+			incentivesEpochs, err := cmd.Flags().GetUint64(FlagIncentivesEpochs)
+			if err != nil {
+				return err
+			}
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.MsgCreatePlan{
+				Owner:           clientCtx.GetFromAddress().String(),
+				RollappId:       argRollappId,
+				AllocatedAmount: allocationAmt,
+				BondingCurve:    curve,
+				StartTime:       startTime,
+				PreLaunchTime:   preLaunchTime,
+				IncentivePlanParams: types.IncentivePlanParams{
+					StartTimeAfterSettlement: incentivesStart,
+					NumEpochsPaidOver:        incentivesEpochs,
+				},
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	cmd.Flags().AddFlagSet(FlagSetCreatePlan())
+
+	return cmd
+}
+
+// ParseBondingCurve parses the bonding curve string into a BondingCurve struct
+// expected format: "M,N,C" for p(x) = M * x^N + C
+func ParseBondingCurve(curveStr string) (types.BondingCurve, error) {
+	var curve types.BondingCurve
+
+	curveParams := strings.Split(curveStr, ",")
+	if len(curveParams) != 3 {
+		return curve, errors.New("invalid bonding curve parameters")
+	}
+
+	M, err := math.LegacyNewDecFromStr(curveParams[0])
+	if err != nil {
+		return curve, errors.New("invalid M parameter")
+	}
+
+	N, err := math.LegacyNewDecFromStr(curveParams[1])
+	if err != nil {
+		return curve, errors.New("invalid N parameter")
+	}
+
+	C, err := math.LegacyNewDecFromStr(curveParams[2])
+	if err != nil {
+		return curve, errors.New("invalid C parameter")
+	}
+
+	curve = types.NewBondingCurve(M, N, C)
+	return curve, curve.ValidateBasic()
+}
+
+// buy
+// sell
+// claim
