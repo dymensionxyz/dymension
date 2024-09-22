678e567d739bbc06b25a2316cef347f5f179b959
Itzhak Bokris <jzak300@gmail.com>
2024-09-22 11:17:32 +0300

    feat: Register incentives and sponsorship amino message to support eip712 (#1240)
    (cherry picked from commit bb2cd8927211109d225800663ef21ce04c0e1b70)

--- x/sponsorship/types/codec.go
+++ x/sponsorship/types/codec.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
 )
 
 // RegisterCodec registers the necessary x/streamer interfaces and concrete types on the provided
@@ -24,4 +25,16 @@ func RegisterInterfaces(reg types.InterfaceRegistry) {
 		&MsgUpdateParams{},
 	)
 	msgservice.RegisterMsgServiceDesc(reg, &_Msg_serviceDesc)
+}
+
+var Amino = codec.NewLegacyAmino()
+
+func init() {
+	RegisterCodec(Amino)
+	// Register all Amino interfaces and concrete types on the authz Amino codec so that this can later be
+	// used to properly serialize MsgGrant and MsgExec instances
+	sdk.RegisterLegacyAminoCodec(Amino)
+	RegisterCodec(authzcodec.Amino)
+
+	Amino.Seal()
 }
