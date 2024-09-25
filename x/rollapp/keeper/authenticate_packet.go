9ae857b16e47e4bb35670196f38104af7d9ce7a8
danwt <30197399+danwt@users.noreply.github.com>
2024-09-25 09:10:27 +0100

    fix tests

--- x/rollapp/keeper/authenticate_packet.go
+++ x/rollapp/keeper/authenticate_packet.go
@@ -4,6 +4,7 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
