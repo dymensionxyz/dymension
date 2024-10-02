5f99b684964a9af2d16edc1e1e71f12a2f12555a
VictorTrustyDev <me@victortrusty.dev>
2024-10-03 04:18:00 +0700

    TODO remove | call migration for Playground store update

--- x/dymns/module.go
+++ x/dymns/module.go
@@ -136,7 +136,10 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
-func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
+func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
+	// TODO DymNS: delete this
+	am.keeper.BeginBlockMigrationForPlayground(ctx)
+}
 
 // EndBlock contains the logic that is automatically triggered at the end of each block
 func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
