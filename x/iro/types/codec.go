d3c31fdc4ea83b1da0842b02df3f384c663a2935
Itzhak Bokris <jzak300@gmail.com>
2024-10-06 18:45:16 +0300

    Add EIP712 support for create-IRO-plan message (#1287)
    Co-authored-by: Michael Tsitrin <michael@dymension.xyz>

--- x/iro/types/codec.go
+++ x/iro/types/codec.go
@@ -5,12 +5,14 @@ import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgBuy{}, "iro/Buy", nil)
 	cdc.RegisterConcrete(&MsgSell{}, "iro/Sell", nil)
 	cdc.RegisterConcrete(&MsgClaim{}, "iro/Claim", nil)
+	cdc.RegisterConcrete(&MsgCreatePlan{}, "iro/CreatePlan", nil)
 	cdc.RegisterConcrete(&MsgUpdateParams{}, "iro/UpdateParams", nil)
 }
 
@@ -20,6 +22,7 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 		&MsgBuy{},
 		&MsgSell{},
 		&MsgClaim{},
+		&MsgCreatePlan{},
 		&MsgUpdateParams{},
 	)
 
@@ -28,5 +31,15 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 
 var (
 	Amino     = codec.NewLegacyAmino()
-	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+	ModuleCdc = codec.NewAminoCodec(Amino)
 )
+
+func init() {
+	RegisterCodec(Amino)
+	// Register all Amino interfaces and concrete types on the authz Amino codec so that this can later be
+	// used to properly serialize MsgGrant and MsgExec instances
+	sdk.RegisterLegacyAminoCodec(Amino)
+	RegisterCodec(authzcodec.Amino)
+
+	Amino.Seal()
+}
