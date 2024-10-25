1412d58ca45ae903bb722083c0b64cbbac0fd96e
danwt <30197399+danwt@users.noreply.github.com>
2024-10-25 12:19:43 +0100

    tests make sense

--- x/sequencer/keeper/msg_server_create_sequencer_test.go
+++ x/sequencer/keeper/msg_server_create_sequencer_test.go
@@ -1,5 +1,11 @@
 package keeper_test
 
+import (
+	"github.com/dymensionxyz/gerr-cosmos/gerrc"
+	"github.com/dymensionxyz/sdk-utils/utils/urand"
+	"github.com/dymensionxyz/sdk-utils/utils/utest"
+)
+
 // On success, we should get back an object with all the right info
 func (s *SequencerTestSuite) TestCreateSequencerBasic() {
 	ra := s.createRollapp()
@@ -17,11 +23,21 @@ func (s *SequencerTestSuite) TestCreateSequencerBasic() {
 // There are several reasons to reject creation
 func (s *SequencerTestSuite) TestCreateSequencerRestrictions() {
 	ra := s.createRollapp()
-	s.fundSequencer(alice, bond)
-	msg := createSequencerMsg(ra.RollappId, alice)
-	msg.Bond = bond
-	_, err := s.msgServer.CreateSequencer(s.Ctx, &msg)
-	s.Require().NoError(err)
-	_, err = s.k().GetRealSequencer(s.Ctx, pkAddr(alice))
-	s.Require().Error(err)
+
+	s.Run("no rollapp", func() {
+		s.fundSequencer(alice, bond)
+		msg := createSequencerMsg(urand.RollappID(), alice)
+		msg.Bond = bond
+		_, err := s.msgServer.CreateSequencer(s.Ctx, &msg)
+		utest.IsErr(s.Require(), err, gerrc.ErrNotFound)
+	})
+	s.Run("already exist", func() {
+		s.fundSequencer(alice, bond)
+		msg := createSequencerMsg(ra.RollappId, alice)
+		msg.Bond = bond
+		_, err := s.msgServer.CreateSequencer(s.Ctx, &msg)
+		s.Require().NoError(err)
+		_, err = s.msgServer.CreateSequencer(s.Ctx, &msg)
+		utest.IsErr(s.Require(), err, gerrc.ErrAlreadyExists)
+	})
 }
