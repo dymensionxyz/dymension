587f8bcc04088a9a42249b29c7284a8c72fc1345
danwt <30197399+danwt@users.noreply.github.com>
2024-10-25 15:53:34 +0100

    start to write a basic rotation test

--- x/sequencer/keeper/rotation_test.go
+++ x/sequencer/keeper/rotation_test.go
@@ -1,1 +1,9 @@
 package keeper_test
+
+func (s *SequencerTestSuite) TestRotationHappyFlow() {
+	ra := s.createRollapp()
+	seqAlice := s.createSequencerWithBond(s.Ctx, ra.RollappId, alice, bond)
+	seqBob := s.createSequencerWithBond(s.Ctx, ra.RollappId, alice, bond)
+	s.Require().True(s.k().IsProposer(s.Ctx, seqAlice))
+	s.Require().False(s.k().IsSuccessor(s.Ctx, seqBob))
+}
