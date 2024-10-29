1bf8a9cdb708308f8aa0c075a30231b070c68044
danwt <30197399+danwt@users.noreply.github.com>
2024-10-29 18:22:57 +0000

    remove from genesis

--- x/lightclient/keeper/genesis.go
+++ x/lightclient/keeper/genesis.go
@@ -5,6 +5,8 @@ import (
 	"github.com/dymensionxyz/dymension/v3/x/lightclient/types"
 )
 
+// TODO: add signer bookkeeping to genesis and do import/export
+
 func (k Keeper) InitGenesis(ctx sdk.Context, genesisState types.GenesisState) {
 	if err := genesisState.Validate(); err != nil {
 		panic(err)
@@ -12,16 +14,12 @@ func (k Keeper) InitGenesis(ctx sdk.Context, genesisState types.GenesisState) {
 	for _, client := range genesisState.GetCanonicalClients() {
 		k.SetCanonicalClient(ctx, client.RollappId, client.IbcClientId)
 	}
-	for _, stateSigner := range genesisState.GetConsensusStateSigners() {
-		k.SetConsensusStateValHash(ctx, stateSigner.IbcClientId, stateSigner.Height, []byte(stateSigner.BlockValHash))
-	}
+
 }
 
 func (k Keeper) ExportGenesis(ctx sdk.Context) types.GenesisState {
 	clients := k.GetAllCanonicalClients(ctx)
-	stateSigners := k.GetAllConsensusStateSigners(ctx)
 	return types.GenesisState{
-		CanonicalClients:      clients,
-		ConsensusStateSigners: stateSigners,
+		CanonicalClients: clients,
 	}
 }
