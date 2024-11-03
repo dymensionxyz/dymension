13a56a59f2640c2618d759c520e47e4ec6e49263
Michael Tsitrin <michael@dymension.xyz>
2024-11-03 15:05:12 +0200

    linter

--- x/delayedack/keeper/fraud.go
+++ x/delayedack/keeper/fraud.go
@@ -41,7 +41,10 @@ func (k Keeper) HandleHardFork(ctx sdk.Context, rollappID string, height uint64,
 		}
 
 		// delete the packet
-		k.DeleteRollappPacket(ctx, &rollappPacket) // nolint: errcheck
+		err := k.DeleteRollappPacket(ctx, &rollappPacket)
+		if err != nil {
+			logger.Error("failed to delete reverted packet", append(logContext, "error", err.Error())...)
+		}
 		logger.Debug("reverted IBC rollapp packet", logContext...)
 	}
 
