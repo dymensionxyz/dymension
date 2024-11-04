ef4afb1a101a0aa80c7438f926ec56c5bcadf381
Michael Tsitrin <michael@dymension.xyz>
2024-11-04 14:56:27 +0200

    register proposal handler

--- x/rollapp/keeper/fraud_proposal.go
+++ x/rollapp/keeper/fraud_proposal.go
@@ -1,56 +1,66 @@
 package keeper
 
 import (
-	errorsmod "cosmossdk.io/errors"
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"context"
 
+	errorsmod "cosmossdk.io/errors"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
 // FraudProposalHandler handles the submission of a fraud proposal
 // The fraud proposal can be submitted by the gov module
-func (k Keeper) FraudProposalHandler(ctx sdk.Context, msg types.MsgRollappFraudProposal) error {
+func (k Keeper) SubmitRollappFraud(goCtx context.Context, msg *types.MsgRollappFraudProposal) (*types.MsgRollappFraudProposalResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
 	if msg.Authority != k.authority {
-		return errorsmod.Wrap(gerrc.ErrUnauthenticated, "only the gov module can submit fraud proposals")
+		return nil, errorsmod.Wrap(gerrc.ErrUnauthenticated, "only the gov module can submit fraud proposals")
 	}
 
 	if err := msg.ValidateBasic(); err != nil {
-		return errorsmod.Wrap(gerrc.ErrInvalidArgument, "invalid msg")
+		return nil, errorsmod.Wrap(gerrc.ErrInvalidArgument, "invalid msg")
 	}
 
 	rollapp, found := k.GetRollapp(ctx, msg.RollappId)
 	if !found {
-		return errorsmod.Wrap(gerrc.ErrNotFound, "rollapp not found")
+		return nil, errorsmod.Wrap(gerrc.ErrNotFound, "rollapp not found")
 	}
 	// check revision number
 	if rollapp.RevisionNumber != msg.RollappRevision {
-		return errorsmod.Wrap(gerrc.ErrFailedPrecondition, "revision number mismatch")
+		return nil, errorsmod.Wrap(gerrc.ErrFailedPrecondition, "revision number mismatch")
 	}
 
 	// validate the rollapp is past it's genesis bridge phase
 	if !rollapp.IsTransferEnabled() {
-		return errorsmod.Wrap(gerrc.ErrFailedPrecondition, "rollapp is not past genesis bridge phase")
+		return nil, errorsmod.Wrap(gerrc.ErrFailedPrecondition, "rollapp is not past genesis bridge phase")
 	}
 
 	// validate the fraud height is already committed
 	sinfo, found := k.GetLatestStateInfo(ctx, msg.RollappId)
 	if !found || sinfo.GetLatestHeight() < msg.FraudHeight {
-		return errorsmod.Wrap(gerrc.ErrFailedPrecondition, "fraud height not committed")
+		return nil, errorsmod.Wrap(gerrc.ErrFailedPrecondition, "fraud height not committed")
 	}
 
 	// check wether the fraud height is already finalized
 	sinfo, found = k.GetLatestFinalizedStateInfo(ctx, msg.RollappId)
 	if found && sinfo.GetLatestHeight() >= msg.FraudHeight {
-		return errorsmod.Wrap(gerrc.ErrFailedPrecondition, "fraud height already finalized")
+		return nil, errorsmod.Wrap(gerrc.ErrFailedPrecondition, "fraud height already finalized")
 	}
 
 	// jail the sequencer if needed
 	if msg.SlashSequencerAddress != "" {
 		err := k.sequencerKeeper.JailByAddr(ctx, msg.SlashSequencerAddress)
 		if err != nil {
-			return errorsmod.Wrap(err, "jail sequencer")
+			return nil, errorsmod.Wrap(err, "jail sequencer")
 		}
 	}
-	return k.HardFork(ctx, msg.RollappId, msg.FraudHeight)
+
+	err := k.HardFork(ctx, msg.RollappId, msg.FraudHeight)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "hard fork")
+	}
+
+	return &types.MsgRollappFraudProposalResponse{}, nil
 }
--- x/rollapp/module.go
+++ x/rollapp/module.go
@@ -127,6 +127,7 @@ func (am AppModule) Name() string {
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
+	types.RegisterProposalMsgServer(cfg.MsgServer(), am.keeper)
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(*am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
