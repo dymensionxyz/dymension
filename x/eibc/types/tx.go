a097b766a91b62b1c8fbb799846f26c61d636109
Itzhak Bokris <jzak300@gmail.com>
2024-11-05 15:01:17 +0200

    Register eibc and delayedack message in amino codec

--- x/eibc/types/tx.go
+++ x/eibc/types/tx.go
@@ -3,6 +3,7 @@ package types
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
 
 	errorsmod "cosmossdk.io/errors"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -13,6 +14,9 @@ import (
 var (
 	_ = sdk.Msg(&MsgFulfillOrder{})
 	_ = sdk.Msg(&MsgUpdateDemandOrder{})
+
+	_ legacytx.LegacyMsg = &MsgFulfillOrder{}
+	_ legacytx.LegacyMsg = &MsgFulfillOrderAuthorized{}
 )
 
 func NewMsgFulfillOrder(fulfillerAddress, orderId, expectedFee string) *MsgFulfillOrder {
