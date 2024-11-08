fed67788452c1f46c36f0db0076284ef92a289a2
danwt <30197399+danwt@users.noreply.github.com>
2024-11-08 11:12:52 +0000

    adds rewardee

--- x/rollapp/types/message_fraud_proposal.go
+++ x/rollapp/types/message_fraud_proposal.go
@@ -31,6 +31,15 @@ func (m *MsgRollappFraudProposal) ValidateBasic() error {
 	return nil
 }
 
+// Returns acc address if rewardee field is not empty
+func (m *MsgRollappFraudProposal) MustRewardee() *sdk.AccAddress {
+	if m.Rewardee == "" {
+		return nil
+	}
+	rewardee, _ := sdk.AccAddressFromBech32(m.Rewardee)
+	return &rewardee
+}
+
 // GetSigners returns the required signers for the MsgRollappFraudProposal.
 func (m *MsgRollappFraudProposal) GetSigners() []sdk.AccAddress {
 	authority, err := sdk.AccAddressFromBech32(m.Authority)
