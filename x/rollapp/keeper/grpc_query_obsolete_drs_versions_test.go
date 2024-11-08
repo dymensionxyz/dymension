46b58d89c4a4eec08e9df9cd82908d04b20d90d2
keruch <verendeev.kirill.2000@gmail.com>
2024-11-08 16:25:50 +0100

    feat(rollapp): obsolete DRS versions query

---
+++ x/rollapp/keeper/grpc_query_obsolete_drs_versions_test.go
@@ -0,0 +1,27 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	testkeeper "github.com/dymensionxyz/dymension/v3/testutil/keeper"
+	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
+)
+
+func TestObsoleteDRSVersionsQuery(t *testing.T) {
+	keeper, ctx := testkeeper.RollappKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+
+	const obsoleteDRSVersion uint32 = 1234567890
+	err := keeper.SetVulnerableDRSVersion(ctx, obsoleteDRSVersion)
+	require.NoError(t, err)
+
+	response, err := keeper.ObsoleteDRSVersions(wctx, &types.QueryObsoleteDRSVersionsRequest{})
+	require.NoError(t, err)
+
+	expected, err := keeper.GetAllVulnerableDRSVersions(ctx)
+	require.NoError(t, err)
+	require.EqualValues(t, expected, response.DrsVersions)
+}
