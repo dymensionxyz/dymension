2f091aaead885c7f48766bc2c6cfe435e6917e74
danwt <30197399+danwt@users.noreply.github.com>
2024-11-09 15:56:43 +0000

    set seq consensus ver to 2

--- x/sequencer/module.go
+++ x/sequencer/module.go
@@ -153,7 +153,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 3 }
+func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
