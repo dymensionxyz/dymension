5eb90fa4f2e6d387f19940ee4a884ee3ccb1a504
danwt <30197399+danwt@users.noreply.github.com>
2024-11-19 18:01:22 +0000

    impl sequencer , but need to add more context

--- x/sequencer/keeper/invariants.go
+++ x/sequencer/keeper/invariants.go
@@ -9,19 +9,21 @@ import (
 	"github.com/dymensionxyz/dymension/v3/x/sequencer/types"
 )
 
-var invars = invar.NamedFuncsList[Keeper]{
-	{"sequencers-count", SequencersCountInvariant},
-	{"sequencers-proposer-bonded", ProposerBondedInvariant},
+var invs = invar.NamedFuncsList[Keeper]{
+	{"notice", InvariantNotice},
+	{"hash-index", InvariantHashIndex},
+	{"status", InvariantStatus},
+	{"tokens", InvariantTokens},
 }
 
 // RegisterInvariants registers the sequencer module invariants
 func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
-	invars.RegisterInvariants(types.ModuleName, ir, k)
+	invs.RegisterInvariants(types.ModuleName, ir, k)
 }
 
 // DO NOT DELETE
 func AllInvariants(k Keeper) sdk.Invariant {
-	return invars.All(types.ModuleName, k)
+	return invs.All(types.ModuleName, k)
 }
 
 func InvariantNotice(k Keeper) invar.Func {
@@ -39,61 +41,91 @@ func InvariantNotice(k Keeper) invar.Func {
 	}
 }
 
-func SequencersCountInvariant(k Keeper) invar.Func {
+func InvariantHashIndex(k Keeper) invar.Func {
 	return func(ctx sdk.Context) (error, bool) {
-		var (
-			broken bool
-			msg    string
-		)
-
-		sequencers := k.AllSequencers(ctx)
-		rollapps := k.rollappKeeper.GetAllRollapps(ctx)
-
-		totalCount := 0
-		for _, rollapp := range rollapps {
-			seqByRollapp := k.RollappSequencers(ctx, rollapp.RollappId)
-			bonded := k.RollappSequencersByStatus(ctx, rollapp.RollappId, types.Bonded)
-			unbonded := k.RollappSequencersByStatus(ctx, rollapp.RollappId, types.Unbonded)
-
-			if len(seqByRollapp) != len(bonded)+len(unbonded) {
-				broken = true
-				msg += "sequencer by rollapp length is not equal to sum of bonded, and unbonded " + rollapp.RollappId + "\n"
+		for _, exp := range k.AllSequencers(ctx) {
+			got, err := k.SequencerByDymintAddr(ctx, exp.MustValsetHash())
+			if err != nil {
+				return err, true
 			}
-
-			totalCount += len(seqByRollapp)
+			if got.Address != exp.Address {
+				return errors.New("address mismatch"), true
+			}
 		}
-
-		if totalCount != len(sequencers) {
-			broken = true
-			msg += "total sequencer count is not equal to sum of sequencers by rollapp\n"
-		}
-
-		return errors.New(msg), broken
+		return nil, false
 	}
 }
 
-// ProposerBondedInvariant checks if the proposer and next proposer are bonded as expected
-func ProposerBondedInvariant(k Keeper) invar.Func {
+func InvariantStatus(k Keeper) invar.Func {
 	return func(ctx sdk.Context) (error, bool) {
-		var (
-			broken bool
-			msg    string
-		)
-
 		rollapps := k.rollappKeeper.GetAllRollapps(ctx)
-		for _, rollapp := range rollapps {
-			proposer := k.GetProposer(ctx, rollapp.RollappId)
+		for _, ra := range rollapps {
+			proposer := k.GetProposer(ctx, ra.RollappId)
 			if !proposer.Bonded() {
-				broken = true
-				msg += "proposer is not bonded " + rollapp.RollappId + "\n"
+				return errors.New("proposer not bonded"), true
 			}
-			successor := k.GetSuccessor(ctx, rollapp.RollappId)
+			successor := k.GetSuccessor(ctx, ra.RollappId)
 			if !successor.Bonded() {
-				broken = true
-				msg += "successor is not bonded " + rollapp.RollappId + "\n"
+				return errors.New("successor not bonded"), true
 			}
-
+			if !proposer.Sentinel() && proposer.Address == successor.Address {
+				return errors.New("proposer and successor are the same"), true
+			}
+			if !successor.Sentinel() && proposer.Sentinel() {
+				return errors.New("proposer is sentinel but successor is not"), true
+			}
+			all := k.RollappSequencers(ctx, ra.RollappId)
+			bonded := k.RollappSequencersByStatus(ctx, ra.RollappId, types.Bonded)
+			unbonded := k.RollappSequencersByStatus(ctx, ra.RollappId, types.Unbonded)
+			if len(all) != len(bonded)+len(unbonded) {
+				return errors.New("sequencer by rollapp length is not equal to sum of bonded, and unbonded"), true
+			}
 		}
-		return errors.New(msg), broken
+		for _, seq := range k.AllProposers(ctx) {
+			if !k.IsProposer(ctx, seq) {
+				return errors.New("sequencer in proposers query is not proposer"), true
+			}
+		}
+		for _, seq := range k.AllSuccessors(ctx) {
+			if !k.IsSuccessor(ctx, seq) {
+				return errors.New("sequencer in successor query is not successor"), true
+			}
+		}
+		return nil, false
+	}
+}
+
+func InvariantTokens(k Keeper) invar.Func {
+	return func(ctx sdk.Context) (error, bool) {
+
+		for _, seq := range k.AllSequencers(ctx) {
+			if err := seq.ValidateBasic(); err != nil {
+				return err, true
+			}
+			if err := k.validBondDenom(ctx, seq.TokensCoin()); err != nil {
+				return err, true
+			}
+			if seq.TokensCoin().Amount.IsNegative() {
+				return errors.New("negative seq tokens"), true
+			}
+		}
+
+		total := sdk.NewCoin(k.bondDenom(ctx), sdk.ZeroInt())
+		for _, seq := range k.AllSequencers(ctx) {
+			total = total.Add(seq.TokensCoin())
+		}
+		// check module balance is equal
+		moduleAcc := k.accountK.GetModuleAccount(ctx, types.ModuleName)
+		balances := k.bankKeeper.GetAllBalances(ctx, moduleAcc.GetAddress())
+		if 1 < len(balances) {
+			return errors.New("module account has more than one coin"), true
+		}
+		if !total.IsZero() && len(balances) == 0 {
+			return errors.New("module account has no balance"), true
+		}
+		if !total.IsZero() && !balances[0].IsEqual(total) {
+			return errors.New("module account balance not equal to sum of sequencer tokens"), true
+		}
+		return nil, false
 	}
 }
