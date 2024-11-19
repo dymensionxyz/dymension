273d396bb35979f8579d5d47b0d1fb5c8f5ec267
danwt <30197399+danwt@users.noreply.github.com>
2024-11-19 18:06:23 +0000

    add the template for sponsorship

--- x/sponsorship/keeper/invariants.go
+++ x/sponsorship/keeper/invariants.go
@@ -2,20 +2,42 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dymensionxyz/dymension/v3/utils/invar"
 
 	"github.com/dymensionxyz/dymension/v3/x/sponsorship/types"
 )
 
-func RegisterInvariants(ir sdk.InvariantRegistry, keeper Keeper) {
-	ir.RegisterRoute(types.ModuleName, "gauge-weights", GaugeWeightsInvariant(keeper))
+var invs = invar.NamedFuncsList[Keeper]{
+	{"delegator-validator-power", InvariantDelegatorValidatorPower},
+	{"distribution", InvariantDistribution},
+	{"votes", InvariantVotes},
+	{"general", InvariantGeneral},
 }
 
-func GaugeWeightsInvariant(Keeper) sdk.Invariant {
-	// TODO
-	return func(ctx sdk.Context) (string, bool) {
-		var broken bool
-		var msg string
+// RegisterInvariants registers the sequencer module invariants
+func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
+	invs.RegisterInvariants(types.ModuleName, ir, k)
+}
 
-		return sdk.FormatInvariant(types.ModuleName, "gauge-weights", msg), broken
+// DO NOT DELETE
+func AllInvariants(k Keeper) sdk.Invariant {
+	return invs.All(types.ModuleName, k)
+}
+
+func InvariantDelegatorValidatorPower(k Keeper) invar.Func {
+	return func(ctx sdk.Context) (error, bool) {
+	}
+}
+
+func InvariantDistribution(k Keeper) invar.Func {
+	return func(ctx sdk.Context) (error, bool) {
+	}
+}
+func InvariantVotes(k Keeper) invar.Func {
+	return func(ctx sdk.Context) (error, bool) {
+	}
+}
+func InvariantGeneral(k Keeper) invar.Func {
+	return func(ctx sdk.Context) (error, bool) {
 	}
 }
