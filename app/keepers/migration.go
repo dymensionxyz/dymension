5594de6eae4b62cb6f8160654ef63b43eb4f7e93
danwt <30197399+danwt@users.noreply.github.com>
2024-11-20 18:04:34 +0000

    move nolint

--- app/keepers/migration.go
+++ app/keepers/migration.go
@@ -15,11 +15,11 @@ func (a *AppKeepers) MigrateModuleAccountPerms(ctx sdk.Context) {
 	for _, moduleName := range keys {
 		perms := maccPerms[moduleName]
 
-		//nolint:all - we want to panic here
 		accI := a.AccountKeeper.GetModuleAccount(ctx, moduleName)
 		if accI == nil {
 			panic(fmt.Sprintf("module account not been set: %s", moduleName))
 		}
+		//nolint:all - we want to panic here
 		acc := accI.(*authtypes.ModuleAccount)
 		acc.Permissions = perms
 		a.AccountKeeper.SetModuleAccount(ctx, acc)
