6a5074496297a03b85b2ded9aaa1e2a3125f0ec5
danwt <30197399+danwt@users.noreply.github.com>
2024-11-20 10:55:12 +0000

    restore main opt out on unbond

--- x/sequencer/keeper/msg_server_bond.go
+++ x/sequencer/keeper/msg_server_bond.go
@@ -68,6 +68,11 @@ func (k msgServer) Unbond(goCtx context.Context, msg *types.MsgUnbond) (*types.M
 		return nil, gerrc.ErrFailedPrecondition.Wrap("cannot unbond while rotation in progress")
 	}
 
+	// ensures they will not get chosen as their own successor!
+	if err := seq.SetOptedIn(ctx, false); err != nil {
+		return nil, err
+	}
+
 	// now we know they are proposer
 	// avoid starting another notice unnecessarily
 	if k.IsProposer(ctx, seq) {
