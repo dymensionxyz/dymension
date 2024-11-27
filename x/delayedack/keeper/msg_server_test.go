7aef2dba0980929674792037d90f510c32616eba
danwt <30197399+danwt@users.noreply.github.com>
2024-11-27 13:36:32 +0000

    why

--- x/delayedack/keeper/msg_server_test.go
+++ x/delayedack/keeper/msg_server_test.go
@@ -1,1 +1,27 @@
 package keeper
+
+import (
+	"fmt"
+	"testing"
+
+	commontypes "github.com/dymensionxyz/dymension/v3/x/common/types"
+)
+
+func TestFoo(t *testing.T) {
+	key := `\0\x01/mande_18071918-1/\0\0\0\0\0\x12k�/ON_RECV/channel-0/\0\0\0\0\0\0��`
+	packetKey, err := commontypes.DecodePacketKey(key)
+	if err != nil {
+		panic(fmt.Errorf("failed to decode base64 packet key: %w", err))
+	}
+	t.Log(packetKey)
+
+	/*
+		status Status,
+		rollappID string,
+		proofHeight uint64,
+		packetType RollappPacket_Type,
+		packetSrcChannel string,
+		packetSequence uint64,
+	*/
+
+}
