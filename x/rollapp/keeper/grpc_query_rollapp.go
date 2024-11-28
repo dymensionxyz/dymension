b31336399003094f955cc069283782e422461b8b
zale144 <aleksandar.sukovic@gmail.com>
2024-11-28 19:58:44 +0100

    PR review fix

--- x/rollapp/keeper/grpc_query_rollapp.go
+++ x/rollapp/keeper/grpc_query_rollapp.go
@@ -96,6 +96,8 @@ func getSummaryResponse(ctx sdk.Context, k Keeper, rollapp types.Rollapp, ok, wi
 	return resp, nil
 }
 
+// Tags returns all the tags available for rollapps.
+// The order is non-deterministic.
 func (k Keeper) Tags(context.Context, *types.QueryTagsRequest) (*types.QueryTagsResponse, error) {
 	tags := make([]string, 0, len(types.RollappTags))
 	for tag := range types.RollappTags {
