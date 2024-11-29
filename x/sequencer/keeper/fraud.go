12af67aa2d178cdfb3054abaff676778d6ea9871
danwt <30197399+danwt@users.noreply.github.com>
2024-11-29 15:54:30 +0000

    remove an extra call

--- x/sequencer/keeper/fraud.go
+++ x/sequencer/keeper/fraud.go
@@ -23,7 +23,6 @@ func (k Keeper) TryKickProposer(ctx sdk.Context, kicker types.Sequencer) error {
 	// clear the proposer
 	k.abruptRemoveProposer(ctx, ra)
 
-	k.unbond(ctx, &proposer)
 	k.SetSequencer(ctx, proposer)
 
 	// This will call hard fork on the rollapp, which will also optOut all sequencers
