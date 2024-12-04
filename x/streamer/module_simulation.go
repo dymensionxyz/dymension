5da9e26de285c0a7c482ae0ea7f25f190ddd239e
danwt <30197399+danwt@users.noreply.github.com>
2024-12-04 12:22:08 +0000

    how on earth do proposal simulations work

--- x/streamer/module_simulation.go
+++ x/streamer/module_simulation.go
@@ -1,8 +1,6 @@
 package streamer
 
 import (
-	"math/rand"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
@@ -16,33 +14,22 @@ import (
 // AppModuleSimulation
 // ----------------------------------------------------------------------------
 
-// GenerateGenesisState creates a randomized GenState of x/incentives.
 func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
 	simulation.RandomizedGenState(simState)
 }
 
-// RegisterStoreDecoder registers a decoder for supply module's types.
 func (am AppModule) RegisterStoreDecoder(sdr sdk.StoreDecoderRegistry) {
 	sdr[types.ModuleName] = collcompat.NewStoreDecoderFuncFromCollectionsSchema(am.keeper.Schema())
 }
 
-// WeightedOperations returns the all the module's operations with their respective weights.
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	return simulation.WeightedOperations(simState.AppParams, simState.Cdc, am.keeper)
 }
 
-// ProposalContents returns all the gov content functions used to
-// simulate governance proposals.
 func (AppModule) ProposalContents(simState module.SimulationState) []simtypes.WeightedProposalContent { //nolint:staticcheck
-	return simulation.ProposalContents()
-}
-
-// ProposalMsgs returns all the gov msgs used to simulate governance proposals.
-func (AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.WeightedProposalMsg {
-	return simulation.ProposalMsgs()
-}
-
-// RandomizedParams creates randomized fee market param changes for the simulator.
-func (AppModule) RandomizedParams(_ *rand.Rand) []simtypes.ParamChange {
 	return nil
+}
+
+func (AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.WeightedProposalMsg {
+	return simulation.ProposalMsgs(simState.AppParams, simState.Cdc, am.keeper)
 }
--- x/streamer/simulation/operations.go
+++ x/streamer/simulation/operations.go
@@ -1,46 +1,72 @@
 package simulation
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 
 	"cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 
 	dymsimtypes "github.com/dymensionxyz/dymension/v3/simulation/types"
+	"github.com/dymensionxyz/dymension/v3/x/streamer/keeper"
 	"github.com/dymensionxyz/dymension/v3/x/streamer/types"
 )
 
 // Simulation operation weights constants
 const (
-	OpWeightSubmitProposal  = "op_weight_submit_proposal"
-	OpWeightVoteProposal    = "op_weight_vote_proposal"
-	OpWeightMsgUpdateParams = "op_weight_msg_update_params"
-
-	DefaultWeightSubmitProposal      = 60
-	DefaultWeightVoteProposal        = 40
+	OpWeightMsgUpdateParams          = "op_weight_msg_update_params"
 	DefaultWeightMsgUpdateParams int = 10
+
+	OpCreateStreamProposal                  = "op_create_stream_proposal"
+	OpTerminateStreamProposal               = "op_terminate_stream_proposal"
+	OpReplaceStreamDistributionProposal     = "op_replace_stream_distribution_proposal"
+	OpUpdateStreamDistributionProposal      = "op_update_stream_distribution_proposal"
+	WeightCreateStreamProposal              = 100
+	WeightTerminateStreamProposal           = 100
+	WeightReplaceStreamDistributionProposal = 100
+	WeightUpdateStreamDistributionProposal  = 100
 )
 
-// ProposalMsgs defines the module weighted proposals' contents
-func ProposalMsgs() []simtypes.WeightedProposalMsg {
+func ProposalMsgs(
+	appParams simtypes.AppParams,
+	cdc codec.JSONCodec,
+	k keeper.Keeper,
+) []simtypes.WeightedProposalMsg {
 	return []simtypes.WeightedProposalMsg{
 		simulation.NewWeightedProposalMsg(
 			OpWeightMsgUpdateParams,
 			DefaultWeightMsgUpdateParams,
 			SimulateMsgUpdateParams,
 		),
+		simulation.NewWeightedProposalMsg(
+			OpCreateStreamProposal,
+			WeightCreateStreamProposal,
+			SimulateCreateStreamProposal,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpTerminateStreamProposal,
+			WeightTerminateStreamProposal,
+			SimulateTerminateStreamProposal,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpReplaceStreamDistributionProposal,
+			WeightReplaceStreamDistributionProposal,
+			SimulateReplaceStreamDistributionProposal,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpUpdateStreamDistributionProposal,
+			WeightUpdateStreamDistributionProposal,
+			SimulateUpdateStreamDistributionProposal,
+		),
 	}
 }
 
-// SimulateMsgUpdateParams returns a random MsgUpdateParams
 func SimulateMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
 	// use the default gov module account address as authority
 	var authority sdk.AccAddress = address.Module("gov")
@@ -53,45 +79,74 @@ func SimulateMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) 
 	//params.UnbondingTime = time.Duration(simtypes.RandTimestamp(r).UnixNano())
 	//params.MinCommissionRate = simtypes.RandomDecAmount(r, sdk.NewDec(1))
 
-	return &types.MsgUpdateParams{
-		Authority: authority.String(),
-		Params:    params,
-	}
+	_ = authority
+	_ = params
+	return nil
+
 }
 
-// WeightedOperations returns all the operations from the module with their respective weights
+func SimulateCreateStreamProposal(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+	m := types.CreateStreamProposal{}
+	return m
+}
+
+func SimulateTerminateStreamProposal(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+
+}
+
+func SimulateReplaceStreamDistributionProposal(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+
+}
+
+func SimulateUpdateStreamDistributionProposal(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+
+}
+
 func WeightedOperations(
 	appParams simtypes.AppParams,
 	cdc codec.JSONCodec,
-	ak dymsimtypes.AccountKeeper,
-	bk dymsimtypes.BankKeeper,
-	ik dymsimtypes.IncentivesKeeper,
-	k Keeper,
+	k keeper.Keeper,
 ) simulation.WeightedOperations {
 	var (
-		weightSubmitProposal int
-		weightVoteProposal   int
+		weightMsgCreateStream          int
+		weightMsgCreateSponsoredStream int
+		weightMsgUpdateStream          int
+		weightMsgTerminateStream       int
 	)
 
-	appParams.GetOrGenerate(cdc, OpWeightSubmitProposal, &weightSubmitProposal, nil,
-		func(*rand.Rand) { weightSubmitProposal = DefaultWeightSubmitProposal })
-	appParams.GetOrGenerate(cdc, OpWeightVoteProposal, &weightVoteProposal, nil,
-		func(*rand.Rand) { weightVoteProposal = DefaultWeightVoteProposal })
+	appParams.GetOrGenerate(cdc, OpWeightMsgCreateStream, &weightMsgCreateStream, nil,
+		func(*rand.Rand) { weightMsgCreateStream = DefaultWeightMsgCreateStream })
+	appParams.GetOrGenerate(cdc, OpWeightMsgCreateSponsoredStream, &weightMsgCreateSponsoredStream, nil,
+		func(*rand.Rand) { weightMsgCreateSponsoredStream = DefaultWeightMsgCreateSponsoredStream })
+	appParams.GetOrGenerate(cdc, OpWeightMsgUpdateStream, &weightMsgUpdateStream, nil,
+		func(*rand.Rand) { weightMsgUpdateStream = DefaultWeightMsgUpdateStream })
+	appParams.GetOrGenerate(cdc, OpWeightMsgTerminateStream, &weightMsgTerminateStream, nil,
+		func(*rand.Rand) { weightMsgTerminateStream = DefaultWeightMsgTerminateStream })
+
+	protoCdc := codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
 
 	return simulation.WeightedOperations{
 		simulation.NewWeightedOperation(
-			weightSubmitProposal,
-			SimulateMsgSubmitProposal(protoCdc, ak, bk, ik, k),
+			weightMsgCreateStream,
+			SimulateMsgCreateStream(protoCdc, ak, bk, ik, k),
 		),
 		simulation.NewWeightedOperation(
-			weightVoteProposal,
-			SimulateMsgVoteProposal(protoCdc, ak, bk, ik, k),
+			weightMsgCreateSponsoredStream,
+			SimulateMsgCreateSponsoredStream(protoCdc, ak, bk, ik, k),
+		),
+		simulation.NewWeightedOperation(
+			weightMsgUpdateStream,
+			SimulateMsgUpdateStream(protoCdc, ak, bk, ik, k),
+		),
+		simulation.NewWeightedOperation(
+			weightMsgTerminateStream,
+			SimulateMsgTerminateStream(protoCdc, ak, bk, ik, k),
 		),
 	}
 }
 
-// SimulateMsgSubmitProposal generates random governance proposal content
-func SimulateMsgSubmitProposal(
+// SimulateMsgCreateStream generates a random stream creation
+func SimulateMsgCreateStream(
 	cdc *codec.ProtoCodec,
 	ak dymsimtypes.AccountKeeper,
 	bk dymsimtypes.BankKeeper,
@@ -103,34 +158,58 @@ func SimulateMsgSubmitProposal(
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
 		simAccount := accs[r.Intn(len(accs))]
 
-		// Randomly choose proposal type
-		proposalTypes := []string{"create", "terminate", "replace", "update"}
-		proposalType := proposalTypes[r.Intn(len(proposalTypes))]
-
-		var content govtypes.Content
-		var err error
-
-		switch proposalType {
-		case "create":
-			content, err = generateCreateStreamProposal(r, ctx, ik)
-		case "terminate":
-			content, err = generateTerminateStreamProposal(r, ctx, k)
-		case "replace":
-			content, err = generateReplaceStreamProposal(r, ctx, k, ik)
-		case "update":
-			content, err = generateUpdateStreamProposal(r, ctx, k, ik)
+		// Get random gauges
+		gauges := dymsimtypes.RandomGaugeSubset(ctx, r, ik)
+		if len(gauges) == 0 {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateStream, "no gauges"), nil, nil
 		}
 
+		// Generate random distribution records
+		records := make([]types.DistrRecord, len(gauges))
+		totalWeight := math.ZeroInt()
+		for i, gauge := range gauges {
+			// Generate random weight between 1 and MaxInt64/len(gauges) to avoid overflow
+			weight, err := dymsimtypes.RandIntBetween(r, math.OneInt(), math.NewIntFromUint64(^uint64(0)/uint64(len(gauges))))
+			if err != nil {
+				return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateStream, "failed to generate weight"), nil, nil
+			}
+			records[i] = types.DistrRecord{
+				GaugeId: gauge.Id,
+				Weight:  weight,
+			}
+			totalWeight = totalWeight.Add(weight)
+		}
+
+		// Generate random coins
+		spendable := bk.SpendableCoins(ctx, simAccount.Address)
+		if spendable.IsZero() {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateStream, "no spendable coins"), nil, nil
+		}
+
+		// Pick a random coin and amount
+		coin := spendable[r.Intn(len(spendable))]
+		amt, err := dymsimtypes.RandIntBetween(r, math.OneInt(), coin.Amount)
 		if err != nil {
-			return simtypes.NoOpMsg(types.ModuleName, "submit_proposal", err.Error()), nil, nil
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateStream, "failed to generate amount"), nil, nil
 		}
+		coins := sdk.NewCoins(sdk.NewCoin(coin.Denom, amt))
 
-		msg, err := govtypes.NewMsgSubmitProposal(content, sdk.NewCoins(), simAccount.Address)
+		// Random start time between now and 1 week in the future
+		startTime := ctx.BlockTime().Add(time.Duration(r.Int63n(7*24*60*60)) * time.Second)
+
+		// Random number of epochs between 1 and 100
+		numEpochs := uint64(r.Int63n(100) + 1)
+
+		// Random epoch identifier
+		epochIdentifiers := []string{"day", "week", "month"}
+		epochIdentifier := epochIdentifiers[r.Intn(len(epochIdentifiers))]
+
+		streamID, err := k.CreateStream(ctx, coins, records, startTime, epochIdentifier, numEpochs, false)
 		if err != nil {
-			return simtypes.NoOpMsg(types.ModuleName, "submit_proposal", err.Error()), nil, nil
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgCreateStream, err.Error()), nil, nil
 		}
 
-		return simtypes.NewOperationMsg(msg, true, ""), nil, nil
+		return simtypes.NewOperationMsg(&types.MsgCreateStream{}, true, ""), nil, nil
 	}
 }
 
@@ -272,156 +351,5 @@ func SimulateMsgTerminateStream(
 		}
 
 		return simtypes.NewOperationMsg(&types.MsgTerminateStream{}, true, ""), nil, nil
-	}
-}
-
-// Helper functions for generating proposal content
-func generateCreateStreamProposal(r *rand.Rand, ctx sdk.Context, ik dymsimtypes.IncentivesKeeper) (*types.CreateStreamProposal, error) {
-	gauges := dymsimtypes.RandomGaugeSubset(ctx, r, ik)
-	if len(gauges) == 0 {
-		return nil, fmt.Errorf("no gauges available")
-	}
-
-	records := make([]types.DistrRecord, len(gauges))
-	for i, gauge := range gauges {
-		weight, err := dymsimtypes.RandIntBetween(r, math.OneInt(), math.NewIntFromUint64(^uint64(0)/uint64(len(gauges))))
-		if err != nil {
-			return nil, err
-		}
-		records[i] = types.DistrRecord{
-			GaugeId: gauge.Id,
-			Weight:  weight,
-		}
-	}
-
-	coins := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(int64(simulation.RandIntBetween(r, 100, 10000)))))
-	startTime := ctx.BlockTime().Add(time.Duration(r.Int63n(7*24*60*60)) * time.Second)
-	epochIdentifiers := []string{"day", "week", "month"}
-
-	return &types.CreateStreamProposal{
-		Title:                simtypes.RandStringOfLength(r, 10),
-		Description:          simtypes.RandStringOfLength(r, 100),
-		DistributeToRecords:  records,
-		Coins:                coins,
-		StartTime:            startTime,
-		DistrEpochIdentifier: epochIdentifiers[r.Intn(len(epochIdentifiers))],
-		NumEpochsPaidOver:    uint64(r.Int63n(100) + 1),
-		Sponsored:            r.Int()%2 == 0,
-	}, nil
-}
-
-func generateTerminateStreamProposal(r *rand.Rand, ctx sdk.Context, k Keeper) (*types.TerminateStreamProposal, error) {
-	streams := k.GetStreams(ctx)
-	if len(streams) == 0 {
-		return nil, fmt.Errorf("no streams available")
-	}
-
-	stream := streams[r.Intn(len(streams))]
-	return &types.TerminateStreamProposal{
-		Title:       simtypes.RandStringOfLength(r, 10),
-		Description: simtypes.RandStringOfLength(r, 100),
-		StreamId:    stream.Id,
-	}, nil
-}
-
-func generateReplaceStreamProposal(r *rand.Rand, ctx sdk.Context, k Keeper, ik dymsimtypes.IncentivesKeeper) (*types.ReplaceStreamDistributionProposal, error) {
-	streams := k.GetStreams(ctx)
-	if len(streams) == 0 {
-		return nil, fmt.Errorf("no streams available")
-	}
-
-	gauges := dymsimtypes.RandomGaugeSubset(ctx, r, ik)
-	if len(gauges) == 0 {
-		return nil, fmt.Errorf("no gauges available")
-	}
-
-	records := make([]types.DistrRecord, len(gauges))
-	for i, gauge := range gauges {
-		weight, err := dymsimtypes.RandIntBetween(r, math.OneInt(), math.NewIntFromUint64(^uint64(0)/uint64(len(gauges))))
-		if err != nil {
-			return nil, err
-		}
-		records[i] = types.DistrRecord{
-			GaugeId: gauge.Id,
-			Weight:  weight,
-		}
-	}
-
-	stream := streams[r.Intn(len(streams))]
-	return &types.ReplaceStreamDistributionProposal{
-		Title:       simtypes.RandStringOfLength(r, 10),
-		Description: simtypes.RandStringOfLength(r, 100),
-		StreamId:    stream.Id,
-		Records:     records,
-	}, nil
-}
-
-func generateUpdateStreamProposal(r *rand.Rand, ctx sdk.Context, k Keeper, ik dymsimtypes.IncentivesKeeper) (*types.UpdateStreamDistributionProposal, error) {
-	streams := k.GetStreams(ctx)
-	if len(streams) == 0 {
-		return nil, fmt.Errorf("no streams available")
-	}
-
-	gauges := dymsimtypes.RandomGaugeSubset(ctx, r, ik)
-	if len(gauges) == 0 {
-		return nil, fmt.Errorf("no gauges available")
-	}
-
-	records := make([]types.DistrRecord, len(gauges))
-	for i, gauge := range gauges {
-		weight, err := dymsimtypes.RandIntBetween(r, math.OneInt(), math.NewIntFromUint64(^uint64(0)/uint64(len(gauges))))
-		if err != nil {
-			return nil, err
-		}
-		records[i] = types.DistrRecord{
-			GaugeId: gauge.Id,
-			Weight:  weight,
-		}
-	}
-
-	stream := streams[r.Intn(len(streams))]
-	return &types.UpdateStreamDistributionProposal{
-		Title:       simtypes.RandStringOfLength(r, 10),
-		Description: simtypes.RandStringOfLength(r, 100),
-		StreamId:    stream.Id,
-		Records:     records,
-	}, nil
-}
-
-// SimulateMsgVoteProposal simulates a vote on an active proposal
-func SimulateMsgVoteProposal(
-	cdc *codec.ProtoCodec,
-	ak dymsimtypes.AccountKeeper,
-	bk dymsimtypes.BankKeeper,
-	ik dymsimtypes.IncentivesKeeper,
-	k Keeper,
-) simtypes.Operation {
-	return func(
-		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
-	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
-		// Get random account
-		simAccount := accs[r.Intn(len(accs))]
-
-		// Get active proposals
-		activeProposals := k.GetActiveProposals(ctx)
-		if len(activeProposals) == 0 {
-			return simtypes.NoOpMsg(types.ModuleName, "vote_proposal", "no active proposals"), nil, nil
-		}
-
-		// Pick random proposal
-		proposal := activeProposals[r.Intn(len(activeProposals))]
-
-		// Random vote option
-		voteOptions := []govtypes.VoteOption{
-			govtypes.OptionYes,
-			govtypes.OptionNo,
-			govtypes.OptionNoWithVeto,
-			govtypes.OptionAbstain,
-		}
-		vote := voteOptions[r.Intn(len(voteOptions))]
-
-		msg := govtypes.NewMsgVote(simAccount.Address, proposal.ProposalId, vote)
-
-		return simtypes.NewOperationMsg(msg, true, ""), nil, nil
 	}
 }
