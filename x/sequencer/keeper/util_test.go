00799a0aeaaf3bf70552e03d1cf1ed2a06c8eb2f
Daniel T <30197399+danwt@users.noreply.github.com>
2024-12-07 18:42:08 +0000

    fix(sequencer, lightclient): do not prevent unbond when canonical client not found (#1630)

--- x/sequencer/keeper/util_test.go
+++ x/sequencer/keeper/util_test.go
@@ -100,7 +100,11 @@ func (s *SequencerTestSuite) SetupTest() {
 
 	// Overwrite to exclude any unblockers set by default in apptesting, to ensure
 	// we are only testing our logic.
-	s.k().SetUnbondBlockers()
+	s.k().SetUnbondBlockers(
+		s.App.LightClientKeeper,
+		s.App.RollappKeeper,
+	)
+	s.App.RollappKeeper.SetHooks(rollapptypes.NewMultiRollappHooks(s.k().RollappHooks()))
 }
 
 func (s *SequencerTestSuite) seq(pk cryptotypes.PubKey) types.Sequencer {
