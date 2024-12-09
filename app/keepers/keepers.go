781de50787d0858af3b0b92da77cc93772bac87a
danwt <30197399+danwt@users.noreply.github.com>
2024-12-09 18:18:57 +0000

    fmt

--- app/keepers/keepers.go
+++ app/keepers/keepers.go
@@ -583,7 +583,7 @@ func (a *AppKeepers) SetupHooks() {
 	// register the staking hooks
 	a.LockupKeeper.SetHooks(
 		lockuptypes.NewMultiLockupHooks(
-			// insert lockup hooks receivers here
+		// insert lockup hooks receivers here
 		),
 	)
 
@@ -603,7 +603,7 @@ func (a *AppKeepers) SetupHooks() {
 
 	a.IncentivesKeeper.SetHooks(
 		incentivestypes.NewMultiIncentiveHooks(
-			// insert incentive hooks receivers here
+		// insert incentive hooks receivers here
 		),
 	)
 
