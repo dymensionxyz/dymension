f0a6c38ce7fb8b59434d654ab1e82d97a75a76c2
danwt <30197399+danwt@users.noreply.github.com>
2024-12-12 10:32:25 +0000

    add upgrade 7

--- app/app.go
+++ app/app.go
@@ -24,6 +24,7 @@ import (
 	v4 "github.com/dymensionxyz/dymension/v3/app/upgrades/v4"
 	v5 "github.com/dymensionxyz/dymension/v3/app/upgrades/v5"
 	v6 "github.com/dymensionxyz/dymension/v3/app/upgrades/v6"
+	v7 "github.com/dymensionxyz/dymension/v3/app/upgrades/v7"
 
 	dbm "github.com/cometbft/cometbft-db"
 	abci "github.com/cometbft/cometbft/abci/types"
@@ -88,7 +89,7 @@ var (
 	DefaultNodeHome string
 
 	// Upgrades contains the upgrade handlers for the application
-	Upgrades = []upgrades.Upgrade{v4.Upgrade, v5.Upgrade, v6.Upgrade}
+	Upgrades = []upgrades.Upgrade{v4.Upgrade, v5.Upgrade, v6.Upgrade, v7.Upgrade}
 )
 
 func init() {
