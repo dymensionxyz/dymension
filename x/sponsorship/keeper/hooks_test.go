c4af89474037a5e916cebd9b122614a54b1ddd33
Michael Tsitrin <michael@dymension.xyz>
2025-02-13 16:41:08 +0200

    fixed sponserhip ut

--- x/sponsorship/keeper/hooks_test.go
+++ x/sponsorship/keeper/hooks_test.go
@@ -21,11 +21,14 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val := s.CreateValidator()
-				del := s.CreateDelegator(val.GetOperator(), initial)
+				valAddr, err := sdk.ValAddressFromBech32(val.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(valAddr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -33,11 +36,11 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
-				s.Delegate(del.GetDelegatorAddr(), val.GetOperator(), update)
+				s.Delegate(delAddr, valAddr, update)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val.GetOperator(), initial.Add(update).Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, valAddr, initial.Add(update).Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
@@ -53,15 +56,20 @@ func (s *KeeperTestSuite) TestHooks() {
 			name: "User with 2 validators",
 			prepare: func() {
 				val1 := s.CreateValidator()
+				val1addr, err := sdk.ValAddressFromBech32(val1.GetOperator())
+				s.Require().NoError(err)
 				val2 := s.CreateValidator()
+				val2addr, err := sdk.ValAddressFromBech32(val2.GetOperator())
+				s.Require().NoError(err)
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
-				del := s.CreateDelegator(val1.GetOperator(), initial)           // delegator 1 -> validator 1
-				s.Delegate(del.GetDelegatorAddr(), val2.GetOperator(), initial) // delegator 1 -> validator 2
+				del := s.CreateDelegator(val1addr, initial) // delegator 1 -> validator 1
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
+				s.Delegate(delAddr, val2addr, initial) // delegator 1 -> validator 2
 
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -69,9 +77,9 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val1.GetOperator(), initial.Amount)
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val2.GetOperator(), initial.Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, val1addr, initial.Amount)
+				s.AssertDelegatorValidator(delAddr, val2addr, initial.Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
@@ -87,15 +95,20 @@ func (s *KeeperTestSuite) TestHooks() {
 			name: "User with 2 validators, partial unbonding from one of them",
 			prepare: func() {
 				val1 := s.CreateValidator()
+				val1addr, err := sdk.ValAddressFromBech32(val1.GetOperator())
+				s.Require().NoError(err)
 				val2 := s.CreateValidator()
+				val2addr, err := sdk.ValAddressFromBech32(val2.GetOperator())
+				s.Require().NoError(err)
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
-				del := s.CreateDelegator(val1.GetOperator(), initial)           // delegator 1 -> validator 1
-				s.Delegate(del.GetDelegatorAddr(), val2.GetOperator(), initial) // delegator 1 -> validator 2
+				del := s.CreateDelegator(val1addr, initial) // delegator 1 -> validator 1
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
+				s.Delegate(delAddr, val2addr, initial) // delegator 1 -> validator 2
 
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -103,12 +116,12 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(800_000))
-				s.Undelegate(del.GetDelegatorAddr(), val1.GetOperator(), update)
+				s.Undelegate(delAddr, val1addr, update)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val1.GetOperator(), initial.Sub(update).Amount)
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val2.GetOperator(), initial.Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, val1addr, initial.Sub(update).Amount)
+				s.AssertDelegatorValidator(delAddr, val2addr, initial.Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
@@ -124,15 +137,20 @@ func (s *KeeperTestSuite) TestHooks() {
 			name: "User with 2 validators, complete unbonding from one of them",
 			prepare: func() {
 				val1 := s.CreateValidator()
+				val1addr, err := sdk.ValAddressFromBech32(val1.GetOperator())
+				s.Require().NoError(err)
 				val2 := s.CreateValidator()
+				val2addr, err := sdk.ValAddressFromBech32(val2.GetOperator())
+				s.Require().NoError(err)
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
-				del := s.CreateDelegator(val1.GetOperator(), initial)           // delegator 1 -> validator 1
-				s.Delegate(del.GetDelegatorAddr(), val2.GetOperator(), initial) // delegator 1 -> validator 2
+				del := s.CreateDelegator(val1addr, initial) // delegator 1 -> validator 1
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
+				s.Delegate(delAddr, val2addr, initial) // delegator 1 -> validator 2
 
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -140,12 +158,12 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, initial.Amount)
-				s.Undelegate(del.GetDelegatorAddr(), val1.GetOperator(), update)
+				s.Undelegate(delAddr, val1addr, update)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertNotHaveDelegatorValidator(del.GetDelegatorAddr(), val1.GetOperator())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val2.GetOperator(), initial.Amount)
+				s.AssertVoted(delAddr)
+				s.AssertNotHaveDelegatorValidator(delAddr, val1addr)
+				s.AssertDelegatorValidator(delAddr, val2addr, initial.Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
@@ -162,11 +180,14 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val := s.CreateValidator()
-				del := s.CreateDelegator(val.GetOperator(), initial)
+				valAddr, err := sdk.ValAddressFromBech32(val.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(valAddr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -174,11 +195,11 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(800_000))
-				s.Undelegate(del.GetDelegatorAddr(), val.GetOperator(), update)
+				s.Undelegate(delAddr, valAddr, update)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val.GetOperator(), initial.Sub(update).Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, valAddr, initial.Sub(update).Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
@@ -195,11 +216,14 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val := s.CreateValidator()
-				del := s.CreateDelegator(val.GetOperator(), initial)
+				valAddr, err := sdk.ValAddressFromBech32(val.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(valAddr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -208,12 +232,12 @@ func (s *KeeperTestSuite) TestHooks() {
 
 				// Completely undelegate
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
-				finalDel := s.Undelegate(del.GetDelegatorAddr(), val.GetOperator(), update)
+				finalDel := s.Undelegate(delAddr, valAddr, update)
 				s.Require().Nil(finalDel)
 
 				// Check the state
-				s.AssertNotVoted(del.GetDelegatorAddr())
-				s.AssertNotHaveDelegatorValidator(del.GetDelegatorAddr(), val.GetOperator())
+				s.AssertNotVoted(delAddr)
+				s.AssertNotHaveDelegatorValidator(delAddr, valAddr)
 			},
 			initialDistr:         types.NewDistribution(),
 			expectedDistr:        types.NewDistribution(), // empty distribution
@@ -224,12 +248,17 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val1 := s.CreateValidator()
+				val1addr, err := sdk.ValAddressFromBech32(val1.GetOperator())
+				s.Require().NoError(err)
 				val2 := s.CreateValidator()
-				del := s.CreateDelegator(val1.GetOperator(), initial)
+				val2addr, err := sdk.ValAddressFromBech32(val2.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(val1addr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -237,12 +266,12 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(300_000))
-				s.BeginRedelegate(del.GetDelegatorAddr(), val1.GetOperator(), val2.GetOperator(), update)
+				s.BeginRedelegate(delAddr, val1addr, val2addr, update)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val1.GetOperator(), initial.Sub(update).Amount)
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val2.GetOperator(), update.Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, val1addr, initial.Sub(update).Amount)
+				s.AssertDelegatorValidator(delAddr, val2addr, update.Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			// final distribution is the same, but validator indexes are updated in the state
@@ -260,12 +289,17 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val1 := s.CreateValidator()
+				val1addr, err := sdk.ValAddressFromBech32(val1.GetOperator())
+				s.Require().NoError(err)
 				val2 := s.CreateValidator()
-				del := s.CreateDelegator(val1.GetOperator(), initial)
+				val2addr, err := sdk.ValAddressFromBech32(val2.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(val1addr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -273,12 +307,12 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
-				s.BeginRedelegate(del.GetDelegatorAddr(), val1.GetOperator(), val2.GetOperator(), update)
+				s.BeginRedelegate(delAddr, val1addr, val2addr, update)
 
 				// Check the state
-				s.AssertNotVoted(del.GetDelegatorAddr())
-				s.AssertNotHaveDelegatorValidator(del.GetDelegatorAddr(), val1.GetOperator())
-				s.AssertNotHaveDelegatorValidator(del.GetDelegatorAddr(), val2.GetOperator())
+				s.AssertNotVoted(delAddr)
+				s.AssertNotHaveDelegatorValidator(delAddr, val1addr)
+				s.AssertNotHaveDelegatorValidator(delAddr, val2addr)
 			},
 			initialDistr:         types.NewDistribution(),
 			expectedDistr:        types.NewDistribution(),
@@ -289,11 +323,14 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val := s.CreateValidator()
-				del := s.CreateDelegator(val.GetOperator(), initial)
+				valAddr, err := sdk.ValAddressFromBech32(val.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(valAddr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -301,12 +338,12 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(800_000))
-				s.Undelegate(del.GetDelegatorAddr(), val.GetOperator(), update)
-				s.CancelUnbondingDelegation(del.GetDelegatorAddr(), val.GetOperator(), s.Ctx.BlockHeight(), update)
+				s.Undelegate(delAddr, valAddr, update)
+				s.CancelUnbondingDelegation(delAddr, valAddr, s.Ctx.BlockHeight(), update)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val.GetOperator(), initial.Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, valAddr, initial.Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
@@ -323,11 +360,14 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val := s.CreateValidator()
-				del := s.CreateDelegator(val.GetOperator(), initial)
+				valAddr, err := sdk.ValAddressFromBech32(val.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(valAddr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -335,14 +375,14 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(800_000))
-				s.Undelegate(del.GetDelegatorAddr(), val.GetOperator(), update)
+				s.Undelegate(delAddr, valAddr, update)
 
 				partiallyCancel := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(400_000))
-				s.CancelUnbondingDelegation(del.GetDelegatorAddr(), val.GetOperator(), s.Ctx.BlockHeight(), partiallyCancel)
+				s.CancelUnbondingDelegation(delAddr, valAddr, s.Ctx.BlockHeight(), partiallyCancel)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val.GetOperator(), initial.Sub(update).Add(partiallyCancel).Amount)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, valAddr, initial.Sub(update).Add(partiallyCancel).Amount)
 			},
 			initialDistr: types.NewDistribution(),
 			// 600_000 = 1_000_000 - 800_000 + 400_000
@@ -360,11 +400,14 @@ func (s *KeeperTestSuite) TestHooks() {
 			prepare: func() {
 				initial := sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000))
 				val1 := s.CreateValidator()
-				del := s.CreateDelegator(val1.GetOperator(), initial)
+				val1addr, err := sdk.ValAddressFromBech32(val1.GetOperator())
+				s.Require().NoError(err)
+				del := s.CreateDelegator(val1addr, initial)
+				delAddr := sdk.MustAccAddressFromBech32(del.GetDelegatorAddr())
 				s.CreateGauges(2)
 
 				s.Vote(types.MsgVote{
-					Voter: del.GetDelegatorAddr().String(),
+					Voter: del.GetDelegatorAddr(),
 					Weights: []types.GaugeWeight{
 						{GaugeId: 1, Weight: types.DYM.MulRaw(20)},
 						{GaugeId: 2, Weight: types.DYM.MulRaw(50)},
@@ -372,12 +415,14 @@ func (s *KeeperTestSuite) TestHooks() {
 				})
 
 				update := math.NewInt(1_000_000)
-				val2 := s.CreateValidatorWithAddress(del.GetDelegatorAddr(), update)
+				val2 := s.CreateValidatorWithAddress(delAddr, update)
+				val2addr, err := sdk.ValAddressFromBech32(val2.GetOperator())
+				s.Require().NoError(err)
 
 				// Check the state
-				s.AssertVoted(del.GetDelegatorAddr())
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val1.GetOperator(), initial.Amount)
-				s.AssertDelegatorValidator(del.GetDelegatorAddr(), val2.GetOperator(), update)
+				s.AssertVoted(delAddr)
+				s.AssertDelegatorValidator(delAddr, val1addr, initial.Amount)
+				s.AssertDelegatorValidator(delAddr, val2addr, update)
 			},
 			initialDistr: types.NewDistribution(),
 			expectedDistr: types.Distribution{
