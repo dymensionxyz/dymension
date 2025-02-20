15e2e0feb60abc314364f20957ebe2d2b90114cf
danwt <30197399+danwt@users.noreply.github.com>
2025-02-20 12:55:25 +0000

    test fail, gonna delete comments

--- x/eibc/keeper/lp_test.go
+++ x/eibc/keeper/lp_test.go
@@ -12,7 +12,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	ctx := suite.Ctx
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "1", // wrong rollup
-		Denom:      "1",
+		Denom:      "aaa",
 		MaxPrice:   math.NewInt(1),
 		MinFee:     math.NewInt(1),
 		SpendLimit: math.NewInt(100),
@@ -20,7 +20,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "1", // wrong rollup
-		Denom:      "2",
+		Denom:      "bbb",
 		MaxPrice:   math.NewInt(1),
 		MinFee:     math.NewInt(1),
 		SpendLimit: math.NewInt(100),
@@ -28,7 +28,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "2",
-		Denom:      "1", // wrong denom
+		Denom:      "aaa", // wrong denom
 		MaxPrice:   math.NewInt(1),
 		MinFee:     math.NewInt(1),
 		SpendLimit: math.NewInt(100),
@@ -36,7 +36,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "2",
-		Denom:      "2",
+		Denom:      "bbb",
 		MaxPrice:   math.NewInt(1), // max price too low
 		MinFee:     math.NewInt(1),
 		SpendLimit: math.NewInt(100),
@@ -44,7 +44,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "2",
-		Denom:      "2",
+		Denom:      "bbb",
 		MaxPrice:   math.NewInt(5),
 		MinFee:     math.NewInt(8), // min fee too high
 		SpendLimit: math.NewInt(100),
@@ -52,7 +52,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	expect, err := k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "2",
-		Denom:      "2",
+		Denom:      "bbb",
 		MaxPrice:   math.NewInt(5), // valid
 		MinFee:     math.NewInt(7), // valid
 		SpendLimit: math.NewInt(100),
@@ -60,7 +60,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "2",
-		Denom:      "2",
+		Denom:      "bbb",
 		MaxPrice:   math.NewInt(6), // also valid, but not first
 		MinFee:     math.NewInt(5), // also valid, but not first
 		SpendLimit: math.NewInt(100),
@@ -68,7 +68,7 @@ func (suite *KeeperTestSuite) TestLPs() {
 	suite.Require().NoError(err)
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "3", // wrong rollup
-		Denom:      "1",
+		Denom:      "aaa",
 		MaxPrice:   math.NewInt(1),
 		MinFee:     math.NewInt(1),
 		SpendLimit: math.NewInt(100),
@@ -77,15 +77,15 @@ func (suite *KeeperTestSuite) TestLPs() {
 
 	_, err = k.LPs.UpsertLP(ctx, &types.OnDemandLiquidity{
 		Rollapp:    "3", // wrong rollup
-		Denom:      "2",
+		Denom:      "bbb",
 		MaxPrice:   math.NewInt(1),
 		MinFee:     math.NewInt(1),
 		SpendLimit: math.NewInt(100),
 	})
 	suite.Require().NoError(err)
 	o := types.DemandOrder{
-		Price:     sdk.NewCoins(sdk.NewCoin("2", sdk.NewInt(5))),
-		Fee:       sdk.NewCoins(sdk.NewCoin("2", sdk.NewInt(7))),
+		Price:     sdk.NewCoins(sdk.NewCoin("foo", sdk.NewInt(5))),
+		Fee:       sdk.NewCoins(sdk.NewCoin("foo", sdk.NewInt(7))),
 		RollappId: "2",
 	}
 	lp, err := k.LPs.FindLP(ctx, k, &o)
--- x/eibc/types/lp.go
+++ x/eibc/types/lp.go
@@ -2,15 +2,13 @@ package types
 
 import (
 	"cosmossdk.io/math"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	eibctypes "github.com/dymensionxyz/dymension/v3/x/eibc/types"
 )
 
 func (lp OnDemandLiquidity) MaxSpend() math.Int {
 	return math.MinInt(lp.MaxPrice, lp.SpendLimit.Sub(lp.Spent))
 }
 
-func (lp OnDemandLiquidity) Accepts(o *eibctypes.DemandOrder) bool {
+func (lp OnDemandLiquidity) Accepts(o *DemandOrder) bool {
 	priceOK := o.PriceAmount().LTE(lp.MaxSpend())
 	feeOK := lp.MinFee.LTE(o.GetFeeAmount())
 	return priceOK && feeOK
