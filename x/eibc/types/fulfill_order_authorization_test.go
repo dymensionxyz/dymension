893282a5d07ad16d37061c874fbe5829af3c530e
zale144 <aleksandar.sukovic@gmail.com>
2025-03-04 14:17:42 +0100

    fix(lockup): get lock creation fee from params (#1762)
    get lock creation fee from params

--- x/eibc/types/fulfill_order_authorization_test.go
+++ x/eibc/types/fulfill_order_authorization_test.go
@@ -461,6 +461,19 @@ func TestFulfillOrderAuthorization_ValidateBasic(t *testing.T) {
 			expectedError: "spend_limit is invalid",
 		},
 		{
+			name: "No SpendLimit Provided",
+			authorization: FulfillOrderAuthorization{
+				Rollapps: []*RollappCriteria{
+					{
+						RollappId:        validaRollappID,
+						MinFeePercentage: sdk.MustNewDecFromStr("0.01"),
+						OperatorFeeShare: sdk.MustNewDecFromStr("0.1"),
+					},
+				},
+			},
+			expectedError: "",
+		},
+		{
 			name: "Duplicate Rollapp IDs",
 			authorization: FulfillOrderAuthorization{
 				Rollapps: []*RollappCriteria{
