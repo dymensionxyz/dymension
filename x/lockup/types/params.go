34272ae9a7cc6b0d60876af4bf0656af44d41f4e
keruch <53012408+keruch@users.noreply.github.com>
2025-03-11 09:16:14 +0100

    feat(lockup): added min lockup duration (#1781)
    feat(lockup): added min lockup diration
    
    Co-authored-by: Michael Tsitrin <michael@dymension.xyz>

--- x/lockup/types/params.go
+++ x/lockup/types/params.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"fmt"
+	"time"
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,6 +13,7 @@ import (
 var (
 	KeyForceUnlockAllowedAddresses = []byte("ForceUnlockAllowedAddresses")
 	KeyLockCreationFee             = []byte("LockCreationFee")
+	KeyMinLockDuration             = []byte("MinLockDuration")
 
 	_ paramtypes.ParamSet = &Params{}
 )
@@ -21,10 +23,11 @@ func ParamKeyTable() paramtypes.KeyTable {
 	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
 }
 
-func NewParams(forceUnlockAllowedAddresses []string, lockCreationFee math.Int) Params {
+func NewParams(forceUnlockAllowedAddresses []string, lockCreationFee math.Int, minLockDuration time.Duration) Params {
 	return Params{
 		ForceUnlockAllowedAddresses: forceUnlockAllowedAddresses,
 		LockCreationFee:             lockCreationFee,
+		MinLockDuration:             minLockDuration,
 	}
 }
 
@@ -33,6 +36,9 @@ func DefaultParams() Params {
 	return Params{
 		ForceUnlockAllowedAddresses: []string{},
 		LockCreationFee:             DefaultLockFee,
+		// TODO: Migrations! Set MinLockDuration to 1 day.
+		//  https://github.com/dymensionxyz/dymension/issues/1723
+		MinLockDuration: 0,
 	}
 }
 
@@ -52,6 +58,7 @@ func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 	return paramtypes.ParamSetPairs{
 		paramtypes.NewParamSetPair(KeyForceUnlockAllowedAddresses, &p.ForceUnlockAllowedAddresses, validateAddresses),
 		paramtypes.NewParamSetPair(KeyLockCreationFee, &p.LockCreationFee, validateLockCreationFee),
+		paramtypes.NewParamSetPair(KeyMinLockDuration, &p.MinLockDuration, validateMinLockDuration),
 	}
 }
 
@@ -78,6 +85,18 @@ func validateLockCreationFee(i interface{}) error {
 
 	if !fee.IsNil() && fee.IsNegative() {
 		return fmt.Errorf("lock creation fee must be non-negative: %d", fee.Int64())
+	}
+
+	return nil
+}
+
+func validateMinLockDuration(i interface{}) error {
+	duration, ok := i.(time.Duration)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	if duration < 0 {
+		return fmt.Errorf("duration should be non-negative: %d", duration)
 	}
 
 	return nil
