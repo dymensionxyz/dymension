1eb4c26e6a85662f9e034fac36c6498b54ba4267
danwt <30197399+danwt@users.noreply.github.com>
2025-03-21 17:27:00 +0000

    add my todo

--- x/forward/keeper/do.go
+++ x/forward/keeper/do.go
@@ -12,6 +12,12 @@ func validForward(data []byte) error {
 	return nil
 }
 
+/*
+What was I doing?
+I need to make a hook type here that can be passed to the eibc keeper
+The hook type needs to embed the keeper and call whatever methods we need to do here
+*/
+
 func (k Keeper) doForwardHook(ctx sdk.Context, order *types.DemandOrder, fundsSource sdk.AccAddress, data ) error {
 	var d types.ForwardHook
 	err := proto.Unmarshal(order.FulfillHook.HookData, &d)
