8bc9d847b9cb0c6ff0b1ed3bc0d4a06581cbda5c
danwt <30197399+danwt@users.noreply.github.com>
2025-03-24 10:54:10 +0000

    note

--- ibctesting/forwarding_test.go
+++ ibctesting/forwarding_test.go
@@ -1,1 +1,14 @@
 package ibctesting
+
+/*
+Friday:
+Got the basic hook structure up:
+A memo can contain a hook name and data
+It will end up in x/forward
+which is connected to the warp route keeper, so it can initiate sends
+
+Next sensible things:
+- Need to ideally have a test that takes a transfer with the memo, and ensures the forwarder gets called
+ (can use a dummy hook)
+-
+*/
