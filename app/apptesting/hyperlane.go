dd3b6671d99997bc383ee8d31ff251ab7bf970bb
danwt <30197399+danwt@users.noreply.github.com>
2025-03-25 12:19:49 +0000

    need to figure out again

--- app/apptesting/hyperlane.go
+++ app/apptesting/hyperlane.go
@@ -11,129 +11,46 @@ import (
 
 /*
 Synthesis
-	Setup
-
-	Send (assume Dym from Hub to Ethereum)
-		WR:
-			Pass token, real recipient, dst, amt
-			Need a router: mapping <token, dst, dst contract (warp route)>
-			Dispatches a message with src mailbox as origin (token.OriginMailbox)
-			Target is the dst contract
-		Dispatch:
-			Need required and default hooks
-			Does post dispatch with the required hook
-
+	Flow
+		Send (assume Dym from Hub to Ethereum)
+			WR:
+				Pass token, real recipient, dst, amt
+				Need an (enrolled) router: mapping <token, dst, dst contract (warp route)>
+				Dispatches a message with src mailbox as origin (token.OriginMailbox)
+				Target is the dst contract
+			Dispatch:
+				Need required and default hooks: the Kyve test used default=Noop and Required=IGP
+					Required fires first THEN the default or override
+				Does post dispatch with the required hook
+				Message origin is src, destination is dst
+		Receive (assume Dym from Hub to Ethereum)
+			Process:
+				Pass mailbox and the message
+				Uses the mailbox default ISM, so long as the warp route does not override it
+				Verifies via ISM, then calls Handle
+			Handle:
+				Need an (enrolled) router: mapping <recipient, src>
+	Setup (from the flow we can derive the setup)
+		Hub:
+			Create noop ISM
+			Create mailbox with noop ISM
+			Create a noop hook
+			On mailbox, set required and default hooks to noop
+			Create a collateral token 'adym' on top of the mailbox (thus, noop ISM will be used)
+		Now do the same on Ethereum
+			...
+		Back to Hub:
+			Enroll a remote router <token, <dst, Ethereum warp route (token) address, Gas=0>
 
 	Questions:
 		Necessity of IGP?
+			...
 		What is remote router gas?
+			It can be used as a default so that transfers don't need to specify it.
+			It acts a bound for the price calculation done in the IGP
+		Is the test setup of default=Noop and required=IGP correct? Is it what Kyve does? It that what is done in practice?
+			...
 
-
-
-*/
-
-/*
-A reminder on how things work
-
-Mailbox has
-	id
-	owner
-	sent and received cnt
-	ref to default ism
-	ref to default hook
-	ref to required hook
-	ref to local domain
-
-Modules
-	A collateral warp route is an example of a module.
-
-Creating a token (warp route)
-	 specify owner
-	 origin mailbox
-	 origin denom
-
-
-Sending a transfer:
-	specify
-		token id
-		sender
-		destination domain
-		recipient
-		amt
-		custom hook id
-		gas limit
-		max fee
-		custom hook metadata(??)
-	it finds a router contract for token id and destination domain (??)
-	it dispatches a message with
-		origin mailbox
-		token id (=sender)(?)
-		max fee
-		router receiver domain
-		router receiver contract
-		warp route recipient and amount gets bundled into opaque payload
-		given gas limit, sender account
-		forward the custom hook metadata
-		uses custom hook id
-	dispatch logic:
-		add a message to origin mailbox
-		calls post dispatch:
-			give actual message <version, nonce, local domain, sender, dst domain, recipient, body>
-			use origin mailbox id
-			use required hook
-			pass custom hook metadata
-		use post dispatch router for first the required hook then the default hook
-			mock:
-				no op
-		 	igp:
-				pay for gas
-			merkle tree:
-				adds message ID to a merkle tree corresponding to the mailbox
-			noop:
-				no op
-
-		IN CASE OF TEST: REQUIRED HOOK IS IGP, DEFAULT HOOK IS NO OP
-
-Receiving a transfer:
-	Relayer does processMessage
-		args are mailbox, raw message and metadata
-		mailbox local domain must be message destination
-		uses approuter to get module and then ISM ID for the recipient contract
-			(this calls into warp route and returns either the default ism of the token origin mailbox, or the token ISM if there is one) (for overriding?)
-		uses ism router to get ism id
-		metadata and message is passed to ISM:
-			noop ISM:
-				says it's valid
-			merkle root multisig ISM:
-				?
-			message id multisig ISM:
-				?
-		after verification, handle it
-		handling is via app router
-
-
-Setting up the router?
-	A warp route keeper has EnrolledRouters
-		(token id, dst domain -> router)
-		used for outbound message
-		they have to be set explicitly for each token
-		it has contract addresses for the receiving contract on the other side
-		it has a gas field which works as default gas limit if user does not specify in their send
-
-	A core keeper has routers:
-		appRouter
-			(recipient 	-> (recipient -> ism ID)
-			used to find ism ID for inbound message
-		ismRouter
-			(id -> verifier)
-			used to get ISM verifier for inbound message
-		postDispatchRouter
-			hook id -> f
-			where f
-			used after commiting a message to a mailbox
-
-
-Setting up the ISM? Hooks?
 */
 
 func (s *KeeperTestHelper) SetupHyperlane() {
