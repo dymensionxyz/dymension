09ee2ab5bd57df72a3e05d61688bb40150425c89
threehonor <pengqi@email.cn>
2025-03-26 18:50:27 +0800

    chore: fix some comments (#1801)
    Signed-off-by: threehonor <pengqi@email.cn>

--- x/streamer/keeper/suite_test.go
+++ x/streamer/keeper/suite_test.go
@@ -192,7 +192,7 @@ func (suite *KeeperTestSuite) CreateValidator() stakingtypes.ValidatorI {
 		valAddr.String(),
 		privEd.PubKey(),
 		sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(1_000_000_000)),
-		stakingtypes.NewDescription("moniker", "indentity", "website", "security_contract", "details"),
+		stakingtypes.NewDescription("moniker", "identity", "website", "security_contract", "details"),
 		stakingtypes.NewCommissionRates(math.LegacyOneDec(), math.LegacyOneDec(), math.LegacyOneDec()),
 		math.OneInt(),
 	)
