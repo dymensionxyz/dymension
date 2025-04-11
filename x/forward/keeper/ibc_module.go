4fab49aa93b666a1c0fcf5f41665a377b335962e
danwt <30197399+danwt@users.noreply.github.com>
2025-04-11 11:29:12 +0100

    add a note

--- x/forward/keeper/ibc_module.go
+++ x/forward/keeper/ibc_module.go
@@ -20,5 +20,24 @@ func NewIBCModule(k Keeper, next porttypes.IBCModule) *IBCModule {
 // Recipient can either be the fulfiller of a hook that already occurred, or the original recipient still, who probably still wants the hook to happen
 // NOTE: there is an asymmetry currently because on fulfill supports multiple hooks, but this finalization onRecv is hardcoded for x/forward atm
 func (m *IBCModule) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, relayer sdk.AccAddress) exported.Acknowledgement {
+
+	/*
+		SPAGHETTI CODE ALERT:
+
+		*In general* we want a way to do something whenever an ibc transfer happens ("Hook"). It can happen
+			1. on EIBC fulfill
+			2. on finalize to the original recipient, for non fulfilled orders
+			3. on finalize to the fulfiller, for fulfilled orders
+
+		Chosen approach is a bit spaghetti:
+
+		1. Do the hook on EIBC fulfillment, using immediate funds
+		2. On finalize, look up the EIBC demand order to check if it's fulfilled or not.
+			a. If it ISN'T, then do the hook AFTER the ibc transfer stack finishes
+			b. If it IS, then do nothing
+
+		We can do (2) by finding the eibc order directly using the packet key, because the status has not yet been update to finalized
+	*/
+
 	return m.IBCModule.OnRecvPacket(ctx, packet, relayer)
 }
