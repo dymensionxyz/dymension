8239c9ba9279b9f4afa065383777b1154ae2d1be
danwt <30197399+danwt@users.noreply.github.com>
2025-05-06 11:56:03 +0100

    remove a couple of unused

--- x/forward/types/expected_keepers.go
+++ x/forward/types/expected_keepers.go
@@ -4,23 +4,11 @@ import (
 	context "context"
 
 	"github.com/bcp-innovations/hyperlane-cosmos/x/warp/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 )
 
 type WarpQuery interface {
 	Token(ctx context.Context, request *types.QueryTokenRequest) (*types.QueryTokenResponse, error)
-}
-
-type BankKeeper interface {
-	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-	SendCoinsFromModuleToModule(ctx context.Context, senderModule, recipientModule string, amt sdk.Coins) error
-	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
-	SendCoins(ctx context.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
-}
-
-type AccountKeeper interface {
-	GetModuleAddress(moduleName string) sdk.AccAddress
 }
 
 type TransferKeeper interface {
