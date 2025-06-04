9bc593b887509b1a86e4652f9c2e618f20d1f879
danwt <30197399+danwt@users.noreply.github.com>
2025-06-04 15:33:55 +0100

    codec

--- x/kas/types/codec.go
+++ x/kas/types/codec.go
@@ -3,19 +3,20 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 var ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	// cdc.RegisterConcrete(&MsgCreateSequencer{}, "sequencer/CreateSequencer", nil)
+	cdc.RegisterConcrete(&MsgIndicateProgress{}, "kas/IndicateProgress", nil)
+	cdc.RegisterConcrete(&MsgBootstrap{}, "kas/Bootstrap", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// registry.RegisterImplementations((*sdk.Msg)(nil),
-	// &MsgCreateSequencer{},
-	// )
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgIndicateProgress{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgBootstrap{})
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
