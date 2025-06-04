8db8a0207d9a5ae1ab2f8489426eb5dd1778d5d3
danwt <30197399+danwt@users.noreply.github.com>
2025-06-04 14:19:34 +0100

    add genesis protos

--- x/kas/types/genesis.pb.go
+++ x/kas/types/genesis.pb.go
@@ -24,6 +24,11 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type GenesisState struct {
+	Bootstrapped         bool                 `protobuf:"varint,1,opt,name=bootstrapped,proto3" json:"bootstrapped,omitempty"`
+	Mailbox              string               `protobuf:"bytes,2,opt,name=mailbox,proto3" json:"mailbox,omitempty"`
+	Ism                  string               `protobuf:"bytes,3,opt,name=ism,proto3" json:"ism,omitempty"`
+	Outpoint             *TransactionOutpoint `protobuf:"bytes,4,opt,name=outpoint,proto3" json:"outpoint,omitempty"`
+	ProcessedWithdrawals []*WithdrawalID      `protobuf:"bytes,5,rep,name=processed_withdrawals,json=processedWithdrawals,proto3" json:"processed_withdrawals,omitempty"`
 }
 
 func (m *GenesisState) Reset()         { *m = GenesisState{} }
@@ -59,6 +64,41 @@ func (m *GenesisState) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_GenesisState proto.InternalMessageInfo
 
+func (m *GenesisState) GetBootstrapped() bool {
+	if m != nil {
+		return m.Bootstrapped
+	}
+	return false
+}
+
+func (m *GenesisState) GetMailbox() string {
+	if m != nil {
+		return m.Mailbox
+	}
+	return ""
+}
+
+func (m *GenesisState) GetIsm() string {
+	if m != nil {
+		return m.Ism
+	}
+	return ""
+}
+
+func (m *GenesisState) GetOutpoint() *TransactionOutpoint {
+	if m != nil {
+		return m.Outpoint
+	}
+	return nil
+}
+
+func (m *GenesisState) GetProcessedWithdrawals() []*WithdrawalID {
+	if m != nil {
+		return m.ProcessedWithdrawals
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*GenesisState)(nil), "dymensionxyz.dymension.kas.GenesisState")
 }
@@ -68,17 +108,27 @@ func init() {
 }
 
 var fileDescriptor_57a8ffb996eb4d27 = []byte{
-	// 154 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x48, 0xa9, 0xcc, 0x4d,
-	0xcd, 0x2b, 0xce, 0xcc, 0xcf, 0xab, 0xa8, 0xac, 0xd2, 0x87, 0x73, 0xf4, 0xb3, 0x13, 0x8b, 0xf5,
-	0xd3, 0x53, 0xf3, 0x52, 0x8b, 0x33, 0x8b, 0xf5, 0x0a, 0x8a, 0xf2, 0x4b, 0xf2, 0x85, 0xa4, 0x90,
-	0x55, 0xea, 0xc1, 0x39, 0x7a, 0xd9, 0x89, 0xc5, 0x52, 0x22, 0xe9, 0xf9, 0xe9, 0xf9, 0x60, 0x65,
-	0xfa, 0x20, 0x16, 0x44, 0x87, 0x12, 0x1f, 0x17, 0x8f, 0x3b, 0xc4, 0x88, 0xe0, 0x92, 0xc4, 0x92,
-	0x54, 0x27, 0xaf, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e, 0x71,
-	0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18, 0x6e, 0x3c, 0x96, 0x63, 0x88, 0x32, 0x48, 0xcf,
-	0x2c, 0xc9, 0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0xc7, 0xe1, 0xa0, 0x32, 0x63, 0xfd, 0x0a,
-	0xb0, 0xab, 0x4a, 0x2a, 0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0x56, 0x18, 0x03, 0x02, 0x00, 0x00,
-	0xff, 0xff, 0x86, 0x94, 0x22, 0xee, 0xc0, 0x00, 0x00, 0x00,
+	// 305 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x90, 0xcf, 0x4a, 0xc3, 0x40,
+	0x18, 0xc4, 0xbb, 0xad, 0x7f, 0xea, 0xb6, 0x07, 0x09, 0x15, 0x42, 0x0f, 0x21, 0xf4, 0xb4, 0xa7,
+	0x5d, 0x69, 0xdf, 0x40, 0x04, 0x51, 0x0f, 0x42, 0x14, 0x04, 0x41, 0x64, 0xd3, 0x2c, 0xe9, 0xd2,
+	0x66, 0xbf, 0x25, 0xdf, 0xd6, 0xb6, 0x3e, 0x82, 0x27, 0x1f, 0xcb, 0x63, 0x8f, 0x1e, 0xa5, 0x7d,
+	0x11, 0x69, 0xa2, 0x51, 0x0f, 0xed, 0xed, 0x9b, 0x61, 0xe6, 0xb7, 0xec, 0x50, 0x96, 0x2c, 0x32,
+	0x65, 0x50, 0x83, 0x99, 0x2f, 0x5e, 0x44, 0x25, 0xc4, 0x58, 0xa2, 0x48, 0x95, 0x51, 0xa8, 0x91,
+	0xdb, 0x1c, 0x1c, 0x78, 0xdd, 0xbf, 0x49, 0x5e, 0x09, 0x3e, 0x96, 0xd8, 0xed, 0xa4, 0x90, 0x42,
+	0x11, 0x13, 0x9b, 0xab, 0x6c, 0x74, 0x7b, 0x3b, 0xd8, 0x49, 0x99, 0xe9, 0xbd, 0xd6, 0x69, 0xfb,
+	0xa2, 0x7c, 0xe7, 0xd6, 0x49, 0xa7, 0xbc, 0x1e, 0x6d, 0xc7, 0x00, 0x0e, 0x5d, 0x2e, 0xad, 0x55,
+	0x89, 0x4f, 0x42, 0xc2, 0x9a, 0xd1, 0x3f, 0xcf, 0xf3, 0xe9, 0x61, 0x26, 0xf5, 0x24, 0x86, 0xb9,
+	0x5f, 0x0f, 0x09, 0x3b, 0x8a, 0x7e, 0xa4, 0x77, 0x4c, 0x1b, 0x1a, 0x33, 0xbf, 0x51, 0xb8, 0x9b,
+	0xd3, 0xbb, 0xa6, 0x4d, 0x98, 0x3a, 0x0b, 0xda, 0x38, 0x7f, 0x2f, 0x24, 0xac, 0xd5, 0x17, 0x7c,
+	0xfb, 0x4f, 0xf8, 0x5d, 0x2e, 0x0d, 0xca, 0xa1, 0xd3, 0x60, 0x6e, 0xbe, 0x6b, 0x51, 0x05, 0xf0,
+	0x1e, 0xe9, 0x89, 0xcd, 0x61, 0xa8, 0x10, 0x55, 0xf2, 0x34, 0xd3, 0x6e, 0x94, 0xe4, 0x72, 0x26,
+	0x27, 0xe8, 0xef, 0x87, 0x0d, 0xd6, 0xea, 0xb3, 0x5d, 0xe4, 0xfb, 0x2a, 0x7e, 0x79, 0x1e, 0x75,
+	0x2a, 0xcc, 0xaf, 0x8d, 0x67, 0x57, 0xef, 0xab, 0x80, 0x2c, 0x57, 0x01, 0xf9, 0x5c, 0x05, 0xe4,
+	0x6d, 0x1d, 0xd4, 0x96, 0xeb, 0xa0, 0xf6, 0xb1, 0x0e, 0x6a, 0x0f, 0xa7, 0xa9, 0x76, 0xa3, 0x69,
+	0xcc, 0x87, 0x90, 0x89, 0x2d, 0xab, 0x3e, 0x0f, 0xc4, 0xbc, 0x98, 0xd6, 0x2d, 0xac, 0xc2, 0xf8,
+	0xa0, 0xd8, 0x77, 0xf0, 0x15, 0x00, 0x00, 0xff, 0xff, 0x7d, 0xe4, 0xed, 0x10, 0xe1, 0x01, 0x00,
+	0x00,
 }
 
 func (m *GenesisState) Marshal() (dAtA []byte, err error) {
@@ -101,6 +151,56 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.ProcessedWithdrawals) > 0 {
+		for iNdEx := len(m.ProcessedWithdrawals) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.ProcessedWithdrawals[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x2a
+		}
+	}
+	if m.Outpoint != nil {
+		{
+			size, err := m.Outpoint.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintGenesis(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0x22
+	}
+	if len(m.Ism) > 0 {
+		i -= len(m.Ism)
+		copy(dAtA[i:], m.Ism)
+		i = encodeVarintGenesis(dAtA, i, uint64(len(m.Ism)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.Mailbox) > 0 {
+		i -= len(m.Mailbox)
+		copy(dAtA[i:], m.Mailbox)
+		i = encodeVarintGenesis(dAtA, i, uint64(len(m.Mailbox)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if m.Bootstrapped {
+		i--
+		if m.Bootstrapped {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i--
+		dAtA[i] = 0x8
+	}
 	return len(dAtA) - i, nil
 }
 
@@ -121,6 +221,27 @@ func (m *GenesisState) Size() (n int) {
 	}
 	var l int
 	_ = l
+	if m.Bootstrapped {
+		n += 2
+	}
+	l = len(m.Mailbox)
+	if l > 0 {
+		n += 1 + l + sovGenesis(uint64(l))
+	}
+	l = len(m.Ism)
+	if l > 0 {
+		n += 1 + l + sovGenesis(uint64(l))
+	}
+	if m.Outpoint != nil {
+		l = m.Outpoint.Size()
+		n += 1 + l + sovGenesis(uint64(l))
+	}
+	if len(m.ProcessedWithdrawals) > 0 {
+		for _, e := range m.ProcessedWithdrawals {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -159,6 +280,160 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: GenesisState: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Bootstrapped", wireType)
+			}
+			var v int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				v |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			m.Bootstrapped = bool(v != 0)
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Mailbox", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Mailbox = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Ism", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Ism = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 4:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Outpoint", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Outpoint == nil {
+				m.Outpoint = &TransactionOutpoint{}
+			}
+			if err := m.Outpoint.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 5:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ProcessedWithdrawals", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.ProcessedWithdrawals = append(m.ProcessedWithdrawals, &WithdrawalID{})
+			if err := m.ProcessedWithdrawals[len(m.ProcessedWithdrawals)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipGenesis(dAtA[iNdEx:])
