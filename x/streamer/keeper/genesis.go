d66e71b5bdd02e596e9349a8d9827eadc7f7fe41
omritoptix <omritoptix@gmail.com>
2025-06-18 11:27:57 -0300

    fix - export all streams - not just acive ones.

--- x/streamer/keeper/genesis.go
+++ x/streamer/keeper/genesis.go
@@ -54,7 +54,7 @@ func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	}
 	return &types.GenesisState{
 		Params:        k.GetParams(ctx),
-		Streams:       k.GetNotFinishedStreams(ctx),
+		Streams:       k.GetStreams(ctx),
 		LastStreamId:  k.GetLastStreamID(ctx),
 		EpochPointers: pointers,
 	}
