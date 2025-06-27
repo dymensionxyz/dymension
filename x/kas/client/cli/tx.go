95d30536ff047ade07bb9602d0c20dbf7f462f10
danwt <30197399+danwt@users.noreply.github.com>
2025-06-27 14:36:17 +0100

    progress checkpoint: code might be broken

--- x/kas/client/cli/tx.go
+++ x/kas/client/cli/tx.go
@@ -2,14 +2,12 @@ package cli
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cobra"
 
-	"github.com/dymensionxyz/dymension/v3/utils"
 	"github.com/dymensionxyz/dymension/v3/x/kas/types"
 )
 
@@ -33,36 +31,7 @@ func CmdSetupBridge() *cobra.Command {
 		Short: "foo",
 		Args:  cobra.MinimumNArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argPubkey := args[0]
-			argRollappId := args[1]
-			bond := args[2]
-
-			var metadata types.SequencerMetadata
-			if len(args) == 4 {
-				if err = utils.ParseJsonFromFile(args[3], &metadata); err != nil {
-					return
-				}
-			}
-
-			rewardAddr, _ := cmd.Flags().GetString(FlagRewardAddress)
-
-			var whitelistedRelayers []string
-			if relayers, _ := cmd.Flags().GetString(FlagWhitelistedRelayers); relayers != "" {
-				whitelistedRelayers = strings.Split(relayers, ",")
-			}
-
 			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-
-			var pk cryptotypes.PubKey
-
-			if err = clientCtx.Codec.UnmarshalInterfaceJSON([]byte(argPubkey), &pk); err != nil {
-				return err
-			}
-
-			bondCoin, err := sdk.ParseCoinNormalized(bond)
 			if err != nil {
 				return err
 			}
@@ -85,8 +54,6 @@ func CmdSetupBridge() *cobra.Command {
 	}
 
 	flags.AddTxFlagsToCmd(cmd)
-	cmd.Flags().String(FlagRewardAddress, "", "Sequencer reward address")
-	cmd.Flags().String(FlagWhitelistedRelayers, "", "Sequencer whitelisted relayers")
 
 	return cmd
 }
