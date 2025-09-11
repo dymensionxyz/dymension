dc0bb8e81de41ce6cd0ca59996502a0a03dee3e1
Michael Tsitrin <michael@dymension.xyz>
2025-09-11 16:08:52 +0300

    UT WIP

--- x/otcbuyback/keeper/auction_test.go
+++ x/otcbuyback/keeper/auction_test.go
@@ -2,118 +2,10 @@ package keeper_test
 
 import (
 	"time"
-
-	"cosmossdk.io/math"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-
-	common "github.com/dymensionxyz/dymension/v3/x/common/types"
-	"github.com/dymensionxyz/dymension/v3/x/otcbuyback/keeper"
-	"github.com/dymensionxyz/dymension/v3/x/otcbuyback/types"
-	streamertypes "github.com/dymensionxyz/dymension/v3/x/streamer/types"
 )
 
-var validCreateAuctionMsg = &types.MsgCreateAuction{
-	Authority:       authtypes.NewModuleAddress(govtypes.ModuleName).String(),
-	Allocation:      common.DymUint64(33), // streamer funded with 100 DYM on setup
-	StartTime:       time.Now().Add(time.Hour),
-	EndTime:         time.Now().Add(25 * time.Hour),   // 24 hour auction
-	InitialDiscount: math.LegacyNewDecWithPrec(5, 2),  // 5%
-	MaxDiscount:     math.LegacyNewDecWithPrec(50, 2), // 50%
-	VestingParams: types.Auction_VestingParams{
-		VestingPeriod:               24 * time.Hour,
-		VestingStartAfterAuctionEnd: time.Hour,
-	},
-	PumpParams: types.Auction_PumpParams{
-		StartTimeAfterAuctionEnd: time.Hour,
-		EpochIdentifier:          "day",
-		NumEpochsPaidOver:        30,
-		NumOfPumpsPerEpoch:       1,
-	},
-}
-
-func (suite *KeeperTestSuite) TestMsgServer_CreateAuction() {
-	var tcMsg types.MsgCreateAuction
-
-	testCases := []struct {
-		name        string
-		setup       func()
-		expectError bool
-		postCheck   func()
-	}{
-		{
-			name: "error - invalid authority",
-			setup: func() {
-				tcMsg.Authority = sdk.AccAddress([]byte("invalid_authority")).String()
-			},
-			expectError: true,
-		},
-		{
-			name: "error - insufficient streamer funds",
-			setup: func() {
-				tcMsg.Allocation = common.DymUint64(1000000000000)
-			},
-			expectError: true,
-		},
-		{
-			name: "error - invalid allocation denom",
-			setup: func() {
-				invalid_coin := sdk.NewCoin("invalid_denom", math.NewIntWithDecimal(1, 18))
-				streamerCoins := sdk.NewCoins(invalid_coin)
-				suite.FundModuleAcc(streamertypes.ModuleName, streamerCoins)
-
-				tcMsg.Allocation = invalid_coin
-			},
-			expectError: true,
-		},
-		{
-			name:        "success - valid auction creation",
-			setup:       func() {},
-			expectError: false,
-			postCheck: func() {
-				// Verify auction was created
-				auction, found := suite.App.OTCBuybackKeeper.GetAuction(suite.Ctx, 1)
-				suite.Require().True(found, "auction should be created")
-				suite.Require().False(auction.IsCompleted(), "new auction should not be completed")
-
-				// Verify funds were transferred from streamer to otcbuyback
-				otcBalance := suite.App.BankKeeper.GetBalance(suite.Ctx,
-					authtypes.NewModuleAddress(types.ModuleName), common.DYMCoin.Denom)
-				suite.Require().Equal(validCreateAuctionMsg.Allocation.String(), otcBalance.String(),
-					"otcbuyback should have received allocation funds")
-			},
-		},
-	}
-
-	for _, tc := range testCases {
-		suite.Run(tc.name, func() {
-			suite.SetupTest() // Reset state for each test
-
-			tcMsg = *validCreateAuctionMsg
-
-			if tc.setup != nil {
-				tc.setup()
-			}
-
-			msgServer := keeper.NewMsgServerImpl(*suite.App.OTCBuybackKeeper)
-			resp, err := msgServer.CreateAuction(suite.Ctx, &tcMsg)
-
-			if tc.expectError {
-				suite.Require().Error(err, "expected error for test case: %s", tc.name)
-			} else {
-				suite.Require().NoError(err, "unexpected error for test case: %s", tc.name)
-				suite.Require().Greater(resp.AuctionId, uint64(0), "auction ID should be positive")
-				if tc.postCheck != nil {
-					tc.postCheck()
-				}
-			}
-		})
-	}
-}
-
 // UT auction ends due to time passing
-func (suite *KeeperTestSuite) TestAuctionEndsDueToTimePassing() {
+func (suite *KeeperTestSuite) TestAuctionLifecycle() {
 
 	suite.Ctx = suite.Ctx.WithBlockTime(time.Now())
 
@@ -144,4 +36,6 @@ func (suite *KeeperTestSuite) TestAuctionEndsDueToTimePassing() {
 	suite.Require().False(auction.IsUpcoming(suite.Ctx.BlockTime()), "auction should be upcoming")
 
 	// FIXME: test auction remaining funds are returned to streamer
+
+	// FIXME: pump streams are created
 }
--- x/otcbuyback/keeper/suite_test.go
+++ x/otcbuyback/keeper/suite_test.go
@@ -6,8 +6,8 @@ import (
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dymensionxyz/dymension/v3/testutil/sample"
 	common "github.com/dymensionxyz/dymension/v3/x/common/types"
-	streamertypes "github.com/dymensionxyz/dymension/v3/x/streamer/types"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/dymensionxyz/dymension/v3/app/apptesting"
@@ -33,28 +33,79 @@ func TestKeeperTestSuite(t *testing.T) {
 func (suite *KeeperTestSuite) SetupTest() {
 	suite.App = apptesting.Setup(suite.T())
 	suite.Ctx = suite.App.NewContext(false).WithBlockTime(time.Now())
-
-	// fund streamer module
-	streamerCoins := sdk.NewCoins(
-		common.DymUint64(1000),
-	)
-	suite.FundModuleAcc(streamertypes.ModuleName, streamerCoins)
 	suite.querier = keeper.NewQueryServerImpl(*suite.App.OTCBuybackKeeper)
+
+	// Note: USDC has 6 decimals instead of 18
+	// price is 4:1
+	poolID := suite.PreparePoolWithCoins(sdk.NewCoins(
+		sdk.NewCoin("usdc", math.NewInt(500_000).MulRaw(1e6)),
+		sdk.NewCoin("adym", math.NewInt(2_000_000).MulRaw(1e18)),
+	))
+
+	spotPrice, err := suite.App.GAMMKeeper.CalculateSpotPrice(suite.Ctx, poolID, "usdc", "adym")
+	suite.Require().NoError(err)
+
+	err = suite.App.OTCBuybackKeeper.SetAcceptedToken(suite.Ctx, "usdc", types.TokenData{
+		PoolId:           poolID,
+		LastAveragePrice: spotPrice,
+	})
+	suite.Require().NoError(err)
 }
 
 func (suite *KeeperTestSuite) CreateDefaultAuction() uint64 {
 	suite.FundModuleAcc(types.ModuleName, sdk.NewCoins(common.DymUint64(100)))
+
+	// Create default vesting and pump params
+	vestingParams := types.Auction_VestingParams{
+		VestingPeriod:               24 * time.Hour,
+		VestingStartAfterAuctionEnd: 0,
+	}
+	pumpParams := types.Auction_PumpParams{
+		StartTimeAfterAuctionEnd: time.Hour,
+		EpochIdentifier:          "day",
+		NumEpochsPaidOver:        30,
+		NumOfPumpsPerEpoch:       1,
+	}
 
 	// Create auction
 	auctionID, err := suite.App.OTCBuybackKeeper.CreateAuction(suite.Ctx,
 		common.DymUint64(100),
 		suite.Ctx.BlockTime(),
 		suite.Ctx.BlockTime().Add(24*time.Hour),
-		math.LegacyNewDecWithPrec(5, 2),  // 5%
+		math.LegacyNewDecWithPrec(10, 2), // 10%
 		math.LegacyNewDecWithPrec(50, 2), // 50%
-		types.DefaultVestingParams,
-		types.DefaultPumpParams,
+		vestingParams,
+		pumpParams,
 	)
 	suite.Require().NoError(err)
 	return auctionID
+}
+
+func (suite *KeeperTestSuite) CreateAuctionWithParams(vestingParams types.Auction_VestingParams, pumpParams types.Auction_PumpParams) uint64 {
+	suite.FundModuleAcc(types.ModuleName, sdk.NewCoins(common.DymUint64(100)))
+
+	auctionID, err := suite.App.OTCBuybackKeeper.CreateAuction(suite.Ctx,
+		common.DymUint64(100),
+		suite.Ctx.BlockTime(),
+		suite.Ctx.BlockTime().Add(24*time.Hour),
+		math.LegacyNewDecWithPrec(5, 2),  // 5%
+		math.LegacyNewDecWithPrec(50, 2), // 50%
+		vestingParams,
+		pumpParams,
+	)
+	suite.Require().NoError(err)
+	return auctionID
+}
+
+// CreateRandomAccount creates a random account address for testing
+func (suite *KeeperTestSuite) CreateRandomAccount() sdk.AccAddress {
+	return sample.Acc()
+}
+
+// BuySomeTokens is a helper function that executes a buy transaction and returns the payment coin
+func (suite *KeeperTestSuite) BuySomeTokens(auctionID uint64, buyer sdk.AccAddress, amountToBuy math.Int, denomToPay string) sdk.Coin {
+	paymentCoin, err := suite.App.OTCBuybackKeeper.Buy(suite.Ctx, buyer, auctionID, amountToBuy, denomToPay)
+	suite.Require().NoError(err, "Buy operation should succeed")
+	suite.Require().True(paymentCoin.IsPositive(), "Payment coin should be positive")
+	return paymentCoin
 }
