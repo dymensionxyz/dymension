aea543f3654fd6a8b5bc7e1716c0094521b41125
danwt <30197399+danwt@users.noreply.github.com>
2025-09-18 10:52:20 +0100

    working on unit test with example

--- x/rollapp/keeper/msg_server_tee_finalize_attest_test.go
+++ x/rollapp/keeper/msg_server_tee_finalize_attest_test.go
@@ -2,6 +2,9 @@ package keeper_test
 
 import (
 	_ "embed"
+	"encoding/json"
+	"strconv"
+	"time"
 
 	"github.com/dymensionxyz/dymension/v3/x/rollapp/types"
 )
@@ -13,10 +16,22 @@ var (
 	policyQuery string
 	//go:embed testdata/tee/policy.rego
 	policyStructure string
+	//go:embed testdata/tee/example_response.json
+	exampleResponse string
 )
 
+type ExampleResponse struct {
+	Result struct {
+		Token string `json:"token"`
+		Nonce struct {
+			RollappId  string `json:"rollapp_id"`
+			CurrHeight string `json:"curr_height"`
+		} `json:"nonce"`
+	} `json:"result"`
+}
+
 func (s *RollappTestSuite) TestValidateAttestation() {
-	s.T().Skip()
+	// s.T().Skip()
 	s.SetupTest()
 	s.k().SetParams(s.Ctx, types.DefaultParams().WithTeeConfig(types.TEEConfig{
 		PolicyValues:    policyValues,
@@ -24,13 +39,25 @@ func (s *RollappTestSuite) TestValidateAttestation() {
 		PolicyStructure: policyStructure,
 	}))
 
+	var res ExampleResponse
+	err := json.Unmarshal([]byte(exampleResponse), &res)
+	s.Require().NoError(err)
+
+	token := res.Result.Token
+
+	rollappId := res.Result.Nonce.RollappId
+	currHeight, err := strconv.ParseUint(res.Result.Nonce.CurrHeight, 10, 64)
+	s.Require().NoError(err)
+
 	// TODO: use proper data and test
-	token := "token"
 	nonce := types.TEENonce{
-		RollappId:       "rollapp_id",
-		CurrHeight:      1,
-		FinalizedHeight: 1,
+		RollappId:       rollappId,
+		CurrHeight:      currHeight,
+		FinalizedHeight: 0,
 	}
-	err := s.k().ValidateAttestation(s.Ctx, nonce.Hash(), token)
+
+	s.Ctx = s.Ctx.WithBlockTime(time.Date(2025, 9, 18, 9, 46, 0, 0, time.UTC))
+	// s.Ctx = s.Ctx.WithBlockTime(time.Now())
+	err = s.k().ValidateAttestation(s.Ctx, nonce.Hash(), token)
 	s.Require().NoError(err)
 }
