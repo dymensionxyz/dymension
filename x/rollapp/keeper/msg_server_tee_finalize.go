b69da4862dbbe30b839f404ec1aae9ef9c49cce4
danwt <30197399+danwt@users.noreply.github.com>
2025-10-03 15:26:35 +0200

    comment

--- x/rollapp/keeper/msg_server_tee_finalize.go
+++ x/rollapp/keeper/msg_server_tee_finalize.go
@@ -63,6 +63,8 @@ func (k msgServer) FastFinalizeWithTEE(goCtx context.Context, msg *types.MsgFast
 		indexToFinalize--
 	}
 
+	// Avoid letting txs through which would do a 'costly'
+	// attestation while not making progress
 	if k.IsIndexFinalized(ctx, rollapp, indexToFinalize) {
 		return nil, gerrc.ErrInvalidArgument.Wrap("index is already finalized")
 	}
