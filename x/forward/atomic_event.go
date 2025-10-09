0f2c0cf4502ed66599c1ea9e1a6559fd4afe39ce
danwt <30197399+danwt@users.noreply.github.com>
2025-10-09 11:13:13 +0200

    replace just event log with event and atomicityg

--- x/forward/atomic_event.go
+++ x/forward/atomic_event.go
@@ -4,13 +4,13 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dymensionxyz/dymension/v3/x/forward/types"
 	"github.com/dymensionxyz/sdk-utils/utils/uevent"
+	"github.com/osmosis-labs/osmosis/v15/osmoutils"
 )
 
-// f returns <is a forward operation, error>. Thus enabling wrapping non-forward operations (parsing and so on).
+// only apply F in state machine if it succeeds
+// emit an event (which has the error if there is one)
 func (k Forward) executeAtomicWithErrEvent(ctx sdk.Context, f func() (bool, error)) {
-	var forwardWasIntended bool // did the user intend to forward?
-	var err error
-	forwardWasIntended, err = f()
+	forwardWasIntended, err := osmosF(ctx, f)
 	evt := &types.EventForward{
 		Ok:           err == nil,
 		WasForwarded: forwardWasIntended,
@@ -22,4 +22,14 @@ func (k Forward) executeAtomicWithErrEvent(ctx sdk.Context, f func() (bool, erro
 	if emitErr != nil {
 		k.Logger(ctx).Error("Emit forward event", "error", emitErr)
 	}
+}
+
+func osmosF(ctx sdk.Context, f func() (bool, error)) (bool, error) {
+	var forwardWasIntended bool // did the user intend to forward?
+	err := osmoutils.ApplyFuncIfNoError(ctx, func(ctx sdk.Context) error {
+		var err error
+		forwardWasIntended, err = f()
+		return err
+	})
+	return forwardWasIntended, err
 }
